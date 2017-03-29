42213c3a95e713720554864a1d110d1c4a07551e
Ryan McCue <ryan@msys.ca>
2017-03-29 13:02:07 -0700

    Styling pages

--- tfrs/views.py
+++ tfrs/views.py
@@ -1,6 +1,7 @@
+import json
+
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
-#from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
@@ -8,14 +9,47 @@ from .models import FuelSupply, FuelSupplier, FuelClass, FuelType
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html', [])
+    table_data = [
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}}
+    ]
+    return render(request, 'dashboard.html', {'table_data': json.dumps(table_data)})
 
 def account_activity(request):
-    return render(request, 'account-activity.html', [])
+    table_data = [
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}},
+        {'transaction':{ 'fuelSupplier': 'Chevron', 'type': 'Sold', 'partner': 'Exxon', 'quantity': 5, 'fairMarketValue': '$100', 'effectiveDate': 'Directors Approval', 'status': 'Pending', 'balance': 1000, 'compliancePeriod': 'January 2015'}}
+    ]
+    return render(request, 'account-activity.html', {'table_data': json.dumps(table_data)})
 
 def new_transaction(request):
-    return render(request, 'new-transaction.html', [])
+    return render(request, 'new-transaction.html', {})
 
+def transaction_summary(request):
+    return render(request, 'transaction-summary.html', {})
+
+def settings(request):
+    return render(request, 'settings.html', {})
+
+def notifications(request):
+    notifications = [
+        {'id': 1, 'subject': 'Credit Transfer Proposed', 'date': '7/12/2015', 'flagged': False, 'message': 'A credit transfer was proposed from exxon'},
+        {'id': 2, 'subject': 'Credits Awarded - Fuel Supply', 'date': '4/3/2015', 'flagged': True, 'message': 'Credits have been awarded by Fuel Supply'},
+        {'id': 3, 'subject': 'Credits Awarded - Part Three', 'date': '6/21/2014', 'flagged': False, 'message': 'Credits have been awarded by Part Three Agreement'}
+    ]
+    return render(request, 'notifications.html', {'notifications': notifications})
 
 class SupplierView(generic.ListView):
     template_name = 'tfrs/index.html'
