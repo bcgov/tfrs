1a391537c151a317e059b4716f616eb8d737702d
Ryan McCue <ryan@msys.ca>
2017-03-31 09:25:32 -0700

    Remove old code, add table extensions, and general design changes

--- tfrs/urls.py
+++ tfrs/urls.py
@@ -12,8 +12,5 @@ urlpatterns = [
     url(r'^notifications/$', views.notifications),
     url(r'^new-transaction/$', views.new_transaction),
     url(r'^transaction-summary/$', views.transaction_summary),
-    url(r'^test$', views.SupplierView.as_view(), name='supplier_index'),
-    url(r'^test/(?P<pk>[0-9]+)/$', views.FuelSupplyView.as_view(), name='supply'),
-    url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
 ]
