243146213db55f1778f868e85c9374b8eefd9aa9
Matthew Hall <matthew.hall@gov.bc.ca>
2017-08-04 15:48:39 -0700

    changed offers to opportunities

--- functional-tests/src/test/groovy/pages/app/OpportunitiesPage.groovy
+++ functional-tests/src/test/groovy/pages/app/OpportunitiesPage.groovy
@@ -3,7 +3,7 @@ package pages.app
 import geb.Page
 
 class OpportunitiesPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Opportunities" }
+    static at = { title == "Opportunities" }
 
     static url = "/opportunities"
 }
