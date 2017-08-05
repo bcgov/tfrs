ee09923a8af6e505577b9ecd3d1e6e038931c6f6
Matthew Hall <matthew.hall@gov.bc.ca>
2017-08-04 17:07:12 -0700

    Update FuelSuppliersPage.groovy

--- functional-tests/src/test/groovy/pages/app/FuelSuppliersPage.groovy
+++ functional-tests/src/test/groovy/pages/app/FuelSuppliersPage.groovy
@@ -5,5 +5,5 @@ import geb.Page
 class FuelSuppliersPage extends Page {
     static at = { title == "Transportation Fuels Reporting System - Fuel Suppliers" }
 
-    static url = "/fuel-suppliers"
+    static url = "/db-fuel-suppliers"
 }
