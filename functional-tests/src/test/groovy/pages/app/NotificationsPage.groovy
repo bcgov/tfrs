d6d5145e150f98b2ff3b8cfc14fda765573d4738
Matthew Hall <matthew.hall@gov.bc.ca>
2017-08-04 17:03:50 -0700

    Update NotificationsPage.groovy

--- functional-tests/src/test/groovy/pages/app/NotificationsPage.groovy
+++ functional-tests/src/test/groovy/pages/app/NotificationsPage.groovy
@@ -3,7 +3,7 @@ package pages.app
 import geb.Page
 
 class NotificationsPage extends Page {
-    static at = { title == "Notifications" }
+    static at = { title == "Transportation Fuels Reporting System - Notifications" }
 
     static url = "/notifications"
 }
