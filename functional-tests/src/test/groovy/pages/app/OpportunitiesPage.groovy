dfc8eb797f559d5189aed712debbb8f6d781b2c4
Matthew Hall <matthew.hall@gov.bc.ca>
2017-08-04 17:03:14 -0700

    Update OpportunitiesPage.groovy

--- functional-tests/src/test/groovy/pages/app/OpportunitiesPage.groovy
+++ functional-tests/src/test/groovy/pages/app/OpportunitiesPage.groovy
@@ -3,7 +3,7 @@ package pages.app
 import geb.Page
 
 class OpportunitiesPage extends Page {
-    static at = { title == "Opportunities" }
+    static at = { title == "Transportation Fuels Reporting System - Opportunities" }
 
     static url = "/opportunities"
 }
