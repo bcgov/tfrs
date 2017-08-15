ae82ac85f9f5aa87439146bdc44ce75a75222118
Stephen Curran <swcurran@gmail.com>
2017-08-15 11:23:08 -0700

    Removed required attribute on two data elements that are not required.

--- APISpec/TestData/load-all.bat
+++ APISpec/TestData/load-all.bat
@@ -7,7 +7,6 @@ rem need to load FuelSupplierContacts before FuleSuppliers.
 call load.bat CreditTradeStatus/CreditTradeStatus_TCS.json api/credittradestatuses/bulk %1%
 call load.bat CreditTradeType/CreditTradeType_CTType.json api/credittradetypes/bulk %1%
 call load.bat FuelSupplierActionsType/FuelSupplierActionsType_FSActionType.json api/fuelsupplieractionstypes/bulk %1%
-call load.bat FuelSupplierContactRole/FuelSupplierContactRole_FSContactRole.json api/fuelsuppliercontactroles/bulk %1%
 call load.bat FuelSupplierStatus/FuelSupplierStatus_FSStatus.json api/fuelsupplierstatuses/bulk %1%
 call load.bat FuelSupplierType/FuelSupplierType_FSType.json api/fuelsuppliertypes/bulk %1%
 call load.bat NotificationType/NotificationType_NotType.json api/notificationtypes/bulk %1%
@@ -21,6 +20,7 @@ call load.bat userRole/userRole_userRole.json api/userroles/bulk %1%
 
 call load.bat FuelSupplier/FuelSupplier_FS.json api/fuelsuppliers/bulk %1%
 call load.bat FuelSupplierCCData/FuelSupplierCCData_FSCCD.json api/fuelsupplierccdata/bulk %1%
+call load.bat FuelSupplierContactRole/FuelSupplierContactRole_FSContactRole.json api/fuelsuppliercontactroles/bulk %1%
 call load.bat FuelSupplierContact/FuelSupplierContact_FSContact.json api/fuelsuppliercontacts/bulk %1%
 
 GOTO End1
--- APISpec/TestData/load-all.sh
+++ APISpec/TestData/load-all.sh
@@ -15,21 +15,21 @@ if [ -e cookie ]; then
 fi
 
 # The order of the loading is important - need to add independent files before dependent ones
-./load.sh CreditTradeStatus/CreditTradeStatus_TCS.json credittradestatuses/bulk $1
-./load.sh CreditTradeType/CreditTradeType_CTType.json credittradetypes/bulk $1
-./load.sh FuelSupplierActionsType/FuelSupplierActionsType_FSActionType.json fuelsupplieractionstypes/bulk $1
-./load.sh FuelSupplierContactRole/FuelSupplierContactRole_FSContactRole.json fuelsuppliercontactroles/bulk $1
-./load.sh FuelSupplierStatus/FuelSupplierStatus_FSStatus.json fuelsupplierstatuses/bulk $1
-./load.sh FuelSupplierType/FuelSupplierType_FSType.json fuelsuppliertypes/bulk $1
-./load.sh NotificationType/NotificationType_NotType.json notificationtypes/bulk $1
-./load.sh OpportunityStatus/OpportunityStatus_OppStatus.json opportunitystatuses/bulk $1
+./load.sh CreditTradeStatus/CreditTradeStatus_TCS.json api/credittradestatuses/bulk $1
+./load.sh CreditTradeType/CreditTradeType_CTType.json api/credittradetypes/bulk $1
+./load.sh FuelSupplierActionsType/FuelSupplierActionsType_FSActionType.json api/fuelsupplieractionstypes/bulk $1
+./load.sh FuelSupplierStatus/FuelSupplierStatus_FSStatus.json api/fuelsupplierstatuses/bulk $1
+./load.sh FuelSupplierType/FuelSupplierType_FSType.json api/fuelsuppliertypes/bulk $1
+./load.sh NotificationType/NotificationType_NotType.json api/notificationtypes/bulk $1
+./load.sh OpportunityStatus/OpportunityStatus_OppStatus.json api/opportunitystatuses/bulk $1
 
-./load.sh permissions/permissions_Perms.json permissions/bulk $1
-./load.sh roles/roles_Role.json roles/bulk $1
-./load.sh rolepermission/rolepermission_RP.json rolepermissions/bulk $1
-./load.sh users/users_user.json users/bulk $1
-./load.sh userRole/userRole_userRole.json userroles/bulk $1
+./load.sh permissions/permissions_Perms.json api/permissions/bulk $1
+./load.sh roles/roles_Role.json api/roles/bulk $1
+./load.sh rolepermission/rolepermission_RP.json api/rolepermissions/bulk $1
+./load.sh users/users_user.json api/users/bulk $1
+./load.sh userRole/userRole_userRole.json api/userroles/bulk $1
 
-./load.sh FuelSupplier/FuelSupplier_FS.json fuelsuppliers/bulk $1
-./load.sh FuelSupplierCCData/FuelSupplierCCData_FSCCD.json fuelsupplierccdata/bulk $1
-./load.sh FuelSupplierContact/FuelSupplierContact_FSContact.json fuelsuppliercontacts/bulk $1
+./load.sh FuelSupplier/FuelSupplier_FS.json api/fuelsuppliers/bulk $1
+./load.sh FuelSupplierCCData/FuelSupplierCCData_FSCCD.json api/fuelsupplierccdata/bulk $1
+./load.sh FuelSupplierContactRole/FuelSupplierContactRole_FSContactRole.json api/fuelsuppliercontactroles/bulk $1
+./load.sh FuelSupplierContact/FuelSupplierContact_FSContact.json api/fuelsuppliercontacts/bulk $1
