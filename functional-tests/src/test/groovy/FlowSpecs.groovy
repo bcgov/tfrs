39b954475204a427cf94689bce418b9de81b2c1c
Matthew Hall <matthew.hall@gov.bc.ca>
2017-09-05 14:19:00 -0700

    Update FlowSpecs.groovy

--- functional-tests/src/test/groovy/FlowSpecs.groovy
+++ functional-tests/src/test/groovy/FlowSpecs.groovy
@@ -3,7 +3,6 @@ import pages.app.DashboardPage
 import pages.app.NotificationsPage
 import pages.app.SettingsPage
 import pages.app.AccountActivityPage
-import pages.app.OpportunitiesPage
 import pages.app.FuelSuppliersPage
 //import pages.app.AdministrationPage
 import pages.external.Accessability
@@ -32,11 +31,10 @@ class FlowSpecs extends GebReportingSpec {
         //DashboardPage       | "navbar-notifications"      | 1             | 5                 || NotificationsPage
         //SettingsPage        | "navbar-dashboard"          | 1             | 5                 || DashboardPage
         //NotificationsPage   | "navbar-settings"           | 1             | 5                 || SettingsPage
-        SettingsPage          | "navbar-opportunities       | 1             | 5                 || OpportunitiesPage
         //Test Externally Linked Pages
-        SettingsPage      | "footer-about-copyright"        | 1             | 3                 || Copyright
-        SettingsPage      | "footer-about-disclaimer"       | 1             | 3                 || Disclaimer
-        SettingsPage      | "footer-about-privacy"          | 1             | 3                 || Privacy
-        SettingsPage      | "footer-about-accessibility"    | 1             | 3                 || Accessability
+        SettingsPage        | "footer-about-copyright"        | 1             | 3                 || Copyright
+        //SettingsPage      | "footer-about-disclaimer"       | 1             | 3                 || Disclaimer
+        //SettingsPage      | "footer-about-privacy"          | 1             | 3                 || Privacy
+        //SettingsPage      | "footer-about-accessibility"    | 1             | 3                 || Accessability
     }
 }
