d4fe878af8ddb92a5fe9009838eafe508a66fa62
Daine Trinidad <Daine.Trinidad@gov.bc.ca>
2017-10-13 15:08:48 -0700

    Set the api url to an env variable `API_BASE_URL` instead of checking NODE_ENV. Also cleaned up the formatting of the code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,97 +1,92 @@
-const Webpack = require('webpack');
-const path = require('path');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const buildPath = path.resolve(__dirname, 'public', 'build');
-const mainPath = path.resolve(__dirname, 'src', 'index.js');
-console.log('using dev');
+const Webpack = require("webpack");
+const path = require("path");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
+const buildPath = path.resolve(__dirname, "public", "build");
+const mainPath = path.resolve(__dirname, "src", "index.js");
+console.log("using dev");
 
-// If NODE_EV is set to `local` for local development
-const isLocal = process.env.NODE_ENV === "local";
-const apiHost = isLocal
-  ? '"http://localhost:8000"'
-  : '"http://tfrs-mem-tfrs-dev.pathfinder.gov.bc.ca/"';
+const apiHost = process.env.API_BASE_URL
+  ? process.env.API_BASE_URL
+  : "http://tfrs-mem-tfrs-dev.pathfinder.gov.bc.ca";
 
 const config = {
   // Makes sure errors in console map to the correct file
   // and line numbe
-  devtool: 'eval',
+  devtool: "eval",
   entry: [
     // Polyfill for Object.assign on IE11, etc
-    'babel-polyfill',
+    "babel-polyfill",
 
     // For hot style updates
-    'webpack/hot/dev-server',
+    "webpack/hot/dev-server",
 
     // The script refreshing the browser on none hot updates
-    'webpack-dev-server/client?http://localhost:8080',
+    "webpack-dev-server/client?http://localhost:8080",
 
     // Our application
-    mainPath],
+    mainPath
+  ],
   output: {
-
     // We need to give Webpack a path. It does not actually need it,
     // because files are kept in memory in webpack-dev-server, but an
     // error will occur if nothing is specified. We use the buildPath
     // as that points to where the files will eventually be bundled
     // in production
     path: buildPath,
-    filename: 'bundle.js',
+    filename: "bundle.js",
 
     // Everything related to Webpack should go through a build path,
     // localhost:3000/build. That makes proxying easier to handle
-    publicPath: '/build/'
+    publicPath: "/build/"
   },
   module: {
-
     loaders: [
-
-    // I highly recommend using the babel-loader as it gives you
-    // ES6/7 syntax and JSX transpiling out of the box
-    {
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: [nodeModulesPath],
-      query:
+      // I highly recommend using the babel-loader as it gives you
+      // ES6/7 syntax and JSX transpiling out of the box
       {
-        presets:['react', 'es2015']
-      }
-    },
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: [nodeModulesPath],
+        query: {
+          presets: ["react", "es2015"]
+        }
+      },
 
-    // Let us also add the style-loader and css-loader, which you can
-    // expand with less-loader etc
-    {
-      test: /\.scss$/,
-      loaders: ['style-loader', 'css-loader', 'sass-loader']
-    },
-    {
-      test: /\.css$/,
-      exclude: /node_modules/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]',
-    },
-    {
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      loaders: [
-          'file?name=[name].[ext]',
-      ]
-    },
-    {
-      test: /\.(otf|eot|svg|ttf|woff|woff2)$/i,
-      loader: 'file?name=./public/assets/fonts/[name].[ext]',
-      query: {
-        limit: 10000
+      // Let us also add the style-loader and css-loader, which you can
+      // expand with less-loader etc
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader:
+          "style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: ["file?name=[name].[ext]"]
+      },
+      {
+        test: /\.(otf|eot|svg|ttf|woff|woff2)$/i,
+        loader: "file?name=./public/assets/fonts/[name].[ext]",
+        query: {
+          limit: 10000
+        }
       }
-    }
-
     ]
   },
   devServer: {
-    historyApiFallback: true,
- },
- devtool: 'source-map', //debug
+    historyApiFallback: true
+  },
+  devtool: "source-map", //debug
   // We have to manually add the Hot Replacement plugin when running
   // from Node
-  plugins: [new Webpack.HotModuleReplacementPlugin(),
-  new Webpack.DefinePlugin({__API__: apiHost})]
+  plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
+    new Webpack.DefinePlugin({ __API__: '"'+apiHost+'"' })
+  ]
 };
 
 module.exports = config;
--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -1,77 +1,74 @@
-var Webpack = require('webpack');
-var path = require('path');
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, 'public', 'build');
-var mainPath = path.resolve(__dirname, 'src', 'index.js');
+var Webpack = require("webpack");
+var path = require("path");
+var nodeModulesPath = path.resolve(__dirname, "node_modules");
+var buildPath = path.resolve(__dirname, "public", "build");
+var mainPath = path.resolve(__dirname, "src", "index.js");
 // var plugins = require('webpack-load-plugins')();
 // plugins.ImageminPlugin = require('imagemin-webpack-plugin');
 // plugsin.imageminMozjpeg = require('imagemin-mozjpeg');
 
-// TODO: Change the path to the actual path. Right now, our dev environment is
-// set to 'production'. Need to separate our deployed dev, test and production
-// environments
-const isProduction = process.env.NODE_ENV === "production";
-const apiHost = isProduction
-  ? '"http://tfrs-mem-tfrs-dev.pathfinder.gov.bc.ca/"'
-  : '"http://tfrs-mem-tfrs-dev.pathfinder.gov.bc.ca/"';
+// Don't set a fall back URL. The users should know immediately if the API URL
+// is not set up correctly
+const apiHost = process.env.API_BASE_URL;
 
 var config = {
   entry: [
     // Polyfill for Object.assign on IE11, etc
-    'babel-polyfill',
+    "babel-polyfill",
 
-    mainPath],
+    mainPath
+  ],
   output: {
     path: buildPath,
-    filename: 'bundle.js',
-    publicPath: '/build/'
+    filename: "bundle.js",
+    publicPath: "/build/"
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: [nodeModulesPath],
-      query:
+    loaders: [
       {
-        presets:['react', 'es2015']
-      }
-    },
-    {
-      test: /\.scss$/,
-      loaders: ['style-loader', 'css-loader', 'sass-loader']
-    },
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    },
-    {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: [nodeModulesPath],
+        query: {
+          presets: ["react", "es2015"]
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-            'file-loader?name=./images/[name].[ext]',
-        ]
-    },
-    {
+        loaders: ["file-loader?name=./images/[name].[ext]"]
+      },
+      {
         test: /\.(otf|eot|svg|ttf|woff|woff2)$/i,
-        loader: 'file-loader?name=./fonts/[name].[ext]',
+        loader: "file-loader?name=./fonts/[name].[ext]",
         query: {
           limit: 10000
         }
-    }]
+      }
+    ]
   },
   devServer: {
-     historyApiFallback: true,
-     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept"
     }
- },
+  },
   plugins: [
     new Webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false
-        }
+      compress: {
+        warnings: false
+      }
     }),
-    new Webpack.DefinePlugin({ __API__: apiHost })
+    new Webpack.DefinePlugin({ __API__: '"' + apiHost + '"' })
   ]
 };
 
