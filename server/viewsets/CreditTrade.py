4f2bcc0b9255f75722b156d6995249e3bdf0fb13
Daine Trinidad <Daine.Trinidad@gov.bc.ca>
2017-11-02 11:24:04 -0700

    Refactoring some code, in preparation for #117

--- server/viewsets/CreditTrade.py
+++ server/viewsets/CreditTrade.py
@@ -1,37 +1,43 @@
-
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, mixins
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
-
 
 from auditable.views import AuditableMixin
 
 from server.models.CreditTrade import CreditTrade
 from server.models.CreditTradeHistory import CreditTradeHistory
 from server.models.CreditTradeStatus import CreditTradeStatus
+from server.models.FuelSupplierBalance import FuelSupplierBalance
+from server.models.FuelSupplier import FuelSupplier
 from server.serializers import CreditTradeCreateSerializer
 from server.serializers import CreditTradeHistoryCreateSerializer
+from server.serializers import CreditTradeApproveSerializer
 from server.serializers import CreditTrade2Serializer as CreditTradeSerializer
-from server.serializers import CreditTradeHistory2Serializer\
+from server.serializers import CreditTradeHistory2Serializer \
     as CreditTradeHistorySerializer
 
+from pprint import pprint
+from datetime import date
 
-class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
+# class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
+class CreditTradeViewSet(AuditableMixin, mixins.CreateModelMixin,
+                         mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+                         mixins.ListModelMixin, viewsets.GenericViewSet):
     """
     This viewset automatically provides `list`, `create`, `retrieve`,
     `update` and `destroy` actions.
     """
 
     permission_classes = (permissions.AllowAny,)
-    http_method_names = ['get', 'post', 'put', 'delete', 'head', 'options',
-                         'trace']
+    http_method_names = ['get', 'post', 'put']
     queryset = CreditTrade.objects.all()
     serializer_class = CreditTradeSerializer
     serializer_classes = {
         'create': CreditTradeCreateSerializer,
         'update': CreditTradeCreateSerializer,
         'default': CreditTradeSerializer,
-        'history': CreditTradeHistorySerializer
+        'history': CreditTradeHistorySerializer,
+        'approve': CreditTradeApproveSerializer,
     }
 
     def get_serializer_class(self):
@@ -39,6 +45,63 @@ class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
             return self.serializer_classes[self.action]
         else:
             return self.serializer_classes['default']
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create a new credit trade
+        """
+        request = self.audit(request)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        self.create_history(serializer.data, True)
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED,
+                        headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Update the credit trade
+        """
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data,
+                                         partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_update(serializer)
+        self.create_history(serializer.data, False)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    @list_route(methods=['post'])
+    def bulk(self):
+        pass
+
+    @detail_route()
+    def history(self, request, pk=None):
+        """
+        Get the credit trade history
+        """
+        credit_trade = self.get_object()
+        history = CreditTradeHistory.objects.filter(creditTradeFK=credit_trade)
+        serializer = self.get_serializer(history, many=True)
+
+        return Response(serializer.data)
+
+    @detail_route(methods=['put'])
+    def delete(self):
+        """Destroys the specified credit trade"""
+        pass
 
     @staticmethod
     def create_history(credit_trade, is_new=True):
@@ -49,10 +112,10 @@ class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
             pk=credit_trade['creditTradeStatusFK'])
 
         try:
-            previous_history = CreditTradeHistory.objects\
-                                         .select_related('creditTradeStatusFK')\
-                                         .filter(creditTradeFK=credit_trade['id'])\
-                                         .latest('create_timestamp')
+            previous_history = CreditTradeHistory.objects \
+                .select_related('creditTradeStatusFK') \
+                .filter(creditTradeFK=credit_trade['id']) \
+                .latest('create_timestamp')
         except CreditTradeHistory.DoesNotExist:
             previous_history = None
 
@@ -64,8 +127,8 @@ class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
 
         if (new_status.status == 'Draft' or
                 (not is_new and
-                 new_status.status == 'Cancelled' and
-                 previous_history.creditTradeStatusFK.status == 'Draft')):
+                         new_status.status == 'Cancelled' and
+                         previous_history.creditTradeStatusFK.status == 'Draft')):
             is_internal_history_record = True
 
         credit_trade_update_time = (
@@ -113,60 +176,3 @@ class CreditTradeViewSet(AuditableMixin, viewsets.ModelViewSet):
 
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
-    def create(self, request, *args, **kwargs):
-        """
-        Create a new credit trade
-        """
-        request = self.audit(request)
-
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        self.perform_create(serializer)
-        self.create_history(serializer.data, True)
-
-        headers = self.get_success_headers(serializer.data)
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED,
-                        headers=headers)
-
-    def update(self, request, *args, **kwargs):
-        """
-        Update the credit trade
-        """
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data,
-                                         partial=partial)
-        serializer.is_valid(raise_exception=True)
-
-        self.perform_update(serializer)
-        self.create_history(serializer.data, False)
-
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
-
-        return Response(serializer.data)
-
-    @list_route(methods=['post'])
-    def bulk(self):
-        pass
-
-    @list_route()
-    def search(self, request):
-        pass
-
-    @detail_route()
-    def history(self, request, pk=None):
-        """
-        Get the credit trade history
-        """
-        credit_trade = self.get_object()
-        history = CreditTradeHistory.objects.filter(creditTradeFK=credit_trade)
-        serializer = self.get_serializer(history, many=True)
-
-        return Response(serializer.data)
-
