2709fec57b19618ae79328cdc1194e4aeda11c6b
Raj <raj.persram@gov.bc.ca>
2017-11-03 14:56:23 -0700

    283 credit transfer credit trade logic to be built (#119)
    * adding Daine's preliminary comments to start the building of the logic for determining who is who in the transaction
    
    * minor change to test if Git username was set properly
    
    * adding Daine's preliminary comments to start the building of the logic for determining who is who in the transaction
    
    * minor change to test if Git username was set properly
    
    * ticket 283 - added exchange icon and placeholder for the credit and dollar amounts for the transaction arrow
    
    * ticket 283 - updated styles on all tables
    
    * ticket 283 - updated general styles for all buttons to give them space from other elements on the page

--- client/src/components/account_activity/CreditTransferVisualRepresentation.jsx
+++ client/src/components/account_activity/CreditTransferVisualRepresentation.jsx
@@ -3,23 +3,47 @@ import { getCreditTradeRespondent } from '../../utils/functions.jsx';
 import * as Values from '../../constants/values.jsx';
 
 export default class CreditTransferVisualRepresentation extends Component {
+  constructor(props) {
+    super(props);
+    //determineBuyerandSeller()
+  }
+
+  determineBuyerandSeller(){
+    // let seller = # logic to choose seller here.
+    // let buyer = # logic to choose buyer based on the type here
+
+    /*
+    this.setState({
+      //update the state here
+      seller: seller,
+      numberOfCredits: this.props.data.numberOfCredits
+      buyer: buyer
+    })
+    */
+  }
+  
   render() {
     return (
       <div className="row visual-representation">
-        <div className="col-sm-4 col-md-3 col-md-offset-1">
+        <div className="col-sm-4 col-md-2 col-md-offset-1">
           <div className="initiator-container label-success">
+            {/* this.state.seller*/}
             <div>{this.props.data.initiatorFK && this.props.data.initiatorFK != null ? getCreditTradeRespondent(this.props.data.initiatorFK) : Values.DEFAULT_INITIATOR}</div>
-            <div>Available: 15,000</div>
+            <div>Available: 65,000</div>
           </div>
         </div>
         <div className="col-sm-4 col-md-2">
           <div className="arrow">
-            <i className="fa fa-arrow-circle-right"></i>
-            <div>10,000</div>
+            <div>25,000 credits</div>
+            {/* <i className="fa fa-arrow-circle-right"></i> */}
+            <i className="fa fa-exchange"></i>
+            {/* this.state.credit */}
+            <div>$13,250,365.00</div>
           </div>
         </div>
         <div className="col-sm-4 col-md-3">
           <div className="respondent-container label-warning">
+            {/* this.state.buyer */}
             <div>{this.props.data.respondentFK && getCreditTradeRespondent(this.props.data.respondentFK)}</div>
             <div>Status: Sell Only</div>
           </div>
