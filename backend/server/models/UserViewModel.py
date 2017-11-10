384394239eec05ebd6d206344716ea4fd2533236
Daine Trinidad <Daine.Trinidad@gov.bc.ca>
2017-11-10 13:06:08 -0800

    Moving all backend code to `backend` and deleting unecessary files #128

---
+++ backend/server/models/UserViewModel.py
@@ -0,0 +1,36 @@
+"""
+    REST API Documentation for the NRS TFRS Credit Trading Application
+
+    The Transportation Fuels Reporting System is being designed to streamline compliance reporting for transportation fuel suppliers in accordance with the Renewable & Low Carbon Fuel Requirements Regulation.
+
+    OpenAPI spec version: v1
+        
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from .UserRole import UserRole
+
+class UserViewModel(models.Model):	    
+    givenName = models.CharField(max_length=255, blank=True, null=True)   
+    surname = models.CharField(max_length=255, blank=True, null=True)   
+    email = models.CharField(max_length=255, blank=True, null=True)   
+    active = models.BooleanField()   
+    smUserId = models.CharField(max_length=255, blank=True, null=True)   
+    userRoles = models.ManyToManyField('UserRole', related_name='UserViewModeluserRoles', blank=True)   
+    class Meta:
+      abstract = True
