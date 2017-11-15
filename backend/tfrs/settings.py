8f136b3f87d1a1ca4632c382960ce84483f8fb0a
Daine Trinidad <Daine.Trinidad@gov.bc.ca>
2017-11-15 10:27:55 -0800

    Cleaning up comments

--- backend/tfrs/settings.py
+++ backend/tfrs/settings.py
@@ -132,7 +132,6 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-print("base dir", BASE_DIR)
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
