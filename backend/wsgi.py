7bfe0fef6bab34abe8f282462647684e495073e2
Daine Trinidad <daine.trinidad@gov.bc.ca>
2017-12-12 17:43:27 -0800

    Putting back django

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,14 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfrs.settings")
-#
-# application = get_wsgi_application()
+import os
 
+from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfrs.settings")
 
-from flask_request import app as application
+application = get_wsgi_application()
