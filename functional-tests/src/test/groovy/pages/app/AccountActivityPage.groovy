c173e1be47f1c2c8751af566948a79c1dad447f6
Roland Stens <roland.stens@gmail.com>
2017-12-14 13:31:31 -0800

    FlowSpecs update for the pages that work.

--- functional-tests/src/test/groovy/pages/app/AccountActivityPage.groovy
+++ functional-tests/src/test/groovy/pages/app/AccountActivityPage.groovy
@@ -1,9 +1,13 @@
 package pages.app
 
 import geb.Page
+import extensions.ReactJSAware
 
-class AccountActivityPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Account Activity" }
-
+class AccountActivityPage extends Page implements ReactJSAware {
+    static at = { reactReady && title == "TFRS" && $("h1.header").text() == "Account Activity" }
     static url = "account-activity"
+
+    static content = {
+            header { $("h1.header").text() }
+    }
 }
