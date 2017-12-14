9a575f961e7279625b555263c28d3cfb52c13225
Roland Stens <roland.stens@gmail.com>
2017-12-14 13:31:31 -0800

    FlowSpecs update for the pages that work.

--- functional-tests/src/test/groovy/pages/app/FuelSuppliersPage.groovy
+++ functional-tests/src/test/groovy/pages/app/FuelSuppliersPage.groovy
@@ -1,9 +1,15 @@
 package pages.app
 
 import geb.Page
+import extensions.ReactJSAware
 
-class FuelSuppliersPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Fuel Suppliers" }
+class FuelSuppliersPage extends Page implements ReactJSAware {
+    static at = { reactReady && title == "TFRS" && $("h1")[1].text() == "Organizations" }
+    static url = "organizations"
 
-    static url = "/db-fuel-suppliers"
+    static content = {
+        header { $("h1")[1].text() }
+        addButton { $("button")[1] }
+        activeOnlyCB { $("input", type:"checkbox") }
+    }
 }
--- functional-tests/src/test/groovy/pages/app/NotificationsPage.groovy
+++ functional-tests/src/test/groovy/pages/app/NotificationsPage.groovy
@@ -1,9 +1,13 @@
 package pages.app
 
 import geb.Page
+import extensions.ReactJSAware
 
-class NotificationsPage extends Page {
-    static at = { title == "Transportation Fuels Reporting System - Notifications" }
+class NotificationsPage extends Page implements ReactJSAware {
+    static at = { reactReady && title == "TFRS" && $("h1")[1].text() == "Notifications" }
+    static url = "notifications"
 
-    static url = "/notifications"
+    static content = {
+        heading { $("h1")[1].text() }
+    }
 }
