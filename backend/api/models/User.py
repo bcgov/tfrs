e7f335a7a73903988c8a25f80eda237fd36857d8
Daine Trinidad <daine.trinidad@gov.bc.ca>
2017-12-15 01:01:20 -0800

    Used Djagno's auth module and changed tests to accomodate authenticatino

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -26,19 +26,17 @@ from auditable.models import Auditable
 
 
 class User(AbstractUser, Auditable):
-    # given_name = models.CharField(max_length=100)
-    # surname = models.CharField(max_length=100)
-    # email = models.CharField(max_length=150, blank=True, null=True)
     password = models.CharField(max_length=128, blank=True, null=True)
     authorization_id = models.CharField(max_length=500, blank=True, null=True)
-    authorization_guid = models.CharField(max_length=100, blank=True, null=True)
+    authorization_guid = models.UUIDField(unique=True, default=None, null=True)
     authorization_directory = models.CharField(max_length=100, blank=True,
                                                null=True)
+    display_name = models.CharField(max_length=500, blank=True, null=True)
     organization = models.ForeignKey(
         'Organization',
         related_name='users',
         blank=True, null=True)
-    effective_date = models.DateField(blank=True, null=True)
+    effective_date = models.DateField(auto_now_add=True, blank=True, null=True)
     expiration_date = models.DateField(blank=True, null=True)
 
     class Meta:
