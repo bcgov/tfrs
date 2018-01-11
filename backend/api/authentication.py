759cebe6e4d85aed0482a315d533b39f5fe84ab4
Daine Trinidad <daine.trinidad@gov.bc.ca>
2018-01-10 16:01:08 -0800

    GUID from siteminder isn't the same format as a regular uuid This commit fixes it by formatting so they match #40

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -1,4 +1,4 @@
-# from django.contrib.auth.models import User
+import uuid
 from django.db.models import Q
 from rest_framework import authentication
 from rest_framework import exceptions
@@ -12,7 +12,7 @@ class UserAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
         header_username = request.META.get('HTTP_SMAUTH_USER', request.META.get(
             'HTTP_SMAUTH_UNIVERSALID'))
-        header_user_guid = request.META.get('HTTP_SMAUTH_USERGUID')
+        header_user_guid = uuid.UUID(request.META.get('HTTP_SMAUTH_USERGUID'))
         header_user_dir = request.META.get('HTTP_SMAUTH_DIRNAME')
         header_user_id = request.META.get('HTTP_SMAUTH_UNIVERSALID').lower()
         header_user_email = request.META.get('HTTP_SMAUTH_USEREMAIL')
@@ -50,7 +50,7 @@ class UserAuthentication(authentication.BaseAuthentication):
                 user.last_name = last_name if last_name else ""
 
             # If we have a guid in the system, but it doesn't match the user's
-            if str(user.authorization_guid) != header_user_guid:
+            if user.authorization_guid != header_user_guid:
                 raise exceptions.AuthenticationFailed(
                     'Invalid user identifier. '
                     'Please contact your administrator.')
