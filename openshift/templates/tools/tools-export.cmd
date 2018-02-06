2ef4c717f375c36f9b76af0ee1db705739975901
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-05 16:54:36 -0800

    update templates

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -1,8 +1,8 @@
-oc export secret -o json --as-template=tools-route > tools-secret.json
-oc export configmap -o json --as-template=tools-route > tools-configmap.json
-oc export rolebinding -o json --as-template=tools-route > tools-rolebinding.json
-oc export pvc -o json --as-template=tools-storage > tools-storage.json
-oc export bc -o json --as-template=tools-build-config > tools-build-config.json
-oc export dc -o json --as-template=tools-deployment-config > tools-deployment-config.json
-oc export service -o json --as-template=tools-service > tools-service.json
-oc export route -o json --as-template=tools-route > tools-route.json
+oc export secret -o json --as-template=tools-route -n mem-tfrs-tools > tools-secret.json
+oc export configmap -o json --as-template=tools-route -n mem-tfrs-tools > tools-configmap.json
+oc export rolebinding -o json --as-template=tools-route -n mem-tfrs-tools > tools-rolebinding.json
+oc export pvc -o json --as-template=tools-storage -n mem-tfrs-tools > tools-storage.json
+oc export bc -o json --as-template=tools-build-config -n mem-tfrs-tools > tools-build-config.json
+oc export dc -o json --as-template=tools-deployment-config -n mem-tfrs-tools > tools-deployment-config.json
+oc export service -o json --as-template=tools-service -n mem-tfrs-tools > tools-service.json
+oc export route -o json --as-template=tools-route -n mem-tfrs-tools > tools-route.json
