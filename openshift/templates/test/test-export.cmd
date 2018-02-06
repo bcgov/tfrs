03954c36d7435e2098291bfe0aad6093a6d33d39
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 10:30:24 -0800

    remove as-template

--- openshift/templates/test/test-export.cmd
+++ openshift/templates/test/test-export.cmd
@@ -1,8 +1,8 @@
-oc export secret -o json --as-template=test-route -n mem-tfrs-test > test-secret.json
-oc export configmap -o json --as-template=test-route -n mem-tfrs-test > test-configmap.json
-oc export rolebinding -o json --as-template=test-route -n mem-tfrs-test > test-rolebinding.json
-oc export pvc -o json --as-template=test-storage -n mem-tfrs-test > test-storage.json
-oc export bc -o json --as-template=test-build-config -n mem-tfrs-test > test-build-config.json
-oc export dc -o json --as-template=test-deployment-config -n mem-tfrs-test > test-deployment-config.json
-oc export service -o json --as-template=test-service -n mem-tfrs-test > test-service.json
-oc export route -o json --as-template=test-route -n mem-tfrs-test > test-route.json
+oc export secret -o json -n mem-tfrs-test > test-secret.json
+oc export configmap -o json -n mem-tfrs-test > test-configmap.json
+oc export rolebinding -o json -n mem-tfrs-test > test-rolebinding.json
+oc export pvc -o json -n mem-tfrs-test > test-storage.json
+oc export bc -o json -n mem-tfrs-test > test-build-config.json
+oc export dc -o json -n mem-tfrs-test > test-deployment-config.json
+oc export service -o json -n mem-tfrs-test > test-service.json
+oc export route -o json -n mem-tfrs-test > test-route.json
