e8847ad8948e3810b873ae3e2711d242f9ca008c
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 12:47:54 -0800

    remove secrets

--- openshift/templates/dev/dev-export.cmd
+++ openshift/templates/dev/dev-export.cmd
@@ -1,4 +1,3 @@
-oc export secret -o json -n mem-tfrs-dev > dev-secret.json
 oc export configmap -o json -n mem-tfrs-dev > dev-configmap.json
 oc export rolebinding -o json -n mem-tfrs-dev > dev-rolebinding.json
 oc export pvc -o json -n mem-tfrs-dev > dev-storage.json
@@ -6,4 +5,4 @@ oc export bc -o json -n mem-tfrs-dev > dev-build-config.json
 oc export dc -o json -n mem-tfrs-dev > dev-deployment-config.json
 oc export service -o json -n mem-tfrs-dev > dev-service.json
 oc export route -o json -n mem-tfrs-dev > dev-route.json
-oc export secret,configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-dev -n mem-tfrs-dev > tfrs-dev-template.json
+oc export configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-dev -n mem-tfrs-dev > tfrs-dev-template.json
