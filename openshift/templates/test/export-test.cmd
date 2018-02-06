da082c4cdc9cfb67095db8d1d2b8aa259a85cd0c
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-05 16:54:36 -0800

    update templates

--- openshift/templates/test/export-test.cmd
+++
@@ -1,5 +0,0 @@
-oc export bc -o json > test-bc.json
-oc export dc -o json > test-dc.json
-oc export service -o json > test-service.json
-oc export route -o json > test-route.json
-oc export pvc -o json > test-pvc.json
