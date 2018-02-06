635ba47daf43f4300171f67e63bd9b0039606b13
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-05 16:46:50 -0800

    update templates

--- openshift/templates/dev/export-dev.cmd
+++ openshift/templates/dev/export-dev.cmd
@@ -1,5 +1,8 @@
-oc export bc -o json > dev-bc.json
-oc export dc -o json > dev-dc.json
-oc export service -o json > dev-service.json
-oc export route -o json > dev-route.json
-oc export pvc -o json > dev-pvc.json
+oc export secret -o json --as-template=dev-route > dev-secret.json
+oc export configmap -o json --as-template=dev-route > dev-configmap.json
+oc export rolebinding -o json --as-template=dev-route > dev-rolebinding.json
+oc export pvc -o json --as-template=dev-storage > dev-storage.json
+oc export bc -o json --as-template=dev-build-config > dev-build-config.json
+oc export dc -o json --as-template=dev-deployment-config > dev-deployment-config.json
+oc export service -o json --as-template=dev-service > dev-service.json
+oc export route -o json --as-template=dev-route > dev-route.json
