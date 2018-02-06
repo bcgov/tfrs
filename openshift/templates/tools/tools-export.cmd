a5684c7b57f109c56f05efcedda6fc8790b58f62
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 11:10:02 -0800

    add tempate

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -6,3 +6,4 @@ oc export bc -o json -n mem-tfrs-tools > tools-build-config.json
 oc export dc -o json -n mem-tfrs-tools > tools-deployment-config.json
 oc export service -o json -n mem-tfrs-tools > tools-service.json
 oc export route -o json -n mem-tfrs-tools > tools-route.json
+oc export secret,configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-tools -n mem-tfrs-tools > tfrs-tools-template.json
