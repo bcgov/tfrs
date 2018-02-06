c7cd6b723e02bd37b7bc3f8af7ffec6e9ea31e05
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 11:14:22 -0800

    add tempate

--- openshift/templates/prod/prod-export.cmd
+++ openshift/templates/prod/prod-export.cmd
@@ -5,3 +5,4 @@ oc export bc -o json -n mem-tfrs-prod > prod-build-config.json
 oc export dc -o json -n mem-tfrs-prod > prod-deployment-config.json
 oc export service -o json -n mem-tfrs-prod > prod-service.json
 oc export route -o json -n mem-tfrs-prod > prod-route.json
+oc export secret,configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-prod -n mem-tfrs-prod > tfrs-prod-template.json
