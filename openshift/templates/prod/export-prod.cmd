050b89aaee0c92bba734c8fefe77e3e4360570d4
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 09:19:04 -0800

    recreate templates

--- openshift/templates/prod/export-prod.cmd
+++
@@ -1,5 +0,0 @@
-oc export bc -o json > prod-bc.json
-oc export dc -o json > prod-dc.json
-oc export service -o json > prod-service.json
-oc export route -o json > prod-route.json
-oc export pvc -o json > prod-pvc.json
