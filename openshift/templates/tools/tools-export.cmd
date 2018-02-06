0b80853861bb7407f4ef8a7158778e820658d663
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-05 16:37:52 -0800

    recreate all templates

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -1,5 +1,8 @@
-oc export bc -o json > tools-bc.json
-oc export dc -o json > tools-dc.json
-oc export service -o json > tools-service.json
-oc export route -o json > tools-route.json
-oc export pvc -o json > tools-pvc.json
+oc export secret -o json --as-template=tools-route > tools-secret.json
+oc export configmap -o json --as-template=tools-route > tools-configmap.json
+oc export rolebinding -o json --as-template=tools-route > tools-rolebinding.json
+oc export pvc -o json --as-template=tools-storage > tools-tools-storage.json
+oc export bc -o json --as-template=tools-build-config > tools-build-config.json
+oc export dc -o json --as-template=tools-deployment-config > tools-deployment-config.json
+oc export service -o json --as-template=tools-service > tools-service.json
+oc export route -o json --as-template=tools-route > tools-route.json
