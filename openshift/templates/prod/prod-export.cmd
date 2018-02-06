80fe3119ed2c6e26dafe0ef9d007ab0553717d78
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 10:33:17 -0800

    remove as-template

--- openshift/templates/prod/prod-export.cmd
+++ openshift/templates/prod/prod-export.cmd
@@ -1,8 +1,8 @@
-oc export secret -o json -n mem-tfrs-prod > prod-secret.json
-oc export configmap -o json -n mem-tfrs-prod > prod-configmap.json
-oc export rolebinding -o json -n mem-tfrs-prod > prod-rolebinding.json
-oc export pvc -o json -n mem-tfrs-prod > prod-storage.json
-oc export bc -o json -n mem-tfrs-prod > prod-build-config.json
-oc export dc -o json -n mem-tfrs-prod > prod-deployment-config.json
-oc export service -o json -n mem-tfrs-prod > prod-service.json
-oc export route -o json -n mem-tfrs-prod > prod-route.json
+oc export secret -o json > prod-secret.json
+oc export configmap -o json > prod-configmap.json
+oc export rolebinding -o json > prod-rolebinding.json
+oc export pvc -o json > prod-storage.json
+oc export bc -o json > prod-build-config.json
+oc export dc -o json > prod-deployment-config.json
+oc export service -o json > prod-service.json
+oc export route -o json > prod-route.json
