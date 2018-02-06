2d957e1ad275d7dceae56a04352a5f6764b56364
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 12:46:42 -0800

    remove secrets

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -1,4 +1,3 @@
-oc export secret -o json -n mem-tfrs-tools > tools-secret.json
 oc export configmap -o json -n mem-tfrs-tools > tools-configmap.json
 oc export rolebinding -o json -n mem-tfrs-tools > tools-rolebinding.json
 oc export pvc -o json -n mem-tfrs-tools > tools-storage.json
@@ -6,4 +5,4 @@ oc export bc -o json -n mem-tfrs-tools > tools-build-config.json
 oc export dc -o json -n mem-tfrs-tools > tools-deployment-config.json
 oc export service -o json -n mem-tfrs-tools > tools-service.json
 oc export route -o json -n mem-tfrs-tools > tools-route.json
-oc export secret,configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-tools -n mem-tfrs-tools > tfrs-tools-template.json
+oc export configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-tools -n mem-tfrs-tools > tfrs-tools-template.json
