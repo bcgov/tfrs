a1e1506e63367caa78d9add56bfbe532c2cf59e3
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-05 16:46:50 -0800

    update templates

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -1,7 +1,7 @@
 oc export secret -o json --as-template=tools-route > tools-secret.json
 oc export configmap -o json --as-template=tools-route > tools-configmap.json
 oc export rolebinding -o json --as-template=tools-route > tools-rolebinding.json
-oc export pvc -o json --as-template=tools-storage > tools-tools-storage.json
+oc export pvc -o json --as-template=tools-storage > tools-storage.json
 oc export bc -o json --as-template=tools-build-config > tools-build-config.json
 oc export dc -o json --as-template=tools-deployment-config > tools-deployment-config.json
 oc export service -o json --as-template=tools-service > tools-service.json
