6ecd768e32ba04bab4ae052d00f9f2af6b8acb85
Kuan Fan <kuan.fan@gov.bc.ca>
2018-02-06 16:02:07 -0800

    export to yaml

--- openshift/templates/tools/tools-export.cmd
+++ openshift/templates/tools/tools-export.cmd
@@ -1,8 +1,6 @@
-oc export configmap -o json -n mem-tfrs-tools > tools-configmap.json
-oc export rolebinding -o json -n mem-tfrs-tools > tools-rolebinding.json
-oc export pvc -o json -n mem-tfrs-tools > tools-storage.json
-oc export bc -o json -n mem-tfrs-tools > tools-build-config.json
-oc export dc -o json -n mem-tfrs-tools > tools-deployment-config.json
-oc export service -o json -n mem-tfrs-tools > tools-service.json
-oc export route -o json -n mem-tfrs-tools > tools-route.json
-oc export configmap,rolebinding,pvc,bc,dc,service,route -o json --as-template=tfrs-tools -n mem-tfrs-tools > tfrs-tools-template.json
+oc export all -o yaml -n mem-tfrs-tools > mem-tfrs-tools-project.yaml
+oc get rolebindings -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-rolebindings.yaml
+oc get serviceaccount -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-serviceaccount.yaml
+oc get pvc -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-pvc.yaml
+oc get configmap -o yaml --export=true  -n mem-tfrs-tools > mem-tfrs-tools-configmap.yaml
+oc export configmap,rolebinding,pvc,bc,dc,service,route -o yaml --as-template=tfrs-tools -n mem-tfrs-tools > mem-tfrs-tools-template.yaml
