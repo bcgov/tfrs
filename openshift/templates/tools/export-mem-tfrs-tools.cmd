c0fb95379571f248e94f3db64d2170bc23079c78
kuanfandevops <31664961+kuanfandevops@users.noreply.github.com>
2018-02-09 12:26:23 -0800

    Request to merge the Openshift backup yaml files (#169)
    * create templates for tools project
    
    * remove jsons
    
    * recreate all templates
    
    * update templates
    
    * update templates
    
    * update templates
    
    * update templates
    
    * recreate templates
    
    * recreate templates
    
    * remove as-template
    
    * remove as-template
    
    * remove as-template
    
    * remove as-template
    
    * remove as-template
    
    * add tempate
    
    * add tempate
    
    * add tempate
    
    * add tempate
    
    * remove secrets
    
    * remove secrets
    
    * remove secrets
    
    * remove secrets
    
    * export to yaml
    
    * export to yaml
    
    * export to yaml
    
    * export to yaml
    
    * use yaml
    
    * replace with yaml
    
    * replace with yaml
    
    * create yaml

---
+++ openshift/templates/tools/export-mem-tfrs-tools.cmd
@@ -0,0 +1,6 @@
+oc export all -o yaml -n mem-tfrs-tools > mem-tfrs-tools-project.yaml
+oc get rolebindings -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-rolebindings.yaml
+oc get serviceaccount -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-serviceaccount.yaml
+oc get pvc -o yaml --export=true -n mem-tfrs-tools > mem-tfrs-tools-pvc.yaml
+oc get configmap -o yaml --export=true  -n mem-tfrs-tools > mem-tfrs-tools-configmap.yaml
+oc export configmap,rolebinding,pvc,bc,dc,service,route -o yaml --as-template=mem-tfrs-tools -n mem-tfrs-tools > mem-tfrs-tools-template.yaml
