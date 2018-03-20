ca371389e8e7120fe8078c93333e48c78abe9bd9
Richard Tan <richard.tan@gov.bc.ca>
2018-03-20 16:06:35 -0700

    Added buttons and improved styling

---
+++ frontend/src/admin/historical_data_entry/components/HistoricalDataEntryFormButtons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import * as Routes from '../../../constants/routes';
+import * as Lang from '../../../constants/langEnUs';
+
+import history from '../../../app/History';
+
+const HistoricalDataEntryFormButtons = props => (
+  <div className="historical-data-entry-actions">
+    <div className="btn-container">
+      <button
+        type="button"
+        className="btn btn-default"
+        onClick={() => history.goBack()}
+      >
+        {Lang.BTN_APP_CANCEL}
+      </button>
+
+      {props.actions.includes(Lang.BTN_COMMIT) &&
+      <button
+        type="submit"
+        className="btn btn-primary"
+        onClick={() => {}}
+      >
+        {Lang.BTN_COMMIT}
+      </button>
+      }
+    </div>
+  </div>
+);
+
+HistoricalDataEntryFormButtons.defaultProps = {
+};
+
+HistoricalDataEntryFormButtons.propTypes = {
+  id: PropTypes.number.isRequired,
+  actions: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default HistoricalDataEntryFormButtons;
