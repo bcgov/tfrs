201a522cd6dca672472017ff97d956640a5a3e32
Richard Tan <richard.tan@gov.bc.ca>
2018-04-19 15:31:40 -0700

    Changed ProgressBar to reflect the statuses better

--- frontend/src/constants/values.js
+++ frontend/src/constants/values.js
@@ -19,11 +19,11 @@ export const CREDIT_TRANSFER_STATUS = {
   },
   proposed: {
     id: 2,
-    description: 'Sign 1/2'
+    description: 'Signed 1/2'
   },
   accepted: {
     id: 3,
-    description: 'Sign 2/2'
+    description: 'Signed 2/2'
   },
   recommendedForDecision: {
     id: 4,
