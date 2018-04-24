5d678361b570d7341d7b9c04026b14f1986cb106
Richard Tan <richard.tan@gov.bc.ca>
2018-04-24 16:05:26 -0700

    Changed some of the messages in the text representation

--- frontend/src/credit_transfers/components/CreditTransferTextRepresentation.js
+++ frontend/src/credit_transfers/components/CreditTransferTextRepresentation.js
@@ -12,24 +12,29 @@ class CreditTransferTextRepresentation extends Component {
 
     this.creditsFrom = this.props.creditsFrom.name;
     this.creditsTo = this.props.creditsTo.name;
+    this.fairMarketValuePerCredit =
+      numeral(this.props.fairMarketValuePerCredit).format(NumberFormat.CURRENCY);
     this.numberOfCredits = numeral(this.props.numberOfCredits).format(NumberFormat.INT);
     this.totalValue = numeral(this.props.totalValue).format(NumberFormat.CURRENCY);
     this.tradeEffectiveDate = (this.props.tradeEffectiveDate)
       ? moment(this.props.tradeEffectiveDate).format('LL') : 'approval';
 
-    this.tradeStatus = (this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
+    this.tradeStatus = (this.props.status.id === CREDIT_TRANSFER_STATUS.approved.id ||
+      this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
       ? CREDIT_TRANSFER_STATUS.approved.description : this.props.status.status;
   }
 
   _buyAction () {
-    return (this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
+    return (this.props.status.id === CREDIT_TRANSFER_STATUS.approved.id ||
+      this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
       ? ' bought '
       : ' is proposing to buy ';
   }
 
   _sellAction () {
-    return (this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
-      ? ' transferred '
+    return (this.props.status.id === CREDIT_TRANSFER_STATUS.approved.id ||
+      this.props.status.id === CREDIT_TRANSFER_STATUS.completed.id)
+      ? ' sold '
       : ' is proposing to sell ';
   }
 
@@ -41,8 +46,9 @@ class CreditTransferTextRepresentation extends Component {
             <span className="value">{this.creditsFrom}</span> {this._sellAction()}
             <span className="value"> {this.numberOfCredits} </span> credit{(this.numberOfCredits > 1) && 's'} to
             <span className="value"> {this.creditsTo} </span>
-            for <span className="value"> {this.totalValue} </span>
-            effective on <span className="value"> {this.tradeEffectiveDate}</span>.
+            for <span className="value"> {this.fairMarketValuePerCredit} </span> per credit
+            for a total value of <span className="value"> {this.totalValue}</span>,
+            effective <span className="value"> {this.tradeEffectiveDate}</span>.
           </div>
         );
       case CREDIT_TRANSFER_TYPES.buy.id:
@@ -58,30 +64,33 @@ class CreditTransferTextRepresentation extends Component {
       case CREDIT_TRANSFER_TYPES.validation.id:
         return (
           <div className="text-representation">
-            A <span className="value">Validation</span> of
-            <span className="value"> {this.numberOfCredits} </span> credit{(this.numberOfCredits > 1) && 's'} for
+            A <span className="value">validation</span> of
+            <span className="value"> {this.numberOfCredits} </span>
+            credit{(this.numberOfCredits > 1) && 's'} earned by
             <span className="value"> {this.creditsTo} </span>
-            has been <span className="value lowercase"> {this.tradeStatus} </span>
+            has been <span className="value lowercase"> {this.tradeStatus}</span>,
             effective on <span className="value"> {this.tradeEffectiveDate}</span>.
           </div>
         );
       case CREDIT_TRANSFER_TYPES.retirement.id:
         return (
           <div className="text-representation">
-            A <span className="value">Reduction</span> of
-            <span className="value"> {this.numberOfCredits} </span> credit{(this.numberOfCredits > 1) && 's'} for
+            A <span className="value">reduction</span> of
+            <span className="value"> {this.numberOfCredits} </span>
+            credit{(this.numberOfCredits > 1) && 's'} earned by
             <span className="value"> {this.creditsFrom} </span>
-            has been <span className="value lowercase"> {this.tradeStatus} </span>
+            has been <span className="value lowercase"> {this.tradeStatus}</span>,
             effective on <span className="value"> {this.tradeEffectiveDate}</span>.
           </div>
         );
       case CREDIT_TRANSFER_TYPES.part3Award.id:
         return (
           <div className="text-representation">
-            A <span className="value">Part 3 award</span> of
-            <span className="value"> {this.numberOfCredits} </span> credit{(this.numberOfCredits > 1) && 's'} for
-            <span className="value"> {this.creditsTo} </span>
-            has been <span className="value lowercase"> {this.tradeStatus} </span>
+            An <span className="value">award</span> of
+            <span className="value"> {this.numberOfCredits} </span>
+            credit{(this.numberOfCredits > 1) && 's'} earned by
+            <span className="value"> {this.creditsTo} </span> for the completion of a
+            Part 3 Agreement has been <span className="value lowercase"> {this.tradeStatus}</span>,
             effective on <span className="value"> {this.tradeEffectiveDate}</span>.
           </div>
         );
@@ -118,6 +127,10 @@ CreditTransferTextRepresentation.propTypes = {
     name: PropTypes.string,
     id: PropTypes.number
   }),
+  fairMarketValuePerCredit: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]).isRequired,
   numberOfCredits: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number
