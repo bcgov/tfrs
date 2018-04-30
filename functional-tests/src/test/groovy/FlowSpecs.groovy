a3e01b7a00eaaf73083d23cb071eddeac552539f
Richard Tan <richard.tan@gov.bc.ca>
2018-04-30 08:45:37 -0700

    Added specific url to logontest, so localhost test will still work

--- functional-tests/src/test/groovy/FlowSpecs.groovy
+++ functional-tests/src/test/groovy/FlowSpecs.groovy
@@ -18,13 +18,15 @@ import spock.lang.*
 class FlowSpecs extends GebReportingSpec {
 
      def "Login Once"(){
-        when: "I go to the TFRS URL "
-            go baseUrl
+        when: "I go to the Login URL "
+            go "https://logontest.gov.bc.ca/clp-cgi/capBceid/logon.cgi?flags=1101:1,7&TYPE=33554433&REALMOID=06-7a693a2b-07ab-481f-ae38-e9cccc861e78&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=5SlxkAjMjyMfuL59wytntBoF3Ika3c1xW%2bUVeexhRKy8oHb7NkxEL5YuGJpnQIWjoZCSSfSPxRYnEVatgnfxz91irpUOq%2bJ%2f&TARGET=https%3a%2f%2fdev%2elowcarbonfuels%2egov%2ebc%2eca%2f"
         and: "I log in on the SiteMinder Login page"    
             at LoginPage
             userName.value("Rstens")
             passWord.value("Tfrs01Testing!")
             logIn.click()
+        and: "I go to the TFRS Dashboard"
+            go baseUrl
         then: "I will arrive at the TFRS Dashboard"    
             at DashboardPage
     }
