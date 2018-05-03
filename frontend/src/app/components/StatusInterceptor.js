5b59d000387804078248c293cdfbee52cb50d51f
Richard Tan <richard.tan@gov.bc.ca>
2018-05-03 10:19:50 -0700

    Updating the error messages

--- frontend/src/app/components/StatusInterceptor.js
+++ frontend/src/app/components/StatusInterceptor.js
@@ -8,19 +8,19 @@ const StatusInterceptor = (props) => {
 
   switch (props.statusCode) {
     case 401:
-      title = 'Not authenticated';
-      message = `Sorry, you are not authenticated to use this application. Please contact your administrator`;
+      title = `You are logged in but we can't find your account in our system. Please contact your company representative for assistance.`;
+      message = `HTTP Status Code: 401 Unauthenticated.`;
       break;
     case 403:
-      title = `You are logged in but do not have permission to access to this content. Please contact your User Manager for help.`;
+      title = `You are logged in but do not have permission to access this content. Please contact your company representative for assistance.`;
       message = `HTTP Status Code: 403 Forbidden.`;
       break;
     case 500:
-      title = `You are logged in but there is an API Error, which is preventing access to the project.`;
+      title = `An API error is preventing access to the system. Please try again later.`;
       message = `HTTP Status Code: 500 Internal Server Error.`;
       break;
     case 502:
-      title = `You are logged in but there is an API Error, which is preventing access to the project.`;
+      title = `A gateway error is preventing access to the system. Please try again later.`;
       message = `HTTP Status Code: 502 Bad Gateway.`;
       break;
     default:
