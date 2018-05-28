cca801b1bd206c39984f2e8ba7086ab06e4b55fa
Richard Tan <richard.tan@gov.bc.ca>
2018-05-28 11:24:48 -0700

    Removed the additional . from technical difficulties

--- frontend/src/app/components/StatusInterceptor.js
+++ frontend/src/app/components/StatusInterceptor.js
@@ -45,7 +45,7 @@ class StatusInterceptor extends Component {
     return (
       <div className="alert alert-danger error-alert" role="alert">
         <p>We&apos;re sorry.</p>
-        <p>It looks like our system is experiencing some technical difficulties.. We have been
+        <p>It looks like our system is experiencing some technical difficulties. We have been
           notified and will look into it. Please try again later.
         </p>
       </div>
@@ -56,7 +56,7 @@ class StatusInterceptor extends Component {
     return (
       <div className="alert alert-danger error-alert" role="alert">
         <p>We&apos;re sorry.</p>
-        <p>It looks like our system is experiencing some technical difficulties.. We have been
+        <p>It looks like our system is experiencing some technical difficulties. We have been
           notified and will look into it. Please try again later.
         </p>
       </div>
