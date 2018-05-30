6d12763cd0bfce9fd9a045ab153e62e9541e5d5f
Robert Johnstone <rob@plasticviking.com>
2018-05-30 12:08:08 -0700

    Modified Credit Transaction Page to pre-filter data based on organization.

--- frontend/src/credit_transfers/components/CreditTransactionsPage.js
+++ frontend/src/credit_transfers/components/CreditTransactionsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import numeral from 'numeral';
 
@@ -8,20 +8,42 @@ import { DEFAULT_ORGANIZATION } from '../../constants/values';
 import history from '../../app/History';
 import Loading from '../../app/components/Loading';
 import CreditTransferTable from './CreditTransferTable';
+import {organization} from "../../sampleData";
 
-const CreditTransactionsPage = (props) => {
-  const { isFetching, items } = props.creditTransfers;
-  const isEmpty = items.length === 0;
+class CreditTransactionsPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterOrganization: 'all',
+    }
+  }
+
+  render() {
+
+      const { isFetching, items } = this.props.creditTransfers;
+      const isEmpty = items.length === 0;
+
+      const uniqueOrganizations = !isFetching ? Array.from(new Set(items.flatMap((item) => {return [item.creditsFrom.name, item.creditsTo.name];}))).sort(): [];
+
+      let preFilteredItems = this.state.filterOrganization != 'all' ? items.filter(
+          item => {
+            return item.creditsFrom.name === (this.state.filterOrganization) ||
+            item.creditsTo.name === (this.state.filterOrganization)
+          }
+      ): items;
+
+
   return (
-    <div className="page_credit_transactions">
+      <div className="page_credit_transactions">
       <h3 className="credit_balance">
-        Credit Balance: {numeral(props.loggedInUser.organizationBalance).format(NumberFormat.INT)}
+        Credit Balance: {numeral(this.props.loggedInUser.organizationBalance).format(NumberFormat.INT)}
       </h3>
-      <h1>{props.title}</h1>
+      <h1>{this.props.title}</h1>
       <div className="right-toolbar-container">
         <div className="actions-container">
-          {props.loggedInUser.organization &&
-            props.loggedInUser.organization.id === DEFAULT_ORGANIZATION.id &&
+          {this.props.loggedInUser.organization &&
+            this.props.loggedInUser.organization.id === DEFAULT_ORGANIZATION.id &&
             <button
               className="btn btn-primary"
               type="button"
@@ -31,17 +53,27 @@ const CreditTransactionsPage = (props) => {
             </button>
           }
         </div>
+          {!isFetching &&
+          <div className="form-group organization_filter">
+              <label htmlFor="organizationFilterSelect">Show transactions involving:</label>
+            <select id="organizationFilterSelect" className="form-control" onChange={e => this.setState({filterOrganization: e.target.value})}>
+                <option value='all'>All Organizations</option>
+                {uniqueOrganizations.map(organizationName => {return (<option key={organizationName} value={organizationName}>{organizationName}</option>)})}
+            </select>
+          </div>
+          }
       </div>
       {isFetching && <Loading />}
       {!isFetching &&
       <CreditTransferTable
-        items={items}
+        items={preFilteredItems}
         isFetching={isFetching}
         isEmpty={isEmpty}
       />
       }
     </div>
-  );
+      )
+  }
 };
 
 CreditTransactionsPage.propTypes = {
