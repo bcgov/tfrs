6913e88d8591224f161e7dc19c5405513a89c075
Robert Johnstone <rob@plasticviking.com>
2018-05-31 10:02:31 -0700

    frontend/src/credit_transfers/components/CreditTransactionsPage.js

--- frontend/src/credit_transfers/components/CreditTransactionsPage.js
+++ frontend/src/credit_transfers/components/CreditTransactionsPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import numeral from 'numeral';
 
@@ -10,38 +10,42 @@ import Loading from '../../app/components/Loading';
 import CreditTransferTable from './CreditTransferTable';
 
 class CreditTransactionsPage extends Component {
-
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
-      filterOrganization: 'all',
-    }
+      filterOrganization: 'all'
+    };
   }
 
-  render() {
+  render () {
+    const { isFetching, items } = this.props.creditTransfers;
+    const isEmpty = items.length === 0;
 
-      const { isFetching, items } = this.props.creditTransfers;
-      const isEmpty = items.length === 0;
+    let uniqueOrganizations = [];
 
-      const uniqueOrganizations = !isFetching ? Array.from(new Set(items.flatMap((item) => {return [item.creditsFrom.name, item.creditsTo.name];}))).sort(): [];
+    if (!isFetching) {
+      uniqueOrganizations = (
+        Array.from(new Set(items.flatMap(item => [item.creditsFrom.name, item.creditsTo.name])))
+      ).sort();
+    }
 
-      let preFilteredItems = this.state.filterOrganization !== 'all' ? items.filter(
-          item => {
-            return item.creditsFrom.name === (this.state.filterOrganization) ||
-            item.creditsTo.name === (this.state.filterOrganization)
-          }
-      ): items;
+    let preFilteredItems = items;
 
+    if (this.state.filterOrganization !== 'all') {
+      preFilteredItems = items.filter(item =>
+        item.creditsFrom.name === (this.state.filterOrganization) ||
+        item.creditsTo.name === (this.state.filterOrganization));
+    }
 
-  return (
+    return (
       <div className="page_credit_transactions">
-      <h3 className="credit_balance">
+        <h3 className="credit_balance">
         Credit Balance: {numeral(this.props.loggedInUser.organizationBalance).format(NumberFormat.INT)}
-      </h3>
-      <h1>{this.props.title}</h1>
-      <div className="right-toolbar-container">
-        <div className="actions-container">
-          {this.props.loggedInUser.organization &&
+        </h3>
+        <h1>{this.props.title}</h1>
+        <div className="right-toolbar-container">
+          <div className="actions-container">
+            {this.props.loggedInUser.organization &&
             this.props.loggedInUser.organization.id === DEFAULT_ORGANIZATION.id &&
             <button
               className="btn btn-primary"
@@ -50,30 +54,40 @@ class CreditTransactionsPage extends Component {
             >
               Propose Trade
             </button>
-          }
-        </div>
+            }
+          </div>
           {(!isFetching && this.props.loggedInUser.organization.id === DEFAULT_ORGANIZATION.id) &&
           <div className="form-group organization_filter">
-              <label htmlFor="organizationFilterSelect">Show transactions involving:</label>
-            <select id="organizationFilterSelect" className="form-control" onChange={e => this.setState({filterOrganization: e.target.value})}>
-                <option value='all'>All Organizations</option>
-                {uniqueOrganizations.map(organizationName => {return (<option key={organizationName} value={organizationName}>{organizationName}</option>)})}
-            </select>
+            <label htmlFor="organizationFilterSelect">Show transactions involving:
+              <select
+                id="organizationFilterSelect"
+                className="form-control"
+                onChange={event => this.setState({ filterOrganization: event.target.value })}
+              >
+                <option value="all">All Organizations</option>
+                {uniqueOrganizations.map(organizationName =>
+                  (
+                    <option key={organizationName} value={organizationName}>
+                      {organizationName}
+                    </option>
+                  ))}
+              </select>
+            </label>
           </div>
           }
+        </div>
+        {isFetching && <Loading />}
+        {!isFetching &&
+        <CreditTransferTable
+          items={preFilteredItems}
+          isFetching={isFetching}
+          isEmpty={isEmpty}
+        />
+        }
       </div>
-      {isFetching && <Loading />}
-      {!isFetching &&
-      <CreditTransferTable
-        items={preFilteredItems}
-        isFetching={isFetching}
-        isEmpty={isEmpty}
-      />
-      }
-    </div>
-      )
+    );
   }
-};
+}
 
 CreditTransactionsPage.propTypes = {
   creditTransfers: PropTypes.shape({
