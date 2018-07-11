5cbd4a5aa7549fdea02ca613839f9e03d0e455f2
Robert Johnstone <rob@plasticviking.com>
2018-07-11 11:27:05 -0700

    additional test for respondent manipulations

--- backend/api/tests/test_credit_trade_flow.py
+++ backend/api/tests/test_credit_trade_flow.py
@@ -23,6 +23,7 @@
 """
 import datetime
 import json
+from collections import namedtuple
 
 from rest_framework import status
 
@@ -149,64 +150,113 @@ class TestCreditTradeFlow(BaseTestCase):
 
         fs1user = self.users['fs_user_1']
         fs2user = self.users['fs_user_2']
+        fs3user = self.users['fs_user_3']
+
+        Manipulation = namedtuple('Manipulation',
+                                  ['initial_value',
+                                   'manipulated_value',
+                                   'response_transformation'])
+
+        to_check = {
+            'fairMarketValuePerCredit': Manipulation(1000.0,
+                                                     750.0,
+                                                     lambda data: float(data)),
+
+            'numberOfCredits': Manipulation(100,
+                                            200,
+                                            lambda data: data),
+
+            'type': Manipulation(self.credit_trade_types['buy'].id,
+                                 self.credit_trade_types['sell'].id,
+                                 lambda data: data['id']),
+
+            'tradeEffectiveDate': Manipulation(
+                datetime.datetime.today().strftime('%Y-%m-%d'),
+                (datetime.datetime.today() + datetime.timedelta(days=-2)).strftime('%Y-%m-%d'),
+                lambda data: data
+            ),
+
+            'zeroReason': Manipulation(None,
+                                       self.zero_reason['other'].id,
+                                       lambda data: data['id'] if data is not None else data),
+
+            'initiator': Manipulation(fs1user.organization.id,
+                                      fs3user.organization.id,
+                                      lambda data: data['id']),
+
+            'respondent': Manipulation(fs2user.organization.id,
+                                       fs3user.organization.id,
+                                       lambda data: data['id'])
+        }
 
         # submit
 
-        payload = {
-            'fairMarketValuePerCredit': 1000.0,
-            'initiator': fs1user.organization.id,
-            'numberOfCredits': 100,
-            'respondent': fs2user.organization.id,
-            'status': self.statuses['submitted'].id,
-            'tradeEffectiveDate': datetime.datetime.today().strftime('%Y-%m-%d'),
-            'type': self.credit_trade_types['buy'].id,
-            'is_rescinded': False,
-            'zeroReason': None
-        }
+        for field, manipulation in to_check.items():
 
-        response = self.clients[fs1user.username].post(
-            '/api/credit_trades',
-            content_type='application/json',
-            data=json.dumps(payload)
-        )
+            with self.subTest("testing respondent field manipulation",
+                              field=field,
+                              initial_value=manipulation.initial_value,
+                              attempted_manipulation=manipulation.manipulated_value):
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+                payload = {
+                    'fairMarketValuePerCredit': 100,
+                    'initiator': fs1user.organization.id,
+                    'numberOfCredits': 100,
+                    'respondent': fs2user.organization.id,
+                    'status': self.statuses['submitted'].id,
+                    'tradeEffectiveDate': datetime.datetime.today().strftime('%Y-%m-%d'),
+                    'type': self.credit_trade_types['buy'].id,
+                    'is_rescinded': False,
+                    'zeroReason': None
+                }
 
-        ct_id = json.loads(response.content.decode('utf-8'))['id']
+                payload[field] = manipulation.initial_value
 
-        # I am altering the deal. Pray I do not alter it further.
+                response = self.clients[fs1user.username].post(
+                    '/api/credit_trades',
+                    content_type='application/json',
+                    data=json.dumps(payload)
+                )
 
-        payload = {
-            'fairMarketValuePerCredit': 750.0,  # <---
-            'initiator': fs1user.organization.id,
-            'numberOfCredits': 100,
-            'respondent': fs2user.organization.id,
-            'status': self.statuses['accepted'].id,
-            'tradeEffectiveDate': datetime.datetime.today().strftime('%Y-%m-%d'),
-            'type': self.credit_trade_types['buy'].id,
-            'is_rescinded': False,
-            'zeroReason': None
-        }
+                self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        response = self.clients[fs2user.username].put(
-            '/api/credit_trades/{}'.format(ct_id),
-            content_type='application/json',
-            data=json.dumps(payload)
-        )
+                ct_id = json.loads(response.content.decode('utf-8'))['id']
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+                # I am altering the deal. Pray I do not alter it further.
 
-        ct_accepted = self.clients['fs_user_1'].get(
-            '/api/credit_trades/{}'.format(ct_id),
-            content_type='application/json')
+                payload = {
+                    'fairMarketValuePerCredit': 100,
+                    'initiator': fs1user.organization.id,
+                    'numberOfCredits': 100,
+                    'respondent': fs2user.organization.id,
+                    'status': self.statuses['accepted'].id,
+                    'tradeEffectiveDate': datetime.datetime.today().strftime('%Y-%m-%d'),
+                    'type': self.credit_trade_types['buy'].id,
+                    'is_rescinded': False,
+                    'zeroReason': None
+                }
 
-        accepted_response = json.loads(
-            ct_accepted.content.decode("utf-8"))
+                payload[field] = manipulation.manipulated_value
 
-        # Status of Credit Trade should be 'accepted'
-        self.assertEqual(accepted_response['status']['id'],
-                         self.statuses['accepted'].id)
+                response = self.clients[fs2user.username].put(
+                    '/api/credit_trades/{}'.format(ct_id),
+                    content_type='application/json',
+                    data=json.dumps(payload)
+                )
 
-        # fairMarketValuePerCredit should not have changed
-        self.assertEqual(float(accepted_response['fairMarketValuePerCredit']),
-                         1000.0)
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+                ct_accepted = self.clients[fs1user.username].get(
+                    '/api/credit_trades/{}'.format(ct_id),
+                    content_type='application/json')
+
+                accepted_response = json.loads(
+                    ct_accepted.content.decode("utf-8"))
+
+                # Status of Credit Trade should be 'accepted'
+                self.assertEqual(accepted_response['status']['id'],
+                                 self.statuses['accepted'].id)
+
+                # altered should not have changed
+                self.assertEqual(manipulation.response_transformation(accepted_response[field]),
+                                 manipulation.initial_value)
