8138d168d0f20d81aae8286946b67f207eb0539a
Richard <richard.mizuno@gmail.com>
2018-08-10 10:46:42 -0700

    Added export to spreadsheet functionality for credit transfers and fuel suppliers (#496)
    Created SpreadSheet Builder service
    Added Download as Spreadsheet button to Credit Transactions
    Added Download as Spreadsheet button to Fuel Suppliers

--- frontend/src/constants/routes/CreditTransactions.js
+++ frontend/src/constants/routes/CreditTransactions.js
@@ -1,8 +1,11 @@
+const API = '/api/credit_trades';
 const BASE_PATH = '/credit_transactions';
 
 const CREDIT_TRANSACTIONS = {
   ADD: `${BASE_PATH}/add`,
+  API,
   EDIT: `${BASE_PATH}/edit/:id`,
+  EXPORT: `${API}/xls`,
   DETAILS: `${BASE_PATH}/view/:id`,
   LIST: BASE_PATH
 };
