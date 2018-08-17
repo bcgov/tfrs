01961f3a12ab81888ca7ce101b50e6e628b4c43c
Richard Tan <richard.tan@gov.bc.ca>
2018-08-17 08:03:45 -0700

    Restored the NotFound page but moved it to src/app/components

---
+++ frontend/src/app/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div>
+    <h1>Not Found</h1>
+    <p>We give you credit for trying to find a valid page, but this is not it.</p>
+    <p>
+      To trade this page for a valid one click <a href="/">here</a> or learn more about Credit Validation <a href="http://www.gov.bc.ca/lowcarbonfuels/">here</a>.
+    </p>
+  </div>
+);
+
+export default NotFound;
