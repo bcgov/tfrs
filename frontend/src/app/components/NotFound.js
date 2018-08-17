3b225fb7622847729d6d1354369122cc1eb7525d
Richard Tan <richard.tan@gov.bc.ca>
2018-08-17 08:06:35 -0700

    Changed the text and style to match our actual not-found page

--- frontend/src/app/components/NotFound.js
+++ frontend/src/app/components/NotFound.js
@@ -1,12 +1,9 @@
 import React from 'react';
 
 const NotFound = () => (
-  <div>
-    <h1>Not Found</h1>
-    <p>We give you credit for trying to find a valid page, but this is not it.</p>
-    <p>
-      To trade this page for a valid one click <a href="/">here</a> or learn more about Credit Validation <a href="http://www.gov.bc.ca/lowcarbonfuels/">here</a>.
-    </p>
+  <div className="alert alert-danger error-alert" role="alert">
+    <p>The requested page could not be found.</p>
+    <p>To trade this page for a valid one click <a href="/">here</a> or learn more about the Renewable and Low Carbon Fuel Requirements Regulation <a href="http://www.gov.bc.ca/lowcarbonfuels/" rel="noopener noreferrer" target="_blank">here</a></p>
   </div>
 );
 
