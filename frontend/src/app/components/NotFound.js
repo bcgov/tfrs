f3152b38807ff02784bd8e7d1b33fdc129103945
Richard <richard.mizuno@gmail.com>
2018-08-17 08:22:58 -0700

    Not found (#532)
    I didn't realize we were actually using one of the jsx files.
    I'm restoring it, but moving it to src/app/Components and changing the extension.

---
+++ frontend/src/app/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+  <div className="alert alert-danger error-alert" role="alert">
+    <p>The requested page could not be found.</p>
+    <p>To trade this page for a valid one click <a href="/">here</a> or learn more about the Renewable and Low Carbon Fuel Requirements Regulation <a href="http://www.gov.bc.ca/lowcarbonfuels/" rel="noopener noreferrer" target="_blank">here</a></p>
+  </div>
+);
+
+export default NotFound;
