391ed65d5f1adc3f7a81185906b9225e8673c09c
Rob Johnstone <rob@plasticviking.com>
2018-08-29 13:17:25 -0700

    styling

--- frontend/src/notifications/components/NotificationsPage.js
+++ frontend/src/notifications/components/NotificationsPage.js
@@ -2,34 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
 import Button from 'react-bootstrap/es/Button';
+import LocalTimestamp from '../../app/components/LocalTimestamp';
+
+const ReadStyledCell = props => (
+  <span className={`${props.read ? 'read' : 'unread'}`}>{props.children}</span>
+);
 
 const NotificationsPage = props => (
   <div className="page_notifications">
     <h1>Notifications</h1>
     <ReactTable
+      className="notification-table"
       loading={props.isFetching}
       data={props.notifications}
       filterable
       columns={
         [
-          { id: 'message', accessor: 'message', Header: 'Message' },
-          { id: 'createTimestamp', accessor: 'createTimestamp', Header: 'Time' },
-          { id: 'relatedCreditTrade', accessor: 'relatedCreditTrade', Header: 'Related Credit Trade' },
-          { id: 'relatedOrganization', accessor: 'relatedOrganization', Header: 'Related Organization' },
-          { id: 'relatedUser', accessor: 'relatedUser', Header: 'Related User' },
-          {
-            id: 'isRead', accessor: 'isRead', Header: 'Is Read?', Cell: row => (row.value ? 'True' : 'False')
-          },
           {
             id: 'markRead',
             accessor: 'markRead',
-            Header: 'Mark (Un)Read',
+            Header: 'Mark',
             Cell: cellInfo => (
-              <Button type="button" onClick={() => props.changeReadStatus(cellInfo.original.id, !cellInfo.original.isRead)}>Mark {cellInfo.original.isRead ? 'Unread':'Read'}</Button>
-            )
+              <Button type="button" onClick={() => props.changeReadStatus(cellInfo.original.id, !cellInfo.original.isRead)}>Mark {cellInfo.original.isRead ? 'Unread' : 'Read'}</Button>
+            ),
+            maxWidth: 140
+          },
+          {
+            id: 'message',
+            accessor: 'message',
+            Header: 'Message',
+            minWidth: 200,
+            Cell: cell => <ReadStyledCell read={cell.original.isRead} >{cell.value}</ReadStyledCell>
+          },
+          {
+            id: 'createTimestamp',
+            accessor: 'createTimestamp',
+            Header: 'Time',
+            Cell: cell => (<LocalTimestamp iso8601Date={cell.value} />)
           }
         ]
       }
+      showPageJump={false}
     />
   </div>
 );
