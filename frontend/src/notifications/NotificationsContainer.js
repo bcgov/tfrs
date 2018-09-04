51346da4ca18ab0e798d985125b7cd048de7e30a
Richard Tan <richard.tan@gov.bc.ca>
2018-09-04 14:11:42 -0700

    Preparing the reducers for integration

--- frontend/src/notifications/NotificationsContainer.js
+++ frontend/src/notifications/NotificationsContainer.js
@@ -3,34 +3,31 @@
  * All data handling & manipulation should be handled here.
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
 
+import { changeNotificationReadStatus } from '../actions/notificationActions';
 import NotificationsDetails from './components/NotificationsDetails';
 
-class NotificationsContainer extends Component {
-  componentWillMount () {
-    this.loadData();
-  }
-
-  loadData () {
-  }
-
-  render () {
-    return (
-      <NotificationsDetails />
-    );
-  }
-}
+const NotificationsContainer = () => (
+  <NotificationsDetails
+    items={this.props.notifications}
+    isFetching={this.props.isFetching}
+    changeReadStatus={this.props.changeReadStatus}
+  />
+);
 
 NotificationsContainer.propTypes = {
 };
 
 const mapStateToProps = state => ({
+  isFetching: state.rootReducer.notificationsReducer.isFetching,
+  notifications: state.rootReducer.notificationsReducer.notifications
 });
 
 const mapDispatchToProps = dispatch => ({
+  changeReadStatus: bindActionCreators(changeNotificationReadStatus, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NotificationsContainer);
--- frontend/src/notifications/components/NotificationsDetails.js
+++ frontend/src/notifications/components/NotificationsDetails.js
@@ -2,18 +2,22 @@
  * Presentational component
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import NotificationsTable from './NotificationsTable';
-import NOTIFICATIONS from '../../constants/notifications';
 
 const NotificationsDetails = props => (
   <div className="page_notifications">
     <h1>Notifications</h1>
 
-    <NotificationsTable items={NOTIFICATIONS} />
+    <NotificationsTable items={props.items} />
   </div>
 );
 
 NotificationsDetails.propTypes = {
+  changeReadStatus: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({
+  })).isRequired
 };
 
 export default NotificationsDetails;
