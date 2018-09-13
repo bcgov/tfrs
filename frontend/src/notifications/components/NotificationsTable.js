be53947896d98ed4df33eed48d9a24a202e80836
Richard <richard.mizuno@gmail.com>
2018-09-13 09:31:50 -0700

    Notifications integration (#587)
    Connected the Back-end code to the Front-end
    Changelog:
    Notification page should now have the checkboxes working
    Notification icon should now work
    Added Button with dropdown
    Subscription should now load the statuses of the checkboxes based on the user's preference
    Saving should work and should have an alert

--- frontend/src/notifications/components/NotificationsTable.js
+++ frontend/src/notifications/components/NotificationsTable.js
@@ -4,54 +4,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTable from 'react-table';
+import { Link } from 'react-router-dom';
 import 'react-table/react-table.css';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import moment from 'moment';
+
+import CheckBox from '../../app/components/CheckBox';
+import NOTIFICATION_TYPES from '../../constants/notificationTypes';
+import CREDIT_TRANSACTIONS from '../../constants/routes/CreditTransactions';
 
 const NotificationsTable = (props) => {
   const columns = [{
     accessor: item => item.id,
     Cell: row => (
-      <FontAwesomeIcon icon={['far', 'square']} />
+      <CheckBox
+        addToFields={props.addToFields}
+        fields={props.fields.notifications}
+        id={row.value}
+        toggleCheck={props.toggleCheck}
+      />
     ),
     className: 'col-mark',
+    filterable: false,
     Header: 'Mark',
     id: 'mark',
     width: 50
   }, {
-    accessor: item => item.notification,
+    accessor: item => NOTIFICATION_TYPES[item.message],
+    Cell: (row) => {
+      const viewUrl = CREDIT_TRANSACTIONS.DETAILS.replace(':id', row.original.relatedCreditTrade);
+
+      return <Link to={viewUrl}>{row.value}</Link>;
+    },
     className: 'col-notification',
     Header: 'Notification',
     headerClassName: 'col-notification',
     id: 'notification'
   }, {
-    accessor: item => item.date,
+    accessor: item => (item.createTimestamp ? moment(item.createTimestamp).format('YYYY-MM-DD h:mm a') : '-'),
     className: 'col-date',
     Header: 'Date',
     headerClassName: 'col-date',
-    id: 'date'
+    id: 'date',
+    width: 150
   }, {
-    accessor: item => item.user,
+    accessor: item => `${item.user.firstName} ${item.user.lastName}`,
     className: 'col-user',
     Header: 'User',
     headerClassName: 'col-user',
-    id: 'user'
+    id: 'user',
+    width: 150
   }, {
-    accessor: item => item.creditTrade,
+    accessor: item => item.relatedCreditTrade,
+    Cell: (row) => {
+      const viewUrl = CREDIT_TRANSACTIONS.DETAILS.replace(':id', row.value);
+
+      return <Link to={viewUrl}>{row.value}</Link>;
+    },
     className: 'col-credit-trade',
-    Header: 'Credit Trade',
+    Header: 'Transaction ID',
     headerClassName: 'col-credit-trade',
-    id: 'creditTrade'
+    id: 'creditTrade',
+    width: 100
   }, {
-    accessor: item => item.organization,
+    accessor: item => item.relatedOrganization,
     className: 'col-organization',
     Header: 'Organization',
     headerClassName: 'col-organization',
-    id: 'organization'
+    id: 'organization',
+    width: 150
   }, {
     accessor: 'id',
     Cell: row => (
       <div className="col-actions">
-        <FontAwesomeIcon icon="folder-open" />
+        <FontAwesomeIcon
+          data-toggle="modal"
+          data-target="#confirmArchiveSingle"
+          icon="folder-open"
+          onClick={() => props.selectIdForModal(row.value)}
+        />
       </div>
     ),
     filterable: false,
@@ -64,20 +95,30 @@ const NotificationsTable = (props) => {
 
   return (
     <ReactTable
+      columns={columns}
       data={props.items}
-      defaultPageSize={5}
+      defaultPageSize={15}
       defaultSorted={[{
         id: 'date',
         desc: true
       }]}
       filterable={filterable}
-      columns={columns}
+      getTrProps={(state, rowInfo) => ({
+        className: (rowInfo && rowInfo.original.isRead) ? 'read' : 'unread'
+      })}
+      pageSizeOptions={[5, 10, 15, 20, 25, 50, 100]}
     />
   );
 };
 
 NotificationsTable.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object).isRequired
+  addToFields: PropTypes.func.isRequired,
+  fields: PropTypes.shape({
+    notifications: PropTypes.array
+  }).isRequired,
+  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  selectIdForModal: PropTypes.func.isRequired,
+  toggleCheck: PropTypes.func.isRequired
 };
 
 export default NotificationsTable;
