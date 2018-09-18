0bbe02f5cff9b39fe8944aa72b015db0b8ae5ed1
Richard <richard.mizuno@gmail.com>
2018-09-18 13:30:08 -0700

    Ux updates (#604)
    Various front-end changes
    Changelog:
    Credit Transactions -> credit transactions
    Removed timestamp from xls filename
    Remove progress bar from toastrs
    Changed some of the success messages for toastrs
    Minor text changes
    Fixed a bug with checkboxes in Notifications

---
+++ frontend/src/constants/organizationStatuses.js
@@ -0,0 +1,12 @@
+const ORGANIZATION_STATUSES = {
+  active: {
+    id: 1,
+    description: 'Active'
+  },
+  archived: {
+    id: 2,
+    description: 'Inactive'
+  }
+};
+
+export default ORGANIZATION_STATUSES;
