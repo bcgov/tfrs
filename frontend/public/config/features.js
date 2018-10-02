c2cfad6ec843bedb3dd8661e6bd77b9cf327d3b4
Robert Johnstone <rob@plasticviking.com>
2018-10-02 12:18:51 -0700

    Externalized Configuration

---
+++ frontend/public/config/features.js
@@ -0,0 +1,9 @@
+//defaults
+
+window.tfrs_config = {
+  "keycloak.enabled": false,
+  "keycloak.authority": "",
+  "keycloak.client_id": "tfrs-app",
+  "keycloak.callback_url": "",
+  "keycloak.post_logout_url": "",
+};
