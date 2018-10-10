6dface3d0ff0954081e04ffe582535e7e90500bd
Richard Tan <richard.tan@gov.bc.ca>
2018-10-10 14:07:52 -0700

    Minor code update

--- frontend/public/config/features.js
+++ frontend/public/config/features.js
@@ -1,9 +1,9 @@
-//defaults
+// defaults
 
 window.tfrs_config = {
-  "keycloak.enabled": false,
-  "keycloak.authority": "",
-  "keycloak.client_id": "tfrs-app",
-  "keycloak.callback_url": "",
-  "keycloak.post_logout_url": "",
+  'keycloak.enabled': false,
+  'keycloak.authority': 'http://localhost:8888/auth/realms/tfrs',
+  'keycloak.client_id': 'tfrs-app',
+  'keycloak.callback_url': 'http://localhost:5001/authCallback',
+  'keycloak.post_logout_url': 'http://localhost:5001/authCallback'
 };
