1b4a3c8af2f9f006598626c218c033f85f9b1f20
Richard Tan <richard.tan@gov.bc.ca>
2018-10-10 14:07:52 -0700

    Minor code update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,24 +1,21 @@
 /*
  Used to track feature configuration
 */
-
-let get_config = (value, def) =>{
+const getConfig = (value, def) => {
   if (global.tfrs_config) {
     return global.tfrs_config[value] || def;
   }
   return def;
 };
 
-
-let CONFIG = {
+const CONFIG = {
   KEYCLOAK: {
-    ENABLED: get_config("keycloak.enabled", false),
-    AUTHORITY: get_config("keycloak.authority","unconfigured"),
-    CLIENT_ID: get_config("keycloak.client_id", "unconfigured"),
-    CALLBACK_URL: get_config("keycloak.callback_url", "unconfigured"),
-    POST_LOGOUT_URL: get_config("keycloak.post_logout_url", "unconfigured")
+    ENABLED: getConfig('keycloak.enabled', false),
+    AUTHORITY: getConfig('keycloak.authority', 'unconfigured'),
+    CLIENT_ID: getConfig('keycloak.client_id', 'unconfigured'),
+    CALLBACK_URL: getConfig('keycloak.callback_url', 'unconfigured'),
+    POST_LOGOUT_URL: getConfig('keycloak.post_logout_url', 'unconfigured')
   }
 };
 
 export default CONFIG;
-
