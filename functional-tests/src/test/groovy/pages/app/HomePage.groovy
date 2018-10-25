7d4e2d7ca691ea494ae148a87601a3bdae0996f5
Nick Phura <Nick.Phura@quartech.com>
2018-10-25 13:59:02 -0700

    Add credit balance assertions to existing tests. Update credit amounts (to ensure all +/- combinations of credits are unique) Other trivial/minor updates.
    Add .env support for the build.gradle (for local testing against docker).
    Rename 'example.env' to '.env' to enable.

--- functional-tests/src/test/groovy/pages/app/HomePage.groovy
+++ functional-tests/src/test/groovy/pages/app/HomePage.groovy
@@ -1,5 +1,7 @@
 package pages
 
+import geb.waiting.WaitTimeoutException
+
 /**
  * The initial landing page the user is redirected to after logging in.
  */
@@ -8,5 +10,33 @@ class HomePage extends BaseAppPage {
   static url = '/'
   static content = {
     pageTitle { $('#main .page_credit_transactions h1') }
+
+    creditBalanceString { $('#main .credit_balance h3') }
+  }
+
+  /**
+   * Parse the credit balance string into an Integer containing only the number of credits.
+   *
+   * @return credit balance value
+   * @throws NumberFormatException if the parsed credit balance string fails to be cast to an Integer.
+   */
+  Integer getCreditBalance() {
+    creditBalanceString.text().replaceAll(/[^0-9]/, '').toInteger()
+  }
+
+  /**
+   * Compare the current credit balance amount against the provided expected amount.
+   *
+   * @param expectedBalance the expected credit balance to assert against.
+   * @return true if the credit balances match.
+   * @throws AssertionError if the credit balances fail to match.
+   */
+  Boolean compareCreditBalance(Integer expectedBalance) {
+    try {
+      getCreditBalance() == expectedBalance
+    } catch (WaitTimeoutException e) {
+      fail('The current credit balance does not match the expected credit balance.')
+    }
+    return true
   }
 }
--- functional-tests/src/test/groovy/specs/CreditTransferSpec.groovy
+++ functional-tests/src/test/groovy/specs/CreditTransferSpec.groovy
@@ -1,5 +1,6 @@
 package specs
 
+import pages.HomePage
 import pages.NewCreditTransferPage
 import pages.CreditTransactionsPage
 import pages.CreditTransactionsConfirmSubmitModal
@@ -14,6 +15,7 @@ import spock.lang.Timeout
 import spock.lang.Title
 import spock.lang.Narrative
 import spock.lang.Stepwise
+import spock.lang.Shared
 
 @Timeout(300)
 @Stepwise
@@ -21,14 +23,21 @@ import spock.lang.Stepwise
 @Narrative('''As a fuel supplier, I want to transfer credits to another fuel supplier.''')
 class CreditTransferSpec extends LoggedInSpec {
 
+  @Shared
+  Integer sendingFuelSupplier_initialCreditBalance
+
+  @Shared
+  Integer receivingFuelSupplier_initialCreditBalance
+
   void 'Log in as the sending fuel supplier and initiate a new credit transfer'() {
     given: 'I am logged in as the sending fuel supplier'
       logInAsSendingFuelSupplier()
-      def initialNotificationCount = headerModule.getNotificationCount()
+      Integer initialNotificationCount = headerModule.getNotificationCount()
+      sendingFuelSupplier_initialCreditBalance = getCreditBalance()
     and: 'I populate all required fields for a new credit transfer'
       to NewCreditTransferPage
-      setTransactionType('Buy')
-      setNumberOfCredits(80)
+      setTransactionType('Sell')
+      setNumberOfCredits(98)
       setRespondent(getReceivingFuelSupplier().org)
       setPricePerCredit(2)
       checkTerms()
@@ -39,15 +48,20 @@ class CreditTransferSpec extends LoggedInSpec {
       submitCreditTransaction()
     then: 'The credit transfer is initiated and I am returned to the Credit Transactions page'
       at CreditTransactionsPage
-    and: 'My unread notification count has increased by 1'
+    and: 'I am shown a success toast popup'
       at new ToastModal('Success!', 'Credit Transfer Proposal sent.')
+    and: 'My unread notification count has increased by 1'
       headerModule.compareNotificationCounts(initialNotificationCount+1)
+    and: 'My credit balance has not changed'
+      page(HomePage)
+      compareCreditBalance(sendingFuelSupplier_initialCreditBalance)
   }
 
   void 'Log in as the receiving fuel supplier and accept the credit transfer'() {
     given: 'I am logged in as the receiving fuel supplier'
       logInAsReceivingFuelSupplier()
-      def initialNotificationCount = headerModule.getNotificationCount()
+      Integer initialNotificationCount = headerModule.getNotificationCount()
+      receivingFuelSupplier_initialCreditBalance = getCreditBalance()
     and: 'I populate all required fields to accept the proposed credit transfer'
       to NotificationsPage
       getCreditTransferLinkByText('Credit Transfer Proposal Signed 1/2').click()
@@ -60,15 +74,19 @@ class CreditTransferSpec extends LoggedInSpec {
       acceptCreditTransaction()
     then: 'The credit transfer is accepted and I am returned to the Credit Transactions page'
       at CreditTransactionsPage
-    and: 'My unread notification count has not increased'
+    and: 'I am shown a success toast popup'
       at new ToastModal('Success!', 'Credit Transfer Proposal accepted.')
+    and: 'My unread notification count has not increased'
       headerModule.compareNotificationCounts(initialNotificationCount)
+    and: 'My credit balance has not changed'
+      page(HomePage)
+      compareCreditBalance(sendingFuelSupplier_initialCreditBalance)
   }
 
   void 'Log in as an analyst and recommend the credit transfer'() {
     given: 'I am logged in as an Analyst'
       logInAsAnalyst()
-      def initialNotificationCount = headerModule.getNotificationCount()
+      Integer initialNotificationCount = headerModule.getNotificationCount()
     and: 'I populate all required fields to recommend the accepted credit transfer'
       to NotificationsPage
       getCreditTransferLinkByText('Credit Transfer Proposal Signed 2/2').click()
@@ -81,15 +99,16 @@ class CreditTransferSpec extends LoggedInSpec {
       recommendCreditTransaction()
     then: 'The credit transfer is recommended and I am returned to the Credit Transactions page'
       at CreditTransactionsPage
-    and: 'My unread notification count has not increased'
+    and: 'I am shown a success toast popup'
       at new ToastModal('Success!', 'Credit Transfer Proposal recommended.')
+    and: 'My unread notification count has not increased'
       headerModule.compareNotificationCounts(initialNotificationCount)
   }
 
   void 'Log in as a Director and approve the credit transfer'() {
     given: 'I am logged in as a Director'
       logInAsDirector()
-      def initialNotificationCount = headerModule.getNotificationCount()
+      Integer initialNotificationCount = headerModule.getNotificationCount()
     and: 'I populate all required fields to approve the recommended credit transfer'
       to NotificationsPage
       getCreditTransferLinkByText('Credit Transfer Proposal Recommended For Approval').click()
@@ -102,8 +121,25 @@ class CreditTransferSpec extends LoggedInSpec {
       approveCreditTransaction()
     then: 'The credit transfer is approved and I am returned to the Credit Transactions page'
       at CreditTransactionsPage
-    and: 'My unread notification count has not increased'
+    and: 'I am shown a success toast popup'
       at new ToastModal('Success!', 'Credit Transfer Proposal approved.')
+    and: 'My unread notification count has not increased'
       headerModule.compareNotificationCounts(initialNotificationCount)
+  }
+
+  void 'Log in as the sending fuel supplier and verify my credit balance has decreased'() {
+    given: 'I am logged in as the sending fuel supplier'
+      logInAsSendingFuelSupplier()
+    when: 'I have previously successfully transferred credits to another fuel supplier'
+    then: 'My credit balance is decreased by the amount transferred'
+      compareCreditBalance(sendingFuelSupplier_initialCreditBalance - 98)
+  }
+
+  void 'Log in as the receiving fuel supplier and verify my credit balance has increased'() {
+    given: 'I am logged in as the receiving fuel supplier'
+      logInAsReceivingFuelSupplier()
+    when: 'I have previously successfully been transferred credits from another fuel supplier'
+    then: 'My credit balance is increased by the amount transferred'
+      compareCreditBalance(receivingFuelSupplier_initialCreditBalance + 98)
   }
 }
