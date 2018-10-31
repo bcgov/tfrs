b3b0e00f2d4b83566c5aaa65d9d0cc30a986cc6b
Robert Johnstone <rob@plasticviking.com>
2018-10-31 15:34:52 -0700

    user creation complete

--- backend/api/viewsets/CreditTrade.py
+++ backend/api/viewsets/CreditTrade.py
@@ -98,7 +98,7 @@ class CreditTradeViewSet(AuditableMixin, mixins.CreateModelMixin,
         digest = hashlib.sha256()
         # you could use anything here (like perhaps the PID or startup time
         digest.update(b'salt')
-        digest.update(request.user.id)
+        digest.update('user {}'.format(self.request.user.username).encode('utf-8'))
         digest.update(most_recent_updated_credit_trade.update_timestamp
                       .isoformat()
                       .encode('utf-8')
