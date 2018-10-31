532f39f52151f921830147183882350ea3340dd3
Nick Phura <Nick.Phura@quartech.com>
2018-10-30 17:40:04 -0700

    1005 - BDD User Add/Edit tests.

---
+++ functional-tests/src/test/groovy/pages/app/UserProfilePage.groovy
@@ -0,0 +1,30 @@
+package pages
+
+class UserProfilePage extends BaseAppPage {
+  static at = {
+    isReactReady() &&
+    // Match urls for regular and admin users profile pages.
+    browser.getCurrentUrl() =~ /(admin)?\/users\/(view|view_by_username)\/(user)?[0-9]+$/ &&
+    pageTitle.text() == expectedPageTitle
+  }
+  static content = {
+    pageTitle { $('#main .page_user h1') }
+
+    editButton { $('#edit-user')}
+  }
+
+  private final String expectedPageTitle
+
+  /**
+   * Constructor.
+   *
+   * @param String expectedPageTitle the expected page title.
+   */
+  UserProfilePage(String expectedPageTitle) {
+    this.expectedPageTitle = expectedPageTitle
+  }
+
+  void clickEditButton() {
+    editButton.click()
+  }
+}
