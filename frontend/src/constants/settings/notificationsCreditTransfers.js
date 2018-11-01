e26a61824f2f661d930769cf685116ef02d83645
Richard <richard.mizuno@gmail.com>
2018-11-01 12:14:56 -0700

    Clickable rows (#714)
    Made the rows clickable for Credit Transactions, User and User Activity
    
    Changelog:
    
    Changed zero reason label
    Changed file name for Download as .xls
    Changed the text in Settings
    Notification Labels have now PVR specific messages
    Credit Transactions table rows are now clickable
    User Table rows are now clickable
    User Activity rows are now clickable
    Various Fixes

--- frontend/src/constants/settings/notificationsCreditTransfers.js
+++ frontend/src/constants/settings/notificationsCreditTransfers.js
@@ -1,59 +1,59 @@
 const CREDIT_TRANSFER_NOTIFICATIONS = [{
   id: 1,
   code: 'CREDIT_TRANSFER_CREATED',
-  description: 'Save Draft',
+  description: 'Draft Saved',
   key: 'draft',
   recipients: ['fuel_supplier']
 }, {
   id: 2,
   code: 'CREDIT_TRANSFER_SIGNED_1OF2',
-  description: 'Sign 1/2',
+  description: 'Signed 1/2',
   key: 'submitted',
   recipients: ['fuel_supplier']
 }, {
   id: 10,
   code: 'CREDIT_TRANSFER_PROPOSAL_REFUSED',
-  description: 'Refuse',
+  description: 'Refused',
   key: 'refused',
   recipients: ['fuel_supplier']
 }, {
   id: 3,
   code: 'CREDIT_TRANSFER_SIGNED_2OF2',
-  description: 'Sign 2/2',
+  description: 'Signed 2/2',
   key: 'accepted',
   recipients: ['fuel_supplier', 'government']
 }, {
   id: 4,
   code: 'CREDIT_TRANSFER_RECOMMENDED_FOR_APPROVAL',
-  description: 'Recommend Approve',
+  description: 'Recommend Director Approval',
   key: 'recommended',
   permission: 'RECOMMEND_CREDIT_TRANSFER',
   recipients: ['government']
 }, {
   id: 5,
   code: 'CREDIT_TRANSFER_RECOMMENDED_FOR_DECLINATION',
-  description: 'Recommend Decline',
+  description: 'Recommend Director Approval Decline',
   key: 'not_recommended',
   permission: 'RECOMMEND_CREDIT_TRANSFER',
   recipients: ['government']
 }, {
   id: 7,
   code: 'CREDIT_TRANSFER_APPROVED',
-  description: 'Approve',
+  description: 'Director Approval',
   key: 'approved',
   permission: 'APPROVE_CREDIT_TRANSFER',
   recipients: ['fuel_supplier', 'government']
 }, {
   id: 9,
   code: 'CREDIT_TRANSFER_DECLINED',
-  description: 'Decline',
+  description: 'Director Declined to Approve',
   key: 'declined',
   permission: 'DECLINE_CREDIT_TRANSFER',
   recipients: ['fuel_supplier', 'government']
 }, {
   id: 11,
   code: 'CREDIT_TRANSFER_RESCINDED',
-  description: 'Rescind',
+  description: 'Rescinded',
   key: 'rescinded',
   permission: 'RESCIND_CREDIT_TRANSFER',
   recipients: ['fuel_supplier', 'government']
