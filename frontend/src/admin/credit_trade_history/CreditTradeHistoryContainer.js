ce5e857ee10a84597a801f0728c83f41e31c365f
Rob Johnstone <rob@plasticviking.com>
2018-11-05 11:39:56 -0800

    complete

--- frontend/src/admin/credit_trade_history/CreditTradeHistoryContainer.js
+++ frontend/src/admin/credit_trade_history/CreditTradeHistoryContainer.js
@@ -5,57 +5,23 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import PropTypes from 'prop-types';
 
-import getCreditTransfersHistory from '../../actions/creditTransferHistoryActions';
 import CreditTradeHistoryPage from './components/CreditTradeHistoryPage';
 import AdminTabs from '../components/AdminTabs';
 
 class CreditTradeHistoryContainer extends Component {
-  componentWillMount () {
-    this.loadData();
-  }
-
-  loadData () {
-    this.props.getCreditTransfersHistory();
-  }
-
   render () {
     return ([
       <AdminTabs active="user-activity" key="nav" />,
       <CreditTradeHistoryPage
-        data={this.props.historicalData}
         key="page"
       />
     ]);
   }
 }
 
-CreditTradeHistoryContainer.defaultProps = {
-  historicalData: {
-    isFetching: true,
-    items: []
-  }
-};
+const mapStateToProps = state => ({});
 
-CreditTradeHistoryContainer.propTypes = {
-  getCreditTransfersHistory: PropTypes.func.isRequired,
-  historicalData: PropTypes.shape({
-    isFetching: PropTypes.bool,
-    items: PropTypes.arrayOf(PropTypes.shape())
-  })
-};
-
-const mapStateToProps = state => ({
-  historicalData: {
-    items: state.rootReducer.creditTransfersHistory.items,
-    isFetching: state.rootReducer.creditTransfersHistory.isFetching
-  }
-});
-
-const mapDispatchToProps = dispatch => ({
-  getCreditTransfersHistory: bindActionCreators(getCreditTransfersHistory, dispatch)
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreditTradeHistoryContainer);
