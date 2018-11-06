0d739aa61c6c51288c6e87765997ab5588df0850
Rob Johnstone <rob@plasticviking.com>
2018-11-06 13:28:02 -0800

    Credit Trade History and User Activity Server-Side Pagination (#723)
    Added server-side pagination for some large tables to reduce load times
    Moved the state of the table into the relevant react components rather than the store, since the maintenance of this state is local to the corresponding component.

--- backend/api/viewsets/CreditTradeHistory.py
+++ backend/api/viewsets/CreditTradeHistory.py
@@ -20,13 +20,14 @@
     limitations under the License.
 """
 from rest_framework import filters, mixins, viewsets
-
-from auditable.views import AuditableMixin
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
 from api.models.CreditTradeHistory import CreditTradeHistory
 from api.permissions.CreditTradeHistory import CreditTradeHistoryPermissions
 from api.serializers import CreditTradeHistorySerializer, \
     CreditTradeHistoryReviewedSerializer
+from auditable.views import AuditableMixin
 
 
 class CreditTradeHistoryViewSet(AuditableMixin, mixins.ListModelMixin,
@@ -45,6 +46,16 @@ class CreditTradeHistoryViewSet(AuditableMixin, mixins.ListModelMixin,
         'list': CreditTradeHistoryReviewedSerializer
     }
 
+    column_sort_mappings = {
+        'updateTimestamp': 'credit_trade_update_time',
+        'creditTradeId': 'id',
+        'creditType': 'type__the_type',
+        'action': 'status__status',
+        'initiator': 'credit_trade__initiator__name',
+        'respondent': 'respondent__name',
+        'user': 'user__display_name'
+    }
+
     def get_serializer_class(self):
         if self.action in list(self.serializer_classes.keys()):
             return self.serializer_classes[self.action]
@@ -60,3 +71,50 @@ class CreditTradeHistoryViewSet(AuditableMixin, mixins.ListModelMixin,
         return CreditTradeHistory.objects.filter(
             user__organization_id=user.organization_id
         )
+
+    def list(self, request, **kwargs):
+        """
+        Function to get the user's activity.
+        This should be restricted based on the user's roles.
+        A government user won't see draft, submitted, refused.
+        A regular user won't see recommended and not recommended.
+        Regular users will only see histories related to their organization
+        """
+
+        limit = None
+        offset = None
+        sort_by = 'credit_trade_update_time'
+        sort_direction = '-'
+
+        if 'limit' in request.GET:
+            limit = int(request.GET['limit'])
+
+        if 'offset' in request.GET:
+            offset = int(request.GET['offset'])
+
+        if 'sort_by' in request.GET:
+            sort_by = self.column_sort_mappings[request.GET['sort_by']]
+
+        if 'sort_direction' in request.GET:
+            sort_direction = request.GET['sort_direction']
+
+        history = self.get_queryset()
+
+        history = history.order_by('{sort_direction}{sort_by}'
+                                   .format(sort_direction=sort_direction,
+                                           sort_by=sort_by))
+        total = history.count()
+
+        headers = {
+            'X-Total-Count': '{}'.format(total)
+        }
+
+        if limit is not None and offset is not None:
+            history = history[offset:offset + limit]
+
+        serializer = self.get_serializer(history,
+                                         read_only=True,
+                                         many=True)
+
+        return Response(headers=headers,
+                        data=serializer.data)
--- backend/api/viewsets/User.py
+++ backend/api/viewsets/User.py
@@ -1,13 +1,12 @@
-from rest_framework import mixins, viewsets, permissions
+from rest_framework import mixins, viewsets
 from rest_framework.decorators import list_route, detail_route
 from rest_framework.response import Response
 
-from api.decorators import permission_required
+from api.decorators import permission_required, exceptions
 from api.models.User import User
 from api.permissions.User import UserPermissions
-from api.serializers import UserSerializer, UserViewSerializer, UserUpdateSerializer
+from api.serializers import UserSerializer, UserViewSerializer, UserUpdateSerializer, CreditTradeHistoryMinSerializer, Q
 from api.serializers.UserCreationRequestSerializer import UserCreationRequestSerializer
-
 from auditable.views import AuditableMixin
 
 
@@ -30,11 +29,86 @@ class UserViewSet(AuditableMixin, viewsets.GenericViewSet,
         'create': UserCreationRequestSerializer
     }
 
+    column_sort_mappings = {
+        'updateTimestamp': 'credit_trade_update_time',
+        'creditTradeId': 'id',
+        'creditType': 'type__the_type',
+        'action': 'status__status'
+    }
+
     def get_serializer_class(self):
         if self.action in list(self.serializer_classes.keys()):
             return self.serializer_classes[self.action]
 
         return self.serializer_classes['default']
+
+    @detail_route()
+    def history(self, request, pk=None):
+        """
+        Function to get the user's activity.
+        This should be restricted based on the user's roles.
+        A government user won't see draft, submitted, refused.
+        A regular user won't see recommended and not recommended.
+        Regular users will only see histories related to their organization
+        """
+        obj = User.objects.filter(id=pk).first()
+
+        limit = None
+        offset = None
+        sort_by = 'credit_trade_update_time'
+        sort_direction = '-'
+
+        if 'limit' in request.GET:
+            limit = int(request.GET['limit'])
+
+        if 'offset' in request.GET:
+            offset = int(request.GET['offset'])
+
+        if 'sort_by' in request.GET:
+            sort_by = self.column_sort_mappings[request.GET['sort_by']]
+
+        if 'sort_direction' in request.GET:
+            sort_direction = request.GET['sort_direction']
+
+        # if the user is not a government user we should limit what we show
+        # so no recommended/not recommended
+        if not request.user.is_government_user:
+            if request.user.organization != obj.organization:
+                raise exceptions.PermissionDenied(
+                    'You do not have sufficient authorization to use this '
+                    'functionality.'
+                )
+
+            history = obj.get_history(
+                (Q(credit_trade__initiator_id=request.user.organization_id) |
+                 Q(credit_trade__respondent_id=request.user.organization_id)) &
+                (Q(status__status__in=[
+                    "Accepted", "Refused", "Submitted"
+                ]) | Q(is_rescinded=True)))
+        else:
+            history = obj.get_history(
+                Q(status__status__in=[
+                    "Accepted", "Approved", "Declined", "Not Recommended",
+                    "Recommended"
+                ]))
+
+        history = history.order_by('{sort_direction}{sort_by}'
+                                   .format(sort_direction=sort_direction,
+                                           sort_by=sort_by))
+        total = history.count()
+
+        headers = {
+            'X-Total-Count': '{}'.format(total)
+        }
+
+        if limit is not None and offset is not None:
+            history = history[offset:offset + limit]
+
+        serializer = CreditTradeHistoryMinSerializer(history, read_only=True,
+                                                     many=True)
+
+        return Response(headers=headers,
+                        data=serializer.data)
 
     @list_route()
     def current(self, request):
@@ -65,7 +139,6 @@ class UserViewSet(AuditableMixin, viewsets.GenericViewSet,
 
         serializer = self.get_serializer(result.first())
         return Response(serializer.data)
-
 
     @list_route()
     @permission_required('USER_MANAGEMENT')
