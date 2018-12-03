0b79c04efb115b0c73109027b1f7cb149f76663f
Rob Johnstone <rob@plasticviking.com>
2018-12-03 14:01:06 -0800

    Configurable debug and fixed authentication (#759)
    Enabling debug can now be done by setting debug.enabled in config/features.js.
    The login redirect is fixed
    Upgraded redux-oidc

--- frontend/src/app/SigninPage.js
+++ frontend/src/app/SigninPage.js
@@ -2,10 +2,6 @@ import React from 'react';
 import userManager from '../store/oidc-usermanager';
 
 class SigninPage extends React.Component {
-  componentWillMount () {
-    userManager.signinRedirect();
-  }
-
   render () {
     return (<p>redirecting</p>);
   }
