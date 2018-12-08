d2d9a7f655ac74c1bf7c3b8dc5c53e4f123c3edf
Richard <richard.mizuno@gmail.com>
2018-12-07 16:31:49 -0800

    Secure document upload (#757)
    Finished the containers for the front-end.
    Changelog:
    Added containers for Add, Edit, Detail and List
    Added reducers for the respective containers
    Front-end placeholder added and will be modified according to the models/backend

---
+++ frontend/src/actions/documentUploads.js
@@ -0,0 +1,158 @@
+import axios from 'axios';
+
+import ActionTypes from '../constants/actionTypes/DocumentUploads';
+import ReducerTypes from '../constants/reducerTypes/DocumentUploads';
+import * as Routes from '../constants/routes';
+
+const getDocumentUploads = () => (dispatch) => {
+  dispatch(getDocumentUploadRequests());
+  return axios.get(Routes.BASE_URL + Routes.SECURE_DOCUMENT_UPLOAD.LIST)
+    .then((response) => {
+      dispatch(getDocumentUploadRequestsSuccess(response.data));
+    }).catch((error) => {
+      dispatch(getDocumentUploadRequestsError(error.response));
+    });
+};
+
+const getDocumentUploadRequests = () => ({
+  name: ReducerTypes.GET_DOCUMENT_UPLOADS_REQUEST,
+  type: ActionTypes.GET_REQUESTS
+});
+
+const getDocumentUploadRequestsSuccess = requests => ({
+  name: ReducerTypes.RECEIVE_DOCUMENT_UPLOADS_REQUEST,
+  type: ActionTypes.RECEIVE_REQUESTS,
+  data: requests,
+  receivedAt: Date.now()
+});
+
+const getDocumentUploadRequestsError = error => ({
+  name: ReducerTypes.ERROR_DOCUMENT_UPLOADS_REQUEST,
+  type: ActionTypes.ERROR,
+  errorMessage: error
+});
+
+const getDocumentUpload = id => (dispatch) => {
+  dispatch(getDocumentUploadRequest());
+  return axios.get(`${Routes.BASE_URL}${Routes.SECURE_DOCUMENT_UPLOAD.API}/${id}`)
+    .then((response) => {
+      dispatch(getDocumentUploadRequestSuccess(response.data));
+    }).catch((error) => {
+      dispatch(getDocumentUploadRequestError(error.response));
+    });
+};
+
+const getDocumentUploadRequest = () => ({
+  name: ReducerTypes.GET_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.GET_REQUEST
+});
+
+const getDocumentUploadRequestSuccess = requests => ({
+  name: ReducerTypes.RECEIVE_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.RECEIVE_REQUEST,
+  data: requests,
+  receivedAt: Date.now()
+});
+
+const getDocumentUploadRequestError = error => ({
+  name: ReducerTypes.ERROR_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.ERROR,
+  errorMessage: error
+});
+
+const deleteDocumentUpload = id => (dispatch) => {
+  dispatch(deleteDocumentUploadRequest());
+
+  return axios
+    .delete(`${Routes.BASE_URL}${Routes.SECURE_DOCUMENT_UPLOAD.API}/${id}`)
+    .then((response) => {
+      dispatch(deleteDocumentUploadRequestSuccess(response.data));
+    }).catch((error) => {
+      dispatch(deleteDocumentUploadRequestError(error.response.data));
+    });
+};
+
+const deleteDocumentUploadRequest = () => ({
+  name: ReducerTypes.DELETE_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.REQUEST
+});
+
+const deleteDocumentUploadRequestSuccess = data => ({
+  name: ReducerTypes.SUCCESS_DELETE_DOCUMENT_UPLOAD,
+  type: ActionTypes.SUCCESS,
+  data
+});
+
+const deleteDocumentUploadRequestError = error => ({
+  name: ReducerTypes.ERROR_DELETE_DOCUMENT_UPLOAD,
+  type: ActionTypes.ERROR,
+  errorMessage: error
+});
+
+const addDocumentUpload = data => (dispatch) => {
+  dispatch(addDocumentUploadRequest());
+
+  return axios
+    .post(Routes.BASE_URL + Routes.SECURE_DOCUMENT_UPLOAD.API, data)
+    .then((response) => {
+      dispatch(addDocumentUploadSuccess(response.data));
+      return Promise.resolve(response);
+    }).catch((error) => {
+      dispatch(addDocumentUploadError(error.response.data));
+      return Promise.reject(error);
+    });
+};
+
+const addDocumentUploadRequest = () => ({
+  name: ReducerTypes.ADD_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.ADD_DOCUMENT_UPLOAD
+});
+
+const addDocumentUploadSuccess = data => ({
+  name: ReducerTypes.SUCCESS_ADD_DOCUMENT_UPLOAD,
+  type: ActionTypes.SUCCESS_ADD_DOCUMENT_UPLOAD,
+  data
+});
+
+const addDocumentUploadError = error => ({
+  name: ReducerTypes.ERROR_ADD_DOCUMENT_UPLOAD,
+  type: ActionTypes.ERROR,
+  errorMessage: error
+});
+
+const updateDocumentUpload = (data, id) => (dispatch) => {
+  dispatch(updateDocumentUploadRequest({ id, data }));
+
+  return axios.put(`${Routes.BASE_URL}${Routes.SECURE_DOCUMENT_UPLOAD.API}/${id}`, data)
+    .then((response) => {
+      dispatch(updateDocumentUploadSuccess(response.data));
+    }).catch((error) => {
+      dispatch(updateDocumentUploadError(error.response));
+    });
+};
+
+const updateDocumentUploadError = error => ({
+  errorMessage: error,
+  name: ReducerTypes.ERROR_UPDATE_DOCUMENT_UPLOAD,
+  type: ActionTypes.ERROR
+});
+
+const updateDocumentUploadRequest = payload => ({
+  name: ReducerTypes.UPDATE_DOCUMENT_UPLOAD_REQUEST,
+  type: ActionTypes.UPDATE_DOCUMENT_UPLOAD,
+  data: payload
+});
+
+const updateDocumentUploadSuccess = response => ({
+  data: response,
+  name: ReducerTypes.SUCCESS_UPDATE_DOCUMENT_UPLOAD,
+  type: ActionTypes.SUCCESS_UPDATE_DOCUMENT_UPLOAD
+});
+
+export {
+  addDocumentUpload,
+  deleteDocumentUpload,
+  getDocumentUpload,
+  getDocumentUploads,
+  updateDocumentUpload
+};
---
+++ frontend/src/secure_document_upload/components/CreditTransactionRequestDetails.js
@@ -0,0 +1,82 @@
+/*
+ * Presentational component
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Loading from '../../app/components/Loading';
+
+const CreditTransactionRequestDetails = props => (
+  <div className="credit-transaction-details">
+    {props.isFetching && <Loading />}
+    {!props.isFetching &&
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            <div className="col-12">
+              <label>
+                Compliance Period:
+                <span className="value"></span>
+              </label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12">
+              <label>
+                Milestone ID:
+                <span className="value"></span>
+              </label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12">
+              <label>
+                Agreement Name:
+                <span className="value"></span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <label>
+            Files:
+          </label>
+        </div>
+      </div>
+    }
+  </div>
+);
+
+CreditTransactionRequestDetails.defaultProps = {
+  errors: {},
+  id: 0,
+  status: {
+    id: 0,
+    status: ''
+  }
+};
+
+CreditTransactionRequestDetails.propTypes = {
+  changeStatus: PropTypes.func.isRequired,
+  errors: PropTypes.shape(),
+  loggedInUser: PropTypes.shape({
+    displayName: PropTypes.string,
+    hasPermission: PropTypes.func,
+    organization: PropTypes.shape({
+      name: PropTypes.string,
+      id: PropTypes.number
+    })
+  }).isRequired,
+  id: PropTypes.number,
+  isFetching: PropTypes.bool.isRequired,
+  status: PropTypes.shape({
+    id: PropTypes.number,
+    status: PropTypes.string
+  }),
+  toggleCheck: PropTypes.func.isRequired
+};
+
+export default CreditTransactionRequestDetails;
