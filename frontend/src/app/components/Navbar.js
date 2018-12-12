324b6171888bce14a58d437151efdf1a36cecd19
Richard <richard.mizuno@gmail.com>
2018-12-12 11:42:54 -0800

    Follow-up Fixes (#767)
    Found a couple of issues by the previous fix. So this is to fix those.
    
    Changelog:
    
    Fixed logout
    If a user has multiple records of the same role, it should no longer make the checkbox unclickable
    Removed validation from Phones as it's causing confusing behaviour

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -268,7 +268,7 @@ class Navbar extends Component {
             {CONFIG.KEYCLOAK.ENABLED &&
             <NavLink
               id="navbar-logout"
-              onClick={(e) => { e.preventDefault(); this.props.dispatch(signUserOut()); }}
+              onClick={(e) => { e.preventDefault(); this.props.signUserOut(); }}
               to={Routes.LOGOUT}
             >
               Log Out
@@ -364,7 +364,7 @@ class Navbar extends Component {
                         </MenuItem>
                         {CONFIG.KEYCLOAK.ENABLED &&
                         <MenuItem onClick={(e) => {
-                          e.preventDefault(); this.props.dispatch(signUserOut());
+                          e.preventDefault(); this.props.signUserOut();
                         }}
                         >
                           <FontAwesomeIcon icon="sign-out-alt" /> Log Out
@@ -416,11 +416,13 @@ Navbar.propTypes = {
       id: PropTypes.number
     }))
   }).isRequired,
+  signUserOut: PropTypes.func.isRequired,
   unreadNotificationsCount: PropTypes.number
 };
 
 const mapDispatchToProps = dispatch => ({
-  getNotifications: bindActionCreators(getNotifications, dispatch)
+  getNotifications: bindActionCreators(getNotifications, dispatch),
+  signUserOut: bindActionCreators(signUserOut, dispatch)
 });
 
 // export default Navbar;
