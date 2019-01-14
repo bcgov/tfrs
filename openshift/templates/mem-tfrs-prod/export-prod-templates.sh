284b61a47702869d77d22ffe95f97552cbf4b59b
Kuan Fan <kuan.fan@gov.bc.ca>
2019-01-14 14:56:00 -0800

    update openshift templates

--- openshift/templates/mem-tfrs-prod/export-prod-templates.sh
+++ openshift/templates/mem-tfrs-prod/export-prod-templates.sh
@@ -1,5 +1,5 @@
 oc export dc/postgresql service/postgresql --as-template='prod-postgresql' --output=json > prod-postgresql.json
 oc export service/schema-spy-public service/schema-spy-audit route/schema-spy-public route/schema-spy-audit dc/schema-spy-audit dc/schema-spy-public --as-template='prod-schemaspy' --output=json > prod-schemaspy.json
-oc export dc/client service/client route/main --as-template='prod-client' --output=json > prod-client.json
-oc export dc/tfrs service/backend route/api --as-template='prod-api' --output=json > prod-api.json
+oc export dc/client service/client route/lowcarbonfuels-frontend route/lowcarbonfuels-notification --as-template='prod-client' --output=json > prod-client.json
+oc export dc/tfrs service/backend route/lowcarbonfuels-backend --as-template='prod-api' --output=json > prod-api.json
 oc export service/request-logger route/sminfo --as-template='prod-request-logger' --output=json > prod-request-logger.json
