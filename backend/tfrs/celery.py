2fa49f7cce1efc6f82b01f2f8823893dfe2dfcf3
Rob Johnstone <rob@plasticviking.com>
2019-02-02 02:44:55 +0800

    Celery-based orphan cleanup (#870)
    * celery-based orphan cleanup
    * updated interval to 1 hour

---
+++ backend/tfrs/celery.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+from tfrs.settings import AMQP
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfrs.settings')
+
+app = Celery('tfrs', broker='amqp://{}:{}@{}:{}/{}'.format(
+    AMQP['USER'],
+    AMQP['PASSWORD'],
+    AMQP['HOST'],
+    AMQP['PORT'],
+    AMQP['VHOST']))
+
+app.conf.update({
+    'beat_scheduler': 'django_celery_beat.schedulers:DatabaseScheduler'
+})
+
+app.autodiscover_tasks()
+
+app.conf.update({
+    'beat_schedule': {
+        'minio-orphan-cleanup': {
+            'task': 'api.tasks.remove_orphans',
+            'schedule': 3600.0
+        }
+    }
+})
