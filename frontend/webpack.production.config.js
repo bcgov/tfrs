4d6547a3456a31f7700f2844664ed29d45b6e0e9
Richard Tan <richard.tan@gov.bc.ca>
2019-02-07 07:25:55 -0800

    Creating an unbundled js

--- frontend/webpack.production.config.js
+++ frontend/webpack.production.config.js
@@ -7,12 +7,19 @@ const buildPath = path.resolve(__dirname, 'public', 'build');
 const mainPath = path.resolve(__dirname, 'src', 'index.js');
 const tokenRenewalPath = path.resolve(__dirname, 'src', 'tokenRenewal.js');
 
+console.log('using dev');
+
 const config = {
-  mode: 'production',
+  // Makes sure errors in console map to the correct file
+  // and line numbe
+  // devtool: 'eval',
+  mode: 'development',
   entry: {
     bundle: [
       // Polyfill for Object.assign on IE11, etc
       'babel-polyfill',
+
+      // Our application
       mainPath
     ],
     tokenRenewal: [
@@ -20,39 +27,62 @@ const config = {
       tokenRenewalPath
     ]
   },
+
   output: {
-    filename: "[name].js",
-    publicPath: '/build/',
-    path: buildPath
+    // We need to give Webpack a path. It does not actually need it,
+    // because files are kept in memory in webpack-dev-server, but an
+    // error will occur if nothing is specified. We use the buildPath
+    // as that points to where the files will eventually be bundled
+    // in production
+    path: buildPath,
+    filename: '[name].js',
+
+    // Everything related to Webpack should go through a build path,
+    // localhost:3000/build. That makes proxying easier to handle
+    publicPath: '/build/'
   },
   resolve: {
     extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
+      // I highly recommend using the babel-loader as it gives you
+      // ES6/7 syntax and JSX transpiling out of the box
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: [nodeModulesPath],
         query: {
-          presets: ['react', 'env']
+          presets: ['react', 'env'],
+          plugins: ['transform-object-rest-spread']
         }
       },
+
+      // Let us also add the style-loader and css-loader, which you can
+      // expand with less-loader etc
       {
         test: /\.scss$/,
         loaders: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        exclude: /node_modules/,
+        loader:
+          'style-loader!css-loader?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loader:
+          'style-loader!css-loader'
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: ['file-loader?name=./images/[name].[ext]']
+        loaders: ['file?name=[name].[ext]']
       },
       {
         test: /\.(otf|eot|svg|ttf|woff|woff2)$/i,
-        loader: 'file-loader?name=./fonts/[name].[ext]',
+        loader: 'file?name=./public/assets/fonts/[name].[ext]',
         query: {
           limit: 10000
         }
@@ -60,17 +90,13 @@ const config = {
     ]
   },
   devServer: {
-    historyApiFallback: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers':
-        'Origin, X-Requested-With, Content-Type, Accept'
-    }
+    historyApiFallback: true
   },
-  optimization: {
-    minimize: true
-  },
+  devtool: 'cheap-module-eval-source-map', // 'source-map', // debug
+  // We have to manually add the Hot Replacement plugin when running
+  // from Node
   plugins: [
+    new Webpack.HotModuleReplacementPlugin(),
     new Webpack.DefinePlugin({
       __LOGOUT_TEST_URL__: JSON.stringify('https://logontest.gov.bc.ca/clp-cgi/logoff.cgi'),
       __LOGOUT_URL__: JSON.stringify('https://logon.gov.bc.ca/clp-cgi/logoff.cgi'),
