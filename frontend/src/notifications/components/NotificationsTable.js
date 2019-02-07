be558f0ba8528820bf293583fca5bb923136fa4a
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-02-07 15:34:39 -0800

    Revert "Added caching and optimizations (#889)"
    This reverts commit 9faff8b72f8b8688b6ddf124b147347e9fcfe586.

--- frontend/src/notifications/components/NotificationsTable.js
+++ frontend/src/notifications/components/NotificationsTable.js
@@ -159,7 +159,6 @@ const NotificationsTable = (props) => {
         id: 'date',
         desc: true
       }]}
-      loading={props.isFetching}
       filterable={filterable}
       getTrProps={(state, rowInfo) => ({
         className: (rowInfo && rowInfo.original.isRead) ? 'read' : 'unread'
@@ -175,7 +174,6 @@ NotificationsTable.propTypes = {
     notifications: PropTypes.array
   }).isRequired,
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
-  isFetching: PropTypes.bool.isRequired,
   selectIdForModal: PropTypes.func.isRequired,
   toggleCheck: PropTypes.func.isRequired,
   updateNotification: PropTypes.func.isRequired
