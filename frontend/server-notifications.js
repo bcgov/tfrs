3cf6fbbade21abd01df5e707bb66aa8655300569
Robert Johnstone <rob@plasticviking.com>
2019-02-28 11:08:12 -0800

    Per-User Socket.IO channels (#930)
    This card addresses a long-standing technical debt issue. Previously, there was a single global socket.io channel and when notifications were triggered, a reload notice would distributed to all connected users.
    This PR introduces authentication to the socket.io channel, including full JWT verification
    notifications are now sent to specific user channels
    Now that this channel is secure, it could easily be extended to include things like, for example, clamav scan results in realtime
    The docker environment is suitably updated.
    IMPORTANT the notifications-server pod will need KEYCLOAK_CERTS_URL environment defined (to match Django)

--- frontend/server-notifications.js
+++ frontend/server-notifications.js
@@ -10,9 +10,4 @@ const io = require('socket.io')(httpServer);
 
 httpServer.listen(3000);
 
-io.on('connect', (socket) => {
-  socket.join('global');
-  socket.emit('action', { type: 'SERVER_INITIATED_NOTIFICATION_RELOAD', message: 'connected' });
-});
-
-notifications.connect(io);
+notifications.setup(io);
