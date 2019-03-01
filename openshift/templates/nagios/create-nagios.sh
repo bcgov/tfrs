a91c1b468c8a8970a73d9bc484dcbd03ebce567e
Kuan Fan <kuan.fan@gov.bc.ca>
2019-03-01 13:40:20 -0800

    update

--- openshift/templates/nagios/create-nagios.sh
+++ openshift/templates/nagios/create-nagios.sh
@@ -1,3 +1,3 @@
 oc process -f ./nagios-base-bc.json | oc create -f -
 oc process -f ./nagios-bc.json | oc create -f -
-oc process -f ./nagios-dc.json
+oc process -f ./nagios-dc.json ENV_NAME=dev IS_NAME=nagios | oc create -f -
