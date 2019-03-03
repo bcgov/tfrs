17f67d5c9d624352c42adf982ec35fd8b73522a4
Robert Johnstone <rob@plasticviking.com>
2019-03-02 18:47:36 -0800

    Allow SFS recall as draft when in security scan failed state

--- backend/api/services/DocumentService.py
+++ backend/api/services/DocumentService.py
@@ -122,7 +122,8 @@ class DocumentService(object):
                 next_status.status != "Draft":
             return False
 
-        if current_status.status in ["Security Scan Failed"]:
+        if current_status.status in ["Security Scan Failed"] and \
+                next_status.status != "Draft":
             return False
 
         if current_status.status not in [
