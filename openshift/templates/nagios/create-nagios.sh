b9c044438461dabaf80f71b411458038553b1051
Kuan Fan <kuan.fan@gov.bc.ca>
2019-03-05 11:23:21 -0800

    update

--- openshift/templates/nagios/create-nagios.sh
+++ openshift/templates/nagios/create-nagios.sh
@@ -1,3 +1,6 @@
 oc process -f ./nagios-base-bc.json | oc create -f -
 oc process -f ./nagios-bc.json | oc create -f -
 oc process -f ./nagios-dc.json ENV_NAME=dev IS_NAME=nagios | oc create -f -
+
+# last step to grant image-pull role to service account nagios
+oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-prod:nagios --namespace=mem-tfrs-tools
