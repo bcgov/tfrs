18f2762b88cae78ddf524925cb1f00531fd92951
Richard <richard.mizuno@gmail.com>
2019-03-06 15:49:46 -0800

    Should now allow multiple accounts to use the same email (#945)
    Changed keycloak_email in user_creation_request to be non-unique
    Added external_username to user_creation_request
    New unique constraint is exernal_username + keycloak_email
    Changed some names and ids in the front-end
    Updated the security scan to handle test cases
    Updated tests
    Added external username field in the front-end

--- backend/api/migrations/0094_auto_20190305_2356.py
+++ backend/api/migrations/0094_auto_20190305_2356.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0092_initial_transport_modes_and_approved_fuels'),
+        ('api', '0093_initial_transport_modes_and_approved_fuels'),
     ]
 
     operations = [
