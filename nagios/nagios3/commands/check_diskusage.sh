d29d0139448b70cd90d1a152526783452092b832
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-04-17 11:51:04 -0700

    Update check_diskusage.sh

--- nagios/nagios3/commands/check_diskusage.sh
+++ nagios/nagios3/commands/check_diskusage.sh
@@ -11,14 +11,12 @@ backupDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAM
 backupDiskUsage=${backupDiskUsagePercent%?}
 diskusageAlarm=false
 if [ ${backupDiskUsage} -gt 70 ]; then
-        echo "The disk usage for /postgresql-backup is above 70%"
         diskusageAlarm=true
 fi
 
 databaseDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/var/lib/pgsql/data" | awk '{print $5}'`
 databaseDiskUsage=${databaseDiskUsagePercent%?}
 if [ ${databaseDiskUsage} -gt 70 ]; then
-        echo "The disk usage for /var/lib/pgsql/data is above 70%"
         diskusageAlarm=true
 fi
 if [ ${diskusageAlarm} = true ]; then
