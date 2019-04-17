5fdf0a7e433adcc933c5292484c0f1f3162d600e
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-04-17 11:06:39 -0700

    update the substitution method

--- nagios/nagios3/commands/check_diskusage.sh
+++ nagios/nagios3/commands/check_diskusage.sh
@@ -5,18 +5,18 @@ if ! (oc project -q $PROJECT_NAME > /dev/null); then
     exit 2
 fi
 
-databasePodName = `oc get pods -n $PROJECT_NAME | grep postgresql96 | awk '{print $1}'`
+databasePodName=`oc get pods -n $PROJECT_NAME | grep postgresql96 | awk '{print $1}'`
 
-backupDiskUsagePercent = `oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/postgresql-backup" | awk '{print $5}'`
-backupDiskUsage=${backupDiskUsagePercent::-1}
+backupDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/postgresql-backup" | awk '{print $5}'`
+backupDiskUsage=${backupDiskUsagePercent%?}
 diskusageAlarm=false
 if [ ${backupDiskUsage} -gt 70 ]; then
         echo "The disk usage for /postgresql-backup is above 70%"
         diskusageAlarm=true
 fi
 
-databaseDiskUsagePercent = `oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/var/lib/pgsql/data" | awk '{print $5}'`
-databaseDiskUsage=${databaseDiskUsagePercent::-1}
+databaseDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/var/lib/pgsql/data" | awk '{print $5}'`
+databaseDiskUsage=${databaseDiskUsagePercent%?}
 if [ ${databaseDiskUsage} -gt 70 ]; then
         echo "The disk usage for /var/lib/pgsql/data is above 70%"
         diskusageAlarm=true
@@ -24,4 +24,5 @@ fi
 if [ ${diskusageAlarm} = true ]; then
         exit 2
 fi
+echo "check_diskusage passed successfully"
 exit 0
