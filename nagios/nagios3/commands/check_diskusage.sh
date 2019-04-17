8dd2788164fe69933596d0bf8ddc3212b3721246
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-04-16 17:01:39 -0700

    Create check_diskusage.sh

---
+++ nagios/nagios3/commands/check_diskusage.sh
@@ -0,0 +1,27 @@
+PROJECT_NAME=$1
+
+if ! (oc project -q $PROJECT_NAME > /dev/null); then
+    echo "Could not select project $PROJECT_NAME"
+    exit 2
+fi
+
+databasePodName = `oc get pods -n $PROJECT_NAME | grep postgresql96 | awk '{print $1}'`
+
+backupDiskUsagePercent = `oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/postgresql-backup" | awk '{print $5}'`
+backupDiskUsage=${backupDiskUsagePercent::-1}
+diskusageAlarm=false
+if [ ${backupDiskUsage} -gt 70 ]; then
+        echo "The disk usage for /postgresql-backup is above 70%"
+        diskusageAlarm=true
+fi
+
+databaseDiskUsagePercent = `oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/var/lib/pgsql/data" | awk '{print $5}'`
+databaseDiskUsage=${databaseDiskUsagePercent::-1}
+if [ ${databaseDiskUsage} -gt 70 ]; then
+        echo "The disk usage for /var/lib/pgsql/data is above 70%"
+        diskusageAlarm=true
+fi
+if [ ${diskusageAlarm} = true ]; then
+        exit 2
+fi
+exit 0
