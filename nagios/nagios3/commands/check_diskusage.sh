d1d2d7d3bc802e5a493db8db3b73279629beadcc
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-04-23 09:47:06 -0700

    Add disk usage monitor to Nagios (#1104)
    new database disk usage monitor

---
+++ nagios/nagios3/commands/check_diskusage.sh
@@ -0,0 +1,27 @@
+PROJECT_NAME=$1
+
+if ! (oc project -q $PROJECT_NAME > /dev/null); then
+    echo "Could not select project $PROJECT_NAME"
+    exit 2
+fi
+
+databasePodName=`oc get pods -n $PROJECT_NAME | grep postgresql96 | awk '{print $1}'`
+
+backupDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/postgresql-backup" | awk '{print $5}'`
+backupDiskUsage=${backupDiskUsagePercent%?}
+diskusageAlarm=false
+if [ ${backupDiskUsage} -gt 70 ]; then
+        diskusageAlarm=true
+fi
+
+databaseDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/var/lib/pgsql/data" | awk '{print $5}'`
+databaseDiskUsage=${databaseDiskUsagePercent%?}
+if [ ${databaseDiskUsage} -gt 70 ]; then
+        diskusageAlarm=true
+fi
+if [ ${diskusageAlarm} = true ]; then
+        echo "CRITICAL - $1 disk usage checking failed"
+        exit 2
+fi
+echo "OK - $1 disk usage checking passed successfully"
+exit 0
