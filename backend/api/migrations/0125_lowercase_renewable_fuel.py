396d824866496c13cb9ceb988795289155c398b1
Richard <richard.mizuno@gmail.com>
2019-05-02 10:00:58 -0700

    Added a migration to set fuel to lowercase in Renewable Fuel (#1128)

---
+++ backend/api/migrations/0125_lowercase_renewable_fuel.py
@@ -0,0 +1,60 @@
+from django.db import migrations
+from django.db.migrations import RunPython
+
+
+def update_to_lowercase(apps, schema_editor):
+    """
+    Sets fuel to lowercase in Renewable Fuel in relation to ...
+    """
+    db_alias = schema_editor.connection.alias
+
+    category = apps.get_model('api', 'DefaultCarbonIntensityCategory')
+
+    category.objects.using(db_alias).filter(
+        name="Renewable Fuel in relation to diesel class fuel"
+    ).update(
+        name="Renewable fuel in relation to diesel class fuel"
+    )
+
+    category.objects.using(db_alias).filter(
+        name="Renewable Fuel in relation to gasoline class fuel"
+    ).update(
+        name="Renewable fuel in relation to gasoline class fuel"
+    )
+
+
+def update_to_uppercase(apps, schema_editor):
+    """
+    Sets fuel to uppercase in Renewable fuel in relation to ...
+    """
+    db_alias = schema_editor.connection.alias
+
+    category = apps.get_model('api', 'DefaultCarbonIntensityCategory')
+
+    category.objects.using(db_alias).filter(
+        name="Renewable fuel in relation to diesel class fuel"
+    ).update(
+        name="Renewable Fuel in relation to diesel class fuel"
+    )
+
+    category.objects.using(db_alias).filter(
+        name="Renewable fuel in relation to gasoline class fuel"
+    ).update(
+        name="Renewable Fuel in relation to gasoline class fuel"
+    )
+
+
+class Migration(migrations.Migration):
+    """
+    Attaches the functions for the migrations
+    """
+    dependencies = [
+        ('api', '0124_update_partially_renewable_fuels'),
+    ]
+
+    operations = [
+        RunPython(
+            update_to_lowercase,
+            update_to_uppercase
+        )
+    ]
