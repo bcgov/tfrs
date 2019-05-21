f458e6e20acad0285e98d3528ffd2cd04778a62f
Richard <richard.mizuno@gmail.com>
2019-05-21 11:40:55 -0700

    Schedule edit container (#1166)
    Added confirmation and alerts for deleting
    Moved some of the functionality to the button container
    Reduced duplication of some of the code
    Added confirmation for deleting
    Added toastr pop-up for deleting

--- frontend/src/compliance_reporting/components/ScheduleTabs.js
+++ frontend/src/compliance_reporting/components/ScheduleTabs.js
@@ -4,49 +4,71 @@ import { Link } from 'react-router-dom';
 
 import COMPLIANCE_REPORTING from '../../constants/routes/ComplianceReporting';
 
-const ScheduleTabs = props => (
-  <ul className="schedule-tabs nav nav-tabs" role="tablist">
-    <li
-      role="presentation"
-      className={`${(props.active === 'intro') && 'active'}`}
-    >
-      <Link id="navbar-administration" to={COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod)}>
-        Introduction
-      </Link>
-    </li>
-    <li
-      role="presentation"
-      className={`${(props.active === 'schedule-a') && 'active'}`}
-    >
-      <Link id="navbar-administration" to={COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod)}>
-        Schedule A
-      </Link>
-    </li>
-    <li
-      role="presentation"
-      className={`${(props.active === 'schedule-b') && 'active'}`}
-    >
-      <Link id="navbar-administration" to={COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod)}>
-        Schedule B
-      </Link>
-    </li>
-    <li
-      role="presentation"
-      className={`${(props.active === 'schedule-c') && 'active'}`}
-    >
-      <Link id="navbar-administration" to={COMPLIANCE_REPORTING.ADD_SCHEDULE_C.replace(':period?', props.compliancePeriod)}>
-        Schedule C
-      </Link>
-    </li>
-  </ul>
-);
+const ScheduleTabs = (props) => {
+  let urls = {
+    intro: COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod),
+    scheduleA: COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod),
+    scheduleB: COMPLIANCE_REPORTING.ADD.replace(':period?', props.compliancePeriod),
+    scheduleC: COMPLIANCE_REPORTING.ADD_SCHEDULE_C.replace(':period?', props.compliancePeriod)
+  };
+
+  if (props.edit) {
+    urls = {
+      intro: COMPLIANCE_REPORTING.EDIT.replace(':id', props.id),
+      scheduleA: COMPLIANCE_REPORTING.EDIT.replace(':id', props.id),
+      scheduleB: COMPLIANCE_REPORTING.EDIT.replace(':id', props.id),
+      scheduleC: COMPLIANCE_REPORTING.EDIT_SCHEDULE_C.replace(':id', props.id)
+    };
+  }
+
+  return (
+    <ul className="schedule-tabs nav nav-tabs" role="tablist">
+      <li
+        role="presentation"
+        className={`${(props.active === 'intro') && 'active'}`}
+      >
+        <Link id="navbar-administration" to={urls.intro}>
+          Introduction
+        </Link>
+      </li>
+      <li
+        role="presentation"
+        className={`${(props.active === 'schedule-a') && 'active'}`}
+      >
+        <Link id="navbar-administration" to={urls.scheduleA}>
+          Schedule A
+        </Link>
+      </li>
+      <li
+        role="presentation"
+        className={`${(props.active === 'schedule-b') && 'active'}`}
+      >
+        <Link id="navbar-administration" to={urls.scheduleB}>
+          Schedule B
+        </Link>
+      </li>
+      <li
+        role="presentation"
+        className={`${(props.active === 'schedule-c') && 'active'}`}
+      >
+        <Link id="navbar-administration" to={urls.scheduleC}>
+          Schedule C
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
 ScheduleTabs.defaultProps = {
+  compliancePeriod: null,
+  id: null
 };
 
 ScheduleTabs.propTypes = {
   active: PropTypes.string.isRequired,
-  compliancePeriod: PropTypes.string.isRequired
+  compliancePeriod: PropTypes.string,
+  edit: PropTypes.bool.isRequired,
+  id: PropTypes.string
 };
 
 export default ScheduleTabs;
