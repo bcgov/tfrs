7113163f5a1d84ef707165acdf7e67415912a9b9
Robert Johnstone <rob@plasticviking.com>
2019-06-19 15:50:34 -0300

    Schedule B Backend

--- backend/api/fixtures/development/dockerized.py
+++ backend/api/fixtures/development/dockerized.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from django.db import transaction
 
 from api.management.data_script import OperationalDataScript
+from api.models.ApprovedFuel import ApprovedFuel
+from api.models.ApprovedFuelProvision import ApprovedFuelProvision
+from api.models.CarbonIntensityDeterminationType import CarbonIntensityDeterminationType
 from api.models.CarbonIntensityLimit import CarbonIntensityLimit
 from api.models.CompliancePeriod import CompliancePeriod
 from api.models.DefaultCarbonIntensity import DefaultCarbonIntensity
@@ -16,6 +19,7 @@ from api.models.EnergyEffectivenessRatio import EnergyEffectivenessRatio
 from api.models.EnergyEffectivenessRatioCategory import EnergyEffectivenessRatioCategory
 from api.models.ExpectedUse import ExpectedUse
 from api.models.FuelClass import FuelClass
+from api.models.NotionalTransferType import NotionalTransferType
 from api.models.Organization import Organization
 from api.models.OrganizationActionsType import OrganizationActionsType
 from api.models.OrganizationBalance import OrganizationBalance
@@ -23,6 +27,7 @@ from api.models.OrganizationStatus import OrganizationStatus
 from api.models.OrganizationType import OrganizationType
 from api.models.PetroleumCarbonIntensity import PetroleumCarbonIntensity
 from api.models.PetroleumCarbonIntensityCategory import PetroleumCarbonIntensityCategory
+from api.models.ProvisionOfTheAct import ProvisionOfTheAct
 from api.models.Role import Role
 from api.models.User import User
 from api.models.UserRole import UserRole
@@ -470,6 +475,159 @@ class DockerEnvironment(OperationalDataScript):
             display_order="3",
             effective_date="2017-01-01"
         )
+
+        NotionalTransferType.objects.create(
+            the_type="Received",
+            display_order="1",
+            effective_date="2017-01-01"
+        )
+        NotionalTransferType.objects.create(
+            the_type="Transferred",
+            display_order="2",
+            effective_date="2017-01-01"
+        )
+
+        prescribed_carbon_intensity = \
+            CarbonIntensityDeterminationType.objects.create(
+                display_order="1",
+                effective_date="2017-01-01",
+                the_type="Carbon Intensity"
+            )
+
+        provision = ProvisionOfTheAct.objects.create(
+            description="Prescribed carbon intensity",
+            display_order="1",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (a)"
+        )
+        ApprovedFuelProvision.objects.create(
+            fuel=ApprovedFuel.objects.get(name="Petroleum-based gasoline"),
+            provision_act=provision,
+            determination_type=prescribed_carbon_intensity
+        )
+
+        provision = ProvisionOfTheAct.objects.create(
+            description="Prescribed carbon intensity",
+            display_order="2",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (b)"
+        )
+        ApprovedFuelProvision.objects.create(
+            fuel=ApprovedFuel.objects.get(name="Petroleum-based diesel"),
+            provision_act=provision,
+            determination_type=prescribed_carbon_intensity
+        )
+
+        # other fuel types
+        approved_fuel_code = \
+            CarbonIntensityDeterminationType.objects.create(
+                display_order="2",
+                effective_date="2017-01-01",
+                the_type="Fuel Code"
+            )
+
+        fuel_types = ApprovedFuel.objects.exclude(
+            name__in=["Petroleum-based diesel", "Petroleum-based gasoline"]
+        )
+
+        # Section 6 (5) (c)
+        provision = ProvisionOfTheAct.objects.create(
+            description="Approved fuel code",
+            display_order="3",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (c)"
+        )
+
+        obj = [
+            ApprovedFuelProvision(
+                fuel=fuel_type,
+                provision_act=provision,
+                determination_type=approved_fuel_code
+            ) for fuel_type in fuel_types
+        ]
+
+        ApprovedFuelProvision.objects.bulk_create(obj)
+
+        # Section 6 (5) (d) (i)
+        default_carbon_intensity = \
+            CarbonIntensityDeterminationType.objects.create(
+                display_order="3",
+                effective_date="2017-01-01",
+                the_type="Default Carbon Intensity"
+            )
+
+        provision = ProvisionOfTheAct.objects.create(
+            description="Default Carbon Intensity Value",
+            display_order="4",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (d) (i)"
+        )
+
+        obj = [
+            ApprovedFuelProvision(
+                fuel=fuel_type,
+                provision_act=provision,
+                determination_type=default_carbon_intensity
+            ) for fuel_type in fuel_types
+        ]
+
+        ApprovedFuelProvision.objects.bulk_create(obj)
+
+        # Section 6 (5) (d) (ii) (A)
+        gh_genius = \
+            CarbonIntensityDeterminationType.objects.create(
+                display_order="4",
+                effective_date="2017-01-01",
+                the_type="GHGenius"
+            )
+
+        provision = ProvisionOfTheAct.objects.create(
+            description="GHGenius modelled",
+            display_order="5",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (d) (ii) (A)"
+        )
+
+        obj = [
+            ApprovedFuelProvision(
+                fuel=fuel_type,
+                provision_act=provision,
+                determination_type=gh_genius
+            ) for fuel_type in fuel_types
+        ]
+
+        ApprovedFuelProvision.objects.bulk_create(obj)
+
+        # Section 6 (5) (d) (ii) (B)
+        alternative_method = \
+            CarbonIntensityDeterminationType.objects.create(
+                display_order="5",
+                effective_date="2017-01-01",
+                the_type="Alternative"
+            )
+
+        provision = ProvisionOfTheAct.objects.create(
+            description="Alternative Method",
+            display_order="6",
+            effective_date="2017-01-01",
+            expiration_date=None,
+            provision="Section 6 (5) (d) (ii) (B)"
+        )
+
+        obj = [
+            ApprovedFuelProvision(
+                fuel=fuel_type,
+                provision_act=provision,
+                determination_type=alternative_method
+            ) for fuel_type in fuel_types
+        ]
+
+        ApprovedFuelProvision.objects.bulk_create(obj)
 
 
 script_class = DockerEnvironment
--- backend/api/serializers/ComplianceReportSchedules.py
+++ backend/api/serializers/ComplianceReportSchedules.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
 
 from api.models.ApprovedFuel import ApprovedFuel
-from api.models.ComplianceReportSchedules import ScheduleC, ScheduleCRecord, ScheduleARecord, ScheduleA
+from api.models.ComplianceReportSchedules import ScheduleC, ScheduleCRecord, ScheduleARecord, ScheduleA, ScheduleBRecord, ScheduleB
 from api.models.ExpectedUse import ExpectedUse
 from api.models.FuelClass import FuelClass
+from api.models.FuelCode import FuelCode
 from api.models.NotionalTransferType import NotionalTransferType
+from api.models.ProvisionOfTheAct import ProvisionOfTheAct
 
 
 class ScheduleCRecordSerializer(serializers.ModelSerializer):
@@ -24,6 +26,26 @@ class ScheduleCDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ScheduleC
+        fields = ('records',)
+
+
+class ScheduleBRecordSerializer(serializers.ModelSerializer):
+
+    fuel_type = SlugRelatedField(slug_field='name', queryset=ApprovedFuel.objects.all())
+    fuel_class = SlugRelatedField(slug_field='fuel_class', queryset=FuelClass.objects.all())
+    provision_of_the_act = SlugRelatedField(slug_field='provision', queryset=ProvisionOfTheAct.objects.all())
+    fuel_code = PrimaryKeyRelatedField(queryset=FuelCode.objects.all(), required=False, allow_null=True)
+
+    class Meta:
+        model = ScheduleBRecord
+        fields = ('fuel_type', 'fuel_class', 'provision_of_the_act', 'quantity', 'fuel_code')
+
+
+class ScheduleBDetailSerializer(serializers.ModelSerializer):
+    records = ScheduleBRecordSerializer(many=True, required=False)
+
+    class Meta:
+        model = ScheduleB
         fields = ('records',)
 
 
