1c1978402e9eddfb3eeda54c1bdccf044f368bcc
Robert Johnstone <rob@plasticviking.com>
2019-06-27 20:21:04 -0300

    Schedule D Backend

--- backend/api/serializers/ComplianceReportSchedules.py
+++ backend/api/serializers/ComplianceReportSchedules.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
 
 from api.models.ApprovedFuel import ApprovedFuel
-from api.models.ComplianceReportSchedules import ScheduleC, ScheduleCRecord, ScheduleARecord, ScheduleA, ScheduleBRecord, ScheduleB
+from api.models.ComplianceReportSchedules import ScheduleC, ScheduleCRecord, ScheduleARecord, ScheduleA, \
+    ScheduleBRecord, ScheduleB, ScheduleD, ScheduleDSheet, ScheduleDSheetInput, ScheduleDSheetOutput
 from api.models.ExpectedUse import ExpectedUse
 from api.models.FuelClass import FuelClass
 from api.models.FuelCode import FuelCode
@@ -10,8 +11,45 @@ from api.models.NotionalTransferType import NotionalTransferType
 from api.models.ProvisionOfTheAct import ProvisionOfTheAct
 
 
-class ScheduleCRecordSerializer(serializers.ModelSerializer):
+class OutputCellSerializer(serializers.ChoiceField):
+    def field_to_native(self, obj, field_name):
+        return getattr(obj, 'get_' + field_name + '_display')()
 
+
+class ScheduleDSheetOutputSerializer(serializers.ModelSerializer):
+    description = OutputCellSerializer(choices=[e.value for e in ScheduleDSheetOutput.OutputCells])
+
+    class Meta:
+        model = ScheduleDSheetOutput
+        fields = ('intensity', 'description')
+
+
+class ScheduleDSheetInputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduleDSheetInput
+        fields = ('worksheet_name', 'cell', 'value', 'units', 'description')
+
+
+class ScheduleDSheetSerializer(serializers.ModelSerializer):
+    fuel_type = SlugRelatedField(slug_field='name', queryset=ApprovedFuel.objects.all())
+    fuel_class = SlugRelatedField(slug_field='fuel_class', queryset=FuelClass.objects.all())
+    inputs = ScheduleDSheetInputSerializer(many=True, required=True)
+    outputs = ScheduleDSheetOutputSerializer(many=True, required=True)
+
+    class Meta:
+        model = ScheduleDSheet
+        fields = ('fuel_type', 'fuel_class', 'feedstock', 'inputs', 'outputs')
+
+
+class ScheduleDDetailSerializer(serializers.ModelSerializer):
+    sheets = ScheduleDSheetSerializer(many=True, required=False)
+
+    class Meta:
+        model = ScheduleD
+        fields = ('sheets',)
+
+
+class ScheduleCRecordSerializer(serializers.ModelSerializer):
     fuel_type = SlugRelatedField(slug_field='name', queryset=ApprovedFuel.objects.all())
     fuel_class = SlugRelatedField(slug_field='fuel_class', queryset=FuelClass.objects.all())
     expected_use = SlugRelatedField(slug_field='description', queryset=ExpectedUse.objects.all())
@@ -30,7 +68,6 @@ class ScheduleCDetailSerializer(serializers.ModelSerializer):
 
 
 class ScheduleBRecordSerializer(serializers.ModelSerializer):
-
     fuel_type = SlugRelatedField(slug_field='name', queryset=ApprovedFuel.objects.all())
     fuel_class = SlugRelatedField(slug_field='fuel_class', queryset=FuelClass.objects.all())
     provision_of_the_act = SlugRelatedField(slug_field='provision', queryset=ProvisionOfTheAct.objects.all())
@@ -50,7 +87,6 @@ class ScheduleBDetailSerializer(serializers.ModelSerializer):
 
 
 class ScheduleARecordSerializer(serializers.ModelSerializer):
-
     transfer_type = SlugRelatedField(slug_field='the_type', queryset=NotionalTransferType.objects.all())
     fuel_class = SlugRelatedField(slug_field='fuel_class', queryset=FuelClass.objects.all())
 
