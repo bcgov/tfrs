4c8c775b1c94647a0b4b26e6fed31cc968c3e53f
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-06-28 15:21:30 -0700

    Devops pipeline split (#1278)
    * split pipeline
    * cleanup pipelines

---
+++ openshift/pipelines/scripts/prepareDCStages.groovy
@@ -0,0 +1,380 @@
+def backendDCStage (String envName) {
+    return {
+        stage("Apply Backend Deployment Config on ${envName}") {
+            timeout(300) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    def KEYCLOAK_SA_BASEURL
+                    def KEYCLOAK_SA_CLIENT_ID
+                    def KEYCLOAK_SA_REALM
+                    def KEYCLOAK_AUDIENCE
+                    def KEYCLOAK_CERTS_URL
+                    def KEYCLOAK_CLIENT_ID
+                    def KEYCLOAK_ISSUER
+                    def KEYCLOAK_REALM
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ENV_NAME = "dev"
+                        SOURCE_IS_NAME = 'tfrs-develop'
+                        KEYCLOAK_SA_BASEURL = "https://sso-dev.pathfinder.gov.bc.ca"
+                        KEYCLOAK_SA_CLIENT_ID = "tfrs-dev-django-sa"
+                        KEYCLOAK_SA_REALM = "tfrs-dev"
+                        KEYCLOAK_AUDIENCE = "tfrs-dev"
+                        KEYCLOAK_CERTS_URL = "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/tfrs-dev/protocol/openid-connect/certs"
+                        KEYCLOAK_CLIENT_ID = "tfrs-dev"
+                        KEYCLOAK_ISSUER = "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/tfrs-dev"
+                        KEYCLOAK_REALM = "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/tfrs-dev"
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ENV_NAME = "test"
+                        SOURCE_IS_NAME = 'tfrs'
+                        KEYCLOAK_SA_BASEURL = "https://sso-test.pathfinder.gov.bc.ca"
+                        KEYCLOAK_SA_CLIENT_ID = "tfrs-django-sa"
+                        KEYCLOAK_SA_REALM = "tfrs"
+                        KEYCLOAK_AUDIENCE = "tfrs"
+                        KEYCLOAK_CERTS_URL = "https://sso-test.pathfinder.gov.bc.ca/auth/realms/tfrs/protocol/openid-connect/certs"
+                        KEYCLOAK_CLIENT_ID = "tfrs"
+                        KEYCLOAK_ISSUER = "https://sso-test.pathfinder.gov.bc.ca/auth/realms/tfrs"
+                        KEYCLOAK_REALM = "https://sso-test.pathfinder.gov.bc.ca/auth/realms/tfrs"
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ENV_NAME = "prod"
+                        SOURCE_IS_NAME = 'tfrs'
+                        KEYCLOAK_SA_BASEURL = "https://sso.pathfinder.gov.bc.ca"
+                        KEYCLOAK_SA_CLIENT_ID = "tfrs-django-sa"
+                        KEYCLOAK_SA_REALM = "tfrs"
+                        KEYCLOAK_AUDIENCE = "tfrs"
+                        KEYCLOAK_CERTS_URL = "https://sso.pathfinder.gov.bc.ca/auth/realms/tfrs/protocol/openid-connect/certs"
+                        KEYCLOAK_CLIENT_ID = "tfrs"
+                        KEYCLOAK_ISSUER = "https://sso.pathfinder.gov.bc.ca/auth/realms/tfrs"
+                        KEYCLOAK_REALM = "https://sso.pathfinder.gov.bc.ca/auth/realms/tfrs"
+                    }
+                    openshift.withProject("${projectName}") {
+                        def backendDCJson = openshift.process(readFile(file:'openshift/templates/components/backend/tfrs-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}",
+                        "KEYCLOAK_SA_BASEURL=${KEYCLOAK_SA_BASEURL}",
+                        "KEYCLOAK_SA_CLIENT_ID=${KEYCLOAK_SA_CLIENT_ID}",
+                        "KEYCLOAK_SA_REALM=${KEYCLOAK_SA_REALM}",
+                        "KEYCLOAK_AUDIENCE=${KEYCLOAK_AUDIENCE}",
+                        "KEYCLOAK_CERTS_URL=${KEYCLOAK_CERTS_URL}",
+                        "KEYCLOAK_CLIENT_ID=${KEYCLOAK_CLIENT_ID}",
+                        "KEYCLOAK_ISSUER=${KEYCLOAK_ISSUER}",
+                        "KEYCLOAK_REALM=${KEYCLOAK_REALM}"
+                        )
+                        openshift.apply(backendDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def backendDCOthersStage (String envName) {
+    return {
+        stage("Apply Backend Others Config on ${envName}") {
+            timeout(300) {
+                script {
+                    def projectName
+                    def ROUTE_HOST_NAME
+                    def ROUTE_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ROUTE_HOST_NAME = "dev-lowcarbonfuels.pathfinder.gov.bc.ca"
+                        ROUTE_NAME = "dev-lowcarbonfuels-backend"
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ROUTE_HOST_NAME = "test-lowcarbonfuels.pathfinder.gov.bc.ca"
+                        ROUTE_NAME = "test-lowcarbonfuels-backend"
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ROUTE_HOST_NAME = "lowcarbonfuels.gov.bc.ca"
+                        ROUTE_NAME = "lowcarbonfuels-backend"
+                    }
+                    openshift.withProject("${projectName}") {
+                        def backendDCJson = openshift.process(readFile(file:'openshift/templates/components/backend/tfrs-dc-others.json'), 
+                        "-p", 
+                        "ROUTE_HOST_NAME=${ROUTE_HOST_NAME}}",
+                        "ROUTE_NAME=${ROUTE_NAME}"
+                        )
+                        openshift.apply(backendDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def celeryDCStage (String envName) {
+    return {
+        stage("Apply Celery Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ENV_NAME = 'dev'
+                        SOURCE_IS_NAME = 'celery-develop'
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ENV_NAME = 'test'
+                        SOURCE_IS_NAME = 'celery'
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ENV_NAME = 'prod'
+                        SOURCE_IS_NAME = 'celery'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def celeryDCJson = openshift.process(readFile(file:'openshift/templates/components/celery/celery-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}"
+                        )
+                        openshift.apply(celeryDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def scanCoordinatorDCStage (String envName) {
+    return {
+        stage('Apply Scan-coordinator Deployment Config on ${envName}') {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ENV_NAME = 'dev'
+                        SOURCE_IS_NAME = 'scan-coordinator-develop'
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ENV_NAME = 'test'
+                        SOURCE_IS_NAME = 'scan-coordinator'
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ENV_NAME = 'prod'
+                        SOURCE_IS_NAME = 'scan-coordinator'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def scanCoordinatorDCJson = openshift.process(readFile(file:'openshift/templates/components/scan-coordinator/scan-coordinator-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}"
+                        )
+                        openshift.apply(scanCoordinatorDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def scanHandlerDCStage (String envName) {
+    return {
+        stage("Apply Scan-handler Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ENV_NAME = 'dev'
+                        SOURCE_IS_NAME = 'scan-handler-develop'
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ENV_NAME = 'test'
+                        SOURCE_IS_NAME = 'scan-handler'
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ENV_NAME = 'prod'
+                        SOURCE_IS_NAME = 'scan-handler'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def scanHandlerDCJson = openshift.process(readFile(file:'openshift/templates/components/scan-handler/scan-handler-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}"
+                        )
+                        openshift.apply(scanHandlerDCJson)
+                    }
+                }
+            }
+        }        
+    }
+}
+
+def notificationServerDCStage (String envName) {
+    return {
+        stage("Apply Notificstion-server Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    def KEYCLOAK_CERTS_URL
+                    if(envName == 'dev') {
+                        projectName = 'mem-tfrs-dev'
+                        ENV_NAME = 'dev'
+                        SOURCE_IS_NAME = 'notification-server-develop'
+                        KEYCLOAK_CERTS_URL = 'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/tfrs-dev/protocol/openid-connect/certs'
+                    } else if(envName == 'test') {
+                        projectName = 'mem-tfrs-test'
+                        ENV_NAME = 'test'
+                        SOURCE_IS_NAME = 'notification-server'
+                        KEYCLOAK_CERTS_URL = 'https://sso-test.pathfinder.gov.bc.ca/auth/realms/tfrs/protocol/openid-connect/certs'
+                    } else if(envName == 'prod') {
+                        projectName = 'mem-tfrs-prod'
+                        ENV_NAME = 'prod'
+                        SOURCE_IS_NAME = 'notification-server'
+                        KEYCLOAK_CERTS_URL = 'https://sso.pathfinder.gov.bc.ca/auth/realms/tfrs/protocol/openid-connect/certs'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def notificationServerDCJson = openshift.process(readFile(file:'openshift/templates/components/notification/notification-server-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}",
+                        "KEYCLOAK_CERTS_URL=${KEYCLOAK_CERTS_URL}"
+                        )
+                        openshift.apply(notificationServerDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def notificationServerOthersDCStage (String envName) {
+    return {
+        stage("Apply Notificstion-server Others Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ROUTE_NAME
+                    def ROUTE_HOST
+                    if(envName == 'dev') {
+                        projectName = 'mem-tfrs-dev'
+                        ROUTE_NAME = 'dev-lowcarbonfuels-notification'
+                        ROUTE_HOST = 'dev-lowcarbonfuels.pathfinder.gov.bc.ca'
+                    } else if(envName == 'test') {
+                        projectName = 'mem-tfrs-test'
+                        ROUTE_NAME = 'dev-lowcarbonfuels-notification'
+                        ROUTE_HOST = 'dev-lowcarbonfuels.pathfinder.gov.bc.ca'
+                    } else if(envName == 'prod') {
+                        projectName = 'mem-tfrs-prod'
+                        ROUTE_NAME = 'dev-lowcarbonfuels-notification'
+                        ROUTE_HOST = 'dev-lowcarbonfuels.pathfinder.gov.bc.ca'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def notificationServerDCJson = openshift.process(readFile(file:'openshift/templates/components/notification/notification-server-others-dc.json'), 
+                        "-p", 
+                        "ROUTE_NAME=dev-lowcarbonfuels-notification",
+                        "ROUTE_HOST=dev-lowcarbonfuels.pathfinder.gov.bc.ca"
+                        )
+                        openshift.apply(notificationServerDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def frontendDCStage (String envName) {
+    return {
+        stage("Apply Client Deployment Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def ENV_NAME
+                    def SOURCE_IS_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        ENV_NAME = 'dev'
+                        SOURCE_IS_NAME = 'client-develop'
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        ENV_NAME = 'test'
+                        SOURCE_IS_NAME = 'client'
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        ENV_NAME = 'prod'
+                        SOURCE_IS_NAME = 'client'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def clientDCJson = openshift.process(readFile(file:'openshift/templates/components/frontend/client-dc.json'), 
+                        "-p", 
+                        "ENV_NAME=${ENV_NAME}", 
+                        "SOURCE_IS_NAME=${SOURCE_IS_NAME}"
+                        )
+                        openshift.apply(clientDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+
+def frontendDCOthersStage (String envName) {
+    return {
+        stage("Apply Client Others Config on ${envName}") {
+            timeout(30) {
+                script {
+                    def projectName
+                    def KEYCLOAK_AUTHORITY
+                    def KEYCLOAK_CLIENT_ID
+                    def KEYCLOAK_CALLBACK_URL
+                    def KEYCLOAK_LOGOUT_URL
+                    def ROUTE_HOST_NAME
+                    def ROUTE_NAME
+                    if(envName == 'dev') {
+                        projectName = "mem-tfrs-dev"
+                        KEYCLOAK_AUTHORITY = 'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/tfrs-dev'
+                        KEYCLOAK_CLIENT_ID = 'tfrs-dev'
+                        KEYCLOAK_CALLBACK_URL = 'https://dev-lowcarbonfuels.pathfinder.gov.bc.ca/authCallback'
+                        KEYCLOAK_LOGOUT_URL = 'https://logontest.gov.bc.ca/clp-cgi/logoff.cgi?returl=https%3A%2F%2Fdev-lowcarbonfuels.pathfinder.gov.bc.ca%2F'
+                        ROUTE_HOST_NAME = 'dev-lowcarbonfuels.pathfinder.gov.bc.ca'
+                        ROUTE_NAME = 'dev-lowcarbonfuels-frontend'
+                    } else if(envName == 'test') {
+                        projectName = "mem-tfrs-test"
+                        KEYCLOAK_AUTHORITY = 'https://sso-test.pathfinder.gov.bc.ca/auth/realms/tfrs'
+                        KEYCLOAK_CLIENT_ID = 'tfrs'
+                        KEYCLOAK_CALLBACK_URL = 'https://test-lowcarbonfuels.pathfinder.gov.bc.ca/authCallback'
+                        KEYCLOAK_LOGOUT_URL = 'https://logontest.gov.bc.ca/clp-cgi/logoff.cgi?returl=https%3A%2F%2Ftest-lowcarbonfuels.pathfinder.gov.bc.ca%2F'
+                        ROUTE_HOST_NAME = 'test-lowcarbonfuels.pathfinder.gov.bc.ca'
+                        ROUTE_NAME = 'test-lowcarbonfuels-frontend'
+                    } else if(envName == 'prod') {
+                        projectName = "mem-tfrs-prod"
+                        KEYCLOAK_AUTHORITY = 'https://sso.pathfinder.gov.bc.ca/auth/realms/tfrs'
+                        KEYCLOAK_CLIENT_ID = 'tfrs'
+                        KEYCLOAK_CALLBACK_URL = 'https://lowcarbonfuels.gov.bc.ca/authCallback'
+                        KEYCLOAK_LOGOUT_URL = 'https://logon.gov.bc.ca/clp-cgi/logoff.cgi?returl=https%3A%2F%lowcarbonfuels.gov.bc.ca%2F'
+                        ROUTE_HOST_NAME = 'lowcarbonfuels.gov.bc.ca'
+                        ROUTE_NAME = 'lowcarbonfuels-frontend'
+                    }
+                    openshift.withProject("${projectName}") {
+                        def clientDCJson = openshift.process(readFile(file:'openshift/templates/components/frontend/client-dc-others.json'), 
+                        "-p", 
+                        "KEYCLOAK_AUTHORITY=${KEYCLOAK_AUTHORITY}",
+                        "KEYCLOAK_CLIENT_ID=${KEYCLOAK_CLIENT_ID}",
+                        "KEYCLOAK_CALLBACK_URL=${KEYCLOAK_CALLBACK_URL}",
+                        "KEYCLOAK_LOGOUT_URL=${KEYCLOAK_LOGOUT_URL}",
+                        "ROUTE_HOST_NAME=${ROUTE_HOST_NAME}",
+                        "ROUTE_NAME=${ROUTE_NAME}"
+                        )
+                        openshift.apply(clientDCJson)
+                    }
+                }
+            }
+        }
+    }
+}
+return this
