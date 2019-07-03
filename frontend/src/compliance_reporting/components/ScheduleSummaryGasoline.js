6aa5d1c10da6ff7a6ae4463a423fb0533e471f39
Richard <richard.mizuno@gmail.com>
2019-07-03 09:47:54 -0700

    Schedule summary calculation (#1272)
    * Added backend code to include the summary calculations
    * Schedule Summary Line 1-3, 12-14 should now populate values based on values in Schedule B and C
    * Added line 5 and 16 calculation
    * Added calculation for Line 10, 11, 21 and 22

--- frontend/src/compliance_reporting/components/ScheduleSummaryGasoline.js
+++ frontend/src/compliance_reporting/components/ScheduleSummaryGasoline.js
@@ -24,8 +24,9 @@ const numericColumn = {
 
 const numericInput = {
   attributes: {
-    dataNumberToFixed: 50,
-    step: '0.01'
+    dataNumberToFixed: 0,
+    maxLength: '50',
+    step: '1'
   },
   className: 'number',
   dataEditor: Input,
@@ -110,7 +111,9 @@ const ScheduleSummaryGasoline = [
   }, {
     readOnly: true,
     value: 'Line 7'
-  }, numericInput],
+  }, {
+    readOnly: true
+  }],
   [{
     className: 'text',
     readOnly: true,
@@ -126,7 +129,9 @@ const ScheduleSummaryGasoline = [
   }, {
     readOnly: true,
     value: 'Line 9'
-  }, numericInput],
+  }, {
+    readOnly: true
+  }],
   [{
     className: 'text',
     readOnly: true,
@@ -145,8 +150,22 @@ const ScheduleSummaryGasoline = [
     readOnly: true,
     value: 'Line 11'
   }, {
-    ...numericColumn,
-    className: 'numeric total'
+    className: 'numeric',
+    readOnly: true,
+    value: '',
+    valueViewer: (cell) => {
+      const { value } = cell;
+
+      if (value === '') {
+        return '-';
+      }
+
+      if (Number(value) < 0) {
+        return <span>({Number(value * -1).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}/L)</span>;
+      }
+
+      return <span>{Number(value).toFixed(2).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}/L</span>;
+    }
   }]
 ];
 
