cfe5a8a1a29768a6195575e3a3245aeca744fd3f
Kristin Lefler <43018460+klefler@users.noreply.github.com>
2019-07-04 14:01:17 -0700

    Schedule_Report_Tab_Col_Comments (#1290)

--- backend/api/models/ComplianceReportSchedules.py
+++ backend/api/models/ComplianceReportSchedules.py
@@ -12,6 +12,7 @@ from api.models.NotionalTransferType import NotionalTransferType
 class ScheduleC(Model):
     class Meta:
         db_table = 'compliance_report_schedule_c'
+    db_table_comment = 'Container for a single instance of "Schedule C - Fuels Used for Other Purposes" report.'
 
 
 class ScheduleCRecord(Model):
@@ -38,7 +39,8 @@ class ScheduleCRecord(Model):
         blank=False,
         null=False,
         decimal_places=2,
-        max_digits=20
+        max_digits=20,
+        db_comment='Quantity of fuel supplied.'
     )
 
     expected_use = models.ForeignKey(
@@ -50,16 +52,19 @@ class ScheduleCRecord(Model):
     rationale = models.CharField(
         max_length=100,
         blank=True,
-        null=True
+        null=True,
+        db_comment='Alternate rationale when expected use is "other".'
     )
 
     class Meta:
         db_table = 'compliance_report_schedule_c_record'
+    db_table_comment = 'Line items for "Schedule C - Fuels Used for Other Purposes" report.'
 
 
 class ScheduleA(Model):
     class Meta:
         db_table = 'compliance_report_schedule_a'
+    db_table_comment = 'Container for a single instance of "Schedule A - Notional Transfers of Renewable Fuel" report.'
 
 
 class ScheduleARecord(Model):
@@ -86,28 +91,33 @@ class ScheduleARecord(Model):
         blank=False,
         null=False,
         decimal_places=2,
-        max_digits=20
+        max_digits=20,
+        db_comment='Quantity of fuel supplied.'
     )
 
     trading_partner = models.CharField(
         max_length=200,
         blank=False,
-        null=False
+        null=False,
+        db_comment='Legal organization name of the trading partner. This is a free form text field with auto-suggested values from existing Organization names.'
     )
 
     postal_address = models.CharField(
         max_length=200,
         blank=False,
-        null=False
+        null=False,
+        db_comment='Contains the trading partner address. This is a free form text field with auto-suggested values from existing Organization addresses.'
     )
 
     class Meta:
         db_table = 'compliance_report_schedule_a_record'
+    db_table_comment = 'Line items for "Schedule A - Notional Transfers of Renewable Fuel" report.'
 
 
 class ScheduleB(Model):
     class Meta:
         db_table = 'compliance_report_schedule_b'
+    db_table_comment = 'Container for a single instance of "Schedule B - Part 3 Fuel Supply" report.'
 
 
 class ScheduleBRecord(Model):
@@ -134,7 +144,8 @@ class ScheduleBRecord(Model):
         blank=False,
         null=False,
         decimal_places=2,
-        max_digits=20
+        max_digits=20,
+        db_comment='Quantity of fuel supplied.'
     )
 
     provision_of_the_act = models.ForeignKey(
@@ -151,3 +162,4 @@ class ScheduleBRecord(Model):
 
     class Meta:
         db_table = 'compliance_report_schedule_b_record'
+    db_table_comment = 'Line items for "Schedule B - Part 3 Fuel Supply" report.'
