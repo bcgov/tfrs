6f3c4d054cbe18fd48f0f8a8c314fd6c7f22ea53
Robert Johnstone <rob@plasticviking.com>
2019-07-09 17:46:37 +0800

    adjust run parameters

--- functional-tests/src/test/groovy/listeners/SessionIdHolder.groovy
+++ functional-tests/src/test/groovy/listeners/SessionIdHolder.groovy
@@ -5,5 +5,14 @@ class SessionIdHolder {
 
   public static final ThreadLocal<String> sessionId = new ThreadLocal<String>()
 
-  public static final String buildId = UUID.randomUUID().toString()
+  public static final String buildId
+
+  static {
+    def env = System.getenv()
+    if (env['BUILD_NUMBER']) {
+      buildId = "Jenkins build " + env['BUILD_NUMBER']
+    } else {
+      buildId = "Manual build " + UUID.randomUUID().toString()
+    }
+  }
 }
