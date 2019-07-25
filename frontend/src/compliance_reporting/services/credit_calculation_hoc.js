d553fcd330d186c930b674b8ada847529157c942
Robert Johnstone <rob@plasticviking.com>
2019-07-26 02:39:58 +0800

    Compliance reporting state elevation (#1345)
    * Schedule B and summary still WIP
    * Preliminary state elevation for schedule summary now included
    * remove spurious migration
    * clear fields when reselecting fuel type on schedule b

---
+++ frontend/src/compliance_reporting/services/credit_calculation_hoc.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Loading from '../../app/components/Loading';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ComplianceReportingService from './ComplianceReportingService';
+
+function getDisplayName (WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
+function withCreditCalculationService (config) {
+  return function (WrappedComponent) {
+    class CreditCalculationServiceSupport extends Component {
+      constructor (props) {
+        super(props);
+
+        this.state = {
+          ready: false
+        };
+      }
+
+      componentDidMount () {
+        ComplianceReportingService.loadData(this.props.period).then(() => {
+          this.setState({ ready: true });
+        });
+      }
+
+      render () {
+        if (!this.state.ready) {
+          return (<Loading />);
+        }
+        return (<WrappedComponent {...this.props} />);
+      }
+    }
+
+    CreditCalculationServiceSupport
+      .displayName = `CreditCalculationServiceSupport(${getDisplayName(WrappedComponent)})`;
+
+    CreditCalculationServiceSupport.propTypes = {
+      period: PropTypes.string.isRequired
+    };
+
+    return connect(null, null)(CreditCalculationServiceSupport);
+  };
+}
+
+export default withCreditCalculationService;
