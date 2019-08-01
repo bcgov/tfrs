b15a03f1abc30308d3d4056b5454f6bfdca2b614
Kuan Fan <31664961+kuanfandevops@users.noreply.github.com>
2019-08-01 13:06:09 -0700

    Add Dev and Test to Nagios monitoring. (#1362)

--- openshift/templates/components/nagios/create-nagios.sh
+++ openshift/templates/components/nagios/create-nagios.sh
@@ -1,6 +1,7 @@
 oc process -f ./nagios-base-bc.json | oc create -f -
 oc process -f ./nagios-bc.json | oc create -f -
-oc process -f ./nagios-dc.json ENV_NAME=dev IS_NAME=nagios | oc create -f -
+oc process -f ./nagios-dc.json ENV_NAME=prod | oc create -f -
 
 # last step to grant image-pull role to service account nagios
 oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-prod:nagios --namespace=mem-tfrs-tools
+oc policy add-role-to-user system:basic-user system:serviceaccount:mem-tfrs-tool:nagios --namespace=mem-tfrs-dev
