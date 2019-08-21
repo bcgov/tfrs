40caf5f8e4ed0998bd3f65d7380c1e410e93ec26
Kuan Fan <kuan.fan@gov.bc.ca>
2019-08-21 16:33:27 -0700

    add env_name to bc

--- openshift/templates/components/nagios/create-nagios.sh
+++ openshift/templates/components/nagios/create-nagios.sh
@@ -1,5 +1,5 @@
 oc process -f ./nagios-base-bc.json | oc create -f -
-oc process -f ./nagios-bc.json | oc create -f -
+oc process -f ./nagios-bc.json ENV_NAME=dev | oc create -f -
 oc process -f ./nagios-dc.json ENV_NAME=prod | oc create -f -
 
 # last step to grant image-pull role to service account nagios
