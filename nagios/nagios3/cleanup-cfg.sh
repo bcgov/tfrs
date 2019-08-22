9c4bdea9d371b9c3859defea75c5d00a30ab3a61
Kuan Fan <kuan.fan@gov.bc.ca>
2019-08-22 15:51:37 -0700

    clean up conf file according to env

--- nagios/nagios3/cleanup-cfg.sh
+++ nagios/nagios3/cleanup-cfg.sh
@@ -2,15 +2,15 @@
 
 ENV_NAME=$1
 
-if [${ENV_NAME} == "dev"]; then
+if [ ${ENV_NAME} == "dev" ]; then
     rm -f /etc/nagios3/conf.d/*-test.cfg
     rm -f /etc/nagios3/conf.d/*-prod.cfg
 fi
-if [${ENV_NAME} == "test"]; then
+if [ ${ENV_NAME} == "test" ]; then
     rm -f /etc/nagios3/conf.d/*-dev.cfg
     rm -f /etc/nagios3/conf.d/*-prod.cfg
 fi
-if [${ENV_NAME} == "prod"]; then
+if [ ${ENV_NAME} == "prod" ]; then
     rm -f /etc/nagios3/conf.d/*-dev.cfg
     rm -f /etc/nagios3/conf.d/*-test.cfg
 fi
