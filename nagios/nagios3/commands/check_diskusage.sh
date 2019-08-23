72f12b09776158a68fc4acca42b241126d9b3208
Kuan Fan <kuan.fan@gov.bc.ca>
2019-08-22 17:06:06 -0700

    remove project select

--- nagios/nagios3/commands/check_diskusage.sh
+++ nagios/nagios3/commands/check_diskusage.sh
@@ -1,10 +1,5 @@
 PROJECT_NAME=$1
 
-if ! (oc project -q $PROJECT_NAME > /dev/null); then
-    echo "Could not select project $PROJECT_NAME"
-    exit 2
-fi
-
 databasePodName=`oc get pods -n $PROJECT_NAME | grep postgresql96 | awk '{print $1}'`
 
 backupDiskUsagePercent=`oc exec $databasePodName -c postgresql96 -n $PROJECT_NAME -- df -k | grep "/postgresql-backup" | awk '{print $5}'`
--- openshift/templates/components/nagios/create-nagios.sh
+++ openshift/templates/components/nagios/create-nagios.sh
@@ -4,6 +4,13 @@ oc process -f ./nagios-bc.json ENV_NAME=test | oc create -f -
 oc process -f ./nagios-bc.json ENV_NAME=prod | oc create -f -
 oc process -f ./nagios-dc.json ENV_NAME=prod | oc create -f -
 
-# last step to grant image-pull role to service account nagios
-oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-prod:nagios --namespace=mem-tfrs-tools
-oc policy add-role-to-user system:basic-user system:serviceaccount:mem-tfrs-tool:nagios --namespace=mem-tfrs-dev
+## grant buikd to pull image from tools project, as nagios uses nagios-base image which is in tools project
+oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-dev:builder --namespace=mem-tfrs-tools
+oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-test:builder --namespace=mem-tfrs-tools
+oc policy add-role-to-user system:image-puller system:serviceaccount:mem-tfrs-prod:builder --namespace=mem-tfrs-tools
+
+## grant service account nagios edit role otherwise check replica will fail
+oc policy add-role-to-user edit system:serviceaccount:mem-tfrs-dev:nagios -n mem-tfrs-dev
+oc policy add-role-to-user edit system:serviceaccount:mem-tfrs-test:nagios -n mem-tfrs-test
+oc policy add-role-to-user edit system:serviceaccount:mem-tfrs-prod:nagios -n mem-tfrs-prod
+
