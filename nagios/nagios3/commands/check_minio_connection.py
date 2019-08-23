8949ae86ef5eb936e7069772b5ba2de7e22bf44d
Kuan Fan <kuan.fan@gov.bc.ca>
2019-08-23 13:47:53 -0700

    add minio env vars

--- nagios/nagios3/commands/check_minio_connection.py
+++ nagios/nagios3/commands/check_minio_connection.py
@@ -6,8 +6,7 @@ MINIO = {
     'ENDPOINT': os.getenv('MINIO_ENDPOINT', None),
     'USE_SSL': bool(os.getenv('MINIO_USE_SSL', 'False').lower() in ['true', 1]),
     'ACCESS_KEY': os.getenv('MINIO_ACCESS_KEY', None),
-    'SECRET_KEY': os.getenv('MINIO_SECRET_KEY', None),
-    'BUCKET_NAME': os.getenv('MINIO_BUCKET_NAME', 'tfrs'),
+    'SECRET_KEY': os.getenv('MINIO_SECRET_KEY', None)
 }
 
 minio = Minio(MINIO['ENDPOINT'],
