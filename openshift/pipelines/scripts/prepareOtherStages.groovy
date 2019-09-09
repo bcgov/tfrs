46a7402b558c22bc1a7588fb6ebe87b58dfccef2
Kuan Fan <kuan.fan@gov.bc.ca>
2019-09-09 11:46:34 -0700

    add script to check db backup space

--- openshift/pipelines/scripts/prepareOtherStages.groovy
+++ openshift/pipelines/scripts/prepareOtherStages.groovy
@@ -47,8 +47,8 @@ def checkBackupSpace (String projectName) {
             postgresql_pod_name = sh (script: "oc get pods -n ${projectName} | grep postgresql96 | awk \'{print \$1}\'", returnStdout: true).trim()
             check_result = sh (script: "oc exec ${postgresql_pod_name} -c postgresql96 -n ${projectName} -- df -k | grep \"/postgresql-backup\" | awk \'{print \$5}\'", returnStdout: true).trim()
             int usedPercentage = check_result.trim().replace("%","").toInteger()
-            if(usedPercentage>=70) {
-                input "Please check if postgresql-backup space is enough for database backup, make sure it is less than 70% full."
+            if(usedPercentage>=80) {
+                input "Please check if postgresql-backup space is enough for database backup, make sure it is less than 80% full."
             } else {
                 echo "postgresql-backup space is ${check_result} full."
             }
