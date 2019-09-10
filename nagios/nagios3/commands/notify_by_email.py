388b4f6a324f4965dc45df8d385f0ab0223c9ef2
Kuan Fan <kuan.fan@gov.bc.ca>
2019-09-10 11:20:33 -0700

    add email notification

---
+++ nagios/nagios3/commands/notify_by_email.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from email.message import EmailMessage
+import smtplib
+
+# sys.argv[1] is subject
+# sys.argv[2] is email body
+# sys.argv[3] is receiver email address(es)
+
+EMAIL = {
+    'SMTP_SERVER_HOST': os.getenv('SMTP_SERVER_HOST', 'apps.smtp.gov.bc.ca'),
+    'SMTP_SERVER_PORT': int(os.getenv('SMTP_SERVER_PORT', 25))
+}
+
+msg = EmailMessage()
+msg.set_content(sys.argv[1])
+
+msg['Subject'] = sys.argv[2]
+msg['From'] = "tfrs@gov.bc.ca"
+msg['To'] = sys.argv[3]
+
+with smtplib.SMTP(host=EMAIL['SMTP_SERVER_HOST'],
+                    port=EMAIL['SMTP_SERVER_PORT']) as server:
+    try:
+        server.send_message(msg)
+        print('OK - Email sending succeed')
+    except Exception as error:
+        print('CRITICAL - Email sending failed')
