8a68467829ee297c3583bf3e2d146b8f1ddbe17a
Kuan Fan <kuan.fan@gov.bc.ca>
2019-09-10 15:24:17 -0700

    update email script

--- nagios/nagios3/commands/notify_by_email.py
+++ nagios/nagios3/commands/notify_by_email.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from email.message import EmailMessage
+from email.mime.text import MIMEText
 import smtplib
 
 # sys.argv[1] is subject
@@ -12,9 +13,10 @@ EMAIL = {
     'SMTP_SERVER_PORT': int(os.getenv('SMTP_SERVER_PORT', 25))
 }
 
+emailBody = sys.argv[2].replace("\\n", "\n")
+
 msg = EmailMessage()
-msg.set_default_type("text/plain")
-msg.set_content(sys.argv[2])
+msg.set_content(emailBody)
 
 msg['Subject'] = sys.argv[1]
 msg['From'] = "noreply@gov.bc.ca"
