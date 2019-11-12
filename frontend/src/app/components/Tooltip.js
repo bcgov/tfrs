0fbaf342c4bd73ab173d6a681655aef234927eb4
Richard Tan <richard.tan@gov.bc.ca>
2019-11-12 11:52:22 -0800

    Added delay in hiding the tooltip when you're hovering over the actual tooltip. This is to reduce the flashing issue

--- frontend/src/app/components/Tooltip.js
+++ frontend/src/app/components/Tooltip.js
@@ -4,9 +4,36 @@ import PropTypes from 'prop-types';
 import { OverlayTrigger, Tooltip as ReactTooltip } from 'react-bootstrap';
 
 class Tooltip extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      hovering: 0
+    };
+
+    this._hover = this._hover.bind(this);
+    this._hoverOut = this._hoverOut.bind(this);
+  }
+
+  _hover () {
+    this.setState({
+      hovering: this.state.hovering + 10000
+    });
+  }
+
+  _hoverOut () {
+    this.setState({
+      hovering: 0
+    });
+  }
+
   _renderOverlay () {
     return (
-      <OverlayTrigger placement="top" overlay={this._tooltip()}>
+      <OverlayTrigger
+        delayHide={this.state.hovering}
+        placement="top"
+        overlay={this._tooltip()}
+      >
         <div className="overlay-trigger">
           {this.props.children}
         </div>
@@ -16,7 +43,14 @@ class Tooltip extends Component {
 
   _tooltip () {
     return (
-      <ReactTooltip className={this.props.className} id="tooltip">
+      <ReactTooltip
+        className={this.props.className}
+        id="tooltip"
+        onBlur={this._hoverOut}
+        onMouseLeave={this._hoverOut}
+        onMouseOver={this._hover}
+        onFocus={this._hover}
+      >
         {Array.isArray(this.props.title) &&
           this.props.title.map(title => (<div key={title}><ReactMarkdown source={title} /></div>))
         }
