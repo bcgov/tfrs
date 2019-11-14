fe31a2daa0e6a3a46acc34cede36a33988b6b3c2
Richard <Richard.Tan@gov.bc.ca>
2019-11-14 11:22:04 -0800

    Ie 11 fix (#1612)
    * Fixed tooltip making stuff unclickable
    
    * Reverted socket io to older version for ie 11 compatibility fix

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import {connect} from 'react-redux';
-import {withRouter} from 'react-router';
-import {hot} from 'react-hot-loader/root';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { hot } from 'react-hot-loader/root';
 
 import KeycloakAwareApp from './KeycloakAwareApp';
 
