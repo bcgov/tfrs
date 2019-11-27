bcb2bde4efb7b3a38d80b964e3463e7f1bb7531e
Richard <Richard.Tan@gov.bc.ca>
2019-11-27 10:23:18 -0800

    Dashboard and Report History Revisions (#1634)
    * Added better labels for the dashboard
    
    * Government of British Columbia is no longer displayed in the report history

--- frontend/src/compliance_reporting/components/ComplianceReportingStatusHistory.js
+++ frontend/src/compliance_reporting/components/ComplianceReportingStatusHistory.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
 
 class ComplianceReportingStatusHistory extends Component {
-
-  static actionFor(h) {
+  static actionFor (h) {
     let action = (<strong>{h.status.fuelSupplierStatus}</strong>);
     let roleDisplay = null;
-
 
     if (h.status.directorStatus === 'Accepted') {
       action = (<strong>Accepted</strong>);
@@ -20,7 +18,6 @@ class ComplianceReportingStatusHistory extends Component {
           roleDisplay = roleDisplay.replace('Government ', '');
         }
       }
-
     } else if (h.status.directorStatus === 'Rejected') {
       action = (<strong>Rejected</strong>);
 
@@ -46,41 +43,44 @@ class ComplianceReportingStatusHistory extends Component {
       action = (<strong>Supplemental Requested </strong>);
     }
 
-    return (<span>
-      {action}
-      <span> on </span>
-      {moment(h.createTimestamp).format('LL')}
-      <span> by </span>
-      <strong>{h.user.firstName} {h.user.lastName}</strong>
-      <span> of </span>
-      <strong>{h.user.organization.name}</strong>
-      {roleDisplay &&
+    return (
       <span>
+        {action}
+        <span> on </span>
+        {moment(h.createTimestamp).format('LL')}
+        <span> by </span>
+        <strong>{h.user.firstName} {h.user.lastName}</strong>
+        {(!roleDisplay || roleDisplay.indexOf('Director') < 0) &&
+          <span>
+            <span> of </span>
+            <strong>{h.user.organization.name}</strong>
+          </span>
+        }
+        {roleDisplay &&
+        <span>
           <strong>, {roleDisplay} </strong>
           <span>under the</span>
           <em> Greenhouse Gas Reduction (Renewable and Low Carbon Fuel Requirements) Act</em>
+        </span>
+        }
       </span>
-      }
-    </span>);
+    );
   }
 
-  render() {
+  render () {
     if (!this.props.complianceReport.history || this.props.complianceReport.history.length === 0) {
       return false;
     }
     const currentId = this.props.complianceReport.id;
 
-    const showCurrent = (this.props.complianceReport.history.filter(
-      c => (c.complianceReport === this.props.complianceReport.id)
-    ).length === 0);
+    const showCurrent = (this.props.complianceReport.history.filter(c =>
+      (c.complianceReport === this.props.complianceReport.id)).length === 0);
 
-    const {deltas} = this.props.complianceReport;
+    const { deltas } = this.props.complianceReport;
 
     const distinctReports = this.props.complianceReport.history.reduce(
       (m, value) => {
-        if (!m.some(v => {
-          return v.displayName === value.displayName
-        })) {
+        if (!m.some(v => (v.displayName === value.displayName))) {
           m.push({
             displayName: value.displayName,
             id: value.complianceReport,
@@ -94,29 +94,32 @@ class ComplianceReportingStatusHistory extends Component {
     );
 
     return (
-      <div className={'panel-group'}>
+      <div className="panel-group">
         {showCurrent &&
         <div className="panel panel-default report-history-panel">
           <div className="panel-body" onClick={() => this.props.onSwitchHandler(-1, 'snapshot')}>
-            <span className={'title'}>{this.props.complianceReport.displayName}</span><br/>
+            <span className="title">{this.props.complianceReport.displayName}</span><br />
             <strong>Draft</strong>
           </div>
         </div>
         }
         {distinctReports.length > 0 &&
-        distinctReports.map(r => {
-          const currentDelta = deltas ? deltas.find(f => f.ancestorDisplayName === r.displayName) : null;
+        distinctReports.map((r) => {
+          const currentDelta = deltas ? deltas.find(f =>
+            f.ancestorDisplayName === r.displayName) : null;
           let deltaPanel = null;
 
           if (currentDelta && !this.props.hideChangelogs) {
             deltaPanel = (
-              <div key={`delta-${r.id}`}
-                   className={'panel panel-default report-history-panel indented'}
-                   onClick={() => this.props.onSwitchHandler(r.id === currentId ? -1 : r.id, 'delta')}>
+              <div
+                className="panel panel-default report-history-panel indented"
+                key={`delta-${r.id}`}
+                onClick={() => this.props.onSwitchHandler(r.id === currentId ? -1 : r.id, 'delta')}
+              >
                 <div className="panel-body">
-                    <span className={'title'}>
-                      {`Changelog for ${currentDelta.ancestorDisplayName}`}
-                    </span>
+                  <span className="title">
+                    {`Changelog for ${currentDelta.ancestorDisplayName}`}
+                  </span>
                   <ul>
                     <li>
                       {`${currentDelta.delta.length} records changed`}
@@ -128,26 +131,24 @@ class ComplianceReportingStatusHistory extends Component {
           }
 
           return ([
-              deltaPanel
-              ,
-              <div className="panel panel-default report-history-panel"
-                   key={r.displayName}
-                   onClick={() => this.props.onSwitchHandler(r.id === currentId ? -1 : r.id, 'snapshot')}>
-                <div className="panel-body">
-                  <span className={'title'}>{r.displayName}</span>
-                  <ul>
-                    {r.history.map(h => {
-                      return (
-                        <li key={h.id}>
-                          {ComplianceReportingStatusHistory.actionFor(h)}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </div>]
+            deltaPanel,
+            <div
+              className="panel panel-default report-history-panel"
+              key={r.displayName}
+              onClick={() => this.props.onSwitchHandler(r.id === currentId ? -1 : r.id, 'snapshot')}
+            >
+              <div className="panel-body">
+                <span className="title">{r.displayName}</span>
+                <ul>
+                  {r.history.map(h => (
+                    <li key={h.id}>
+                      {ComplianceReportingStatusHistory.actionFor(h)}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>]
           );
-
         })
         }
       </div>
@@ -179,6 +180,7 @@ ComplianceReportingStatusHistory.propTypes = {
         description: PropTypes.string
       })
     ]),
+    displayName: PropTypes.string,
     history: PropTypes.arrayOf(PropTypes.shape({
       creditTradeUpdateTime: PropTypes.string,
       isRescinded: PropTypes.bool,
@@ -193,6 +195,7 @@ ComplianceReportingStatusHistory.propTypes = {
         lastName: PropTypes.string
       })
     })),
+    id: PropTypes.number,
     organization: PropTypes.shape({
       name: PropTypes.string
     })
