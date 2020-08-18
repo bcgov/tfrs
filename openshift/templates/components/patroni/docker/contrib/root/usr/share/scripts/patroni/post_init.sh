6d09684353abcc10af2dc20a9dd571c2aeafc636
Kuan Fan <kuan.fan@gov.bc.ca>
2020-08-18 16:57:41 -0700

    add extension pg_trgm when create

--- openshift/templates/components/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ openshift/templates/components/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -9,7 +9,7 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
 
   echo "Creating extensions" 
-  psql -U postgres -q -d "${APP_DATABASE}" -c 'create extension if not exists hstore'
+  psql -U postgres -q -d "${APP_DATABASE}" -c 'create extension if not exists hstore pg_trgm'
 
 else
   echo "Skipping user creation"
