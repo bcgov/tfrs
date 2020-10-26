766b955267e1bde1a7037e29c9adafe89999a7f7
Richard Tan <richard.tan@gov.bc.ca>
2020-10-26 15:15:58 -0700

    New script to remove duplicate users (unused)

---
+++ backend/api/fixtures/operational/0024_remove_duplicate_bceid_users_from_prod.py
@@ -0,0 +1,52 @@
+from django.db import transaction
+
+from api.management.data_script import OperationalDataScript
+from api.models.NotificationMessage import NotificationMessage
+from api.models.User import User
+from api.models.UserCreationRequest import UserCreationRequest
+
+
+class RemoveBCEIDUsers(OperationalDataScript):
+    """
+    Removes some of the duplicate BCEID users from prod
+    """
+    is_revertable = False
+    comment = 'Removes some of the duplicate BCEID users from prod'
+
+    def check_run_preconditions(self):
+        return True
+
+    @transaction.atomic
+    def run(self):
+        user_list = [
+            "idemitsuapollo",
+            "iacdavid",
+            "BHumphreys",
+            "DHudema",
+            "GBhatia",
+            "GBhatia2",
+            "KMack",
+            "SStener",
+            "test1.phone",
+            "rpi"
+        ]
+
+        users = UserCreationRequest.objects.filter(
+            external_username__in=user_list
+        )
+
+        for user in users:
+            NotificationMessage.objects.filter(
+                user_id=user.user_id
+            ).delete()
+
+            UserCreationRequest.objects.filter(
+                user_id=user.user_id
+            ).delete()
+
+            User.objects.filter(
+                id=user.user_id
+            ).delete()
+
+
+script_class = RemoveBCEIDUsers
