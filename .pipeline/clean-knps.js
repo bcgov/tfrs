2b68c33bda49a31a960f3c3acc24f1e502ef22fc
Kuan Fan <kuan.fan@gov.bc.ca>
2021-03-03 13:25:29 -0800

    udpate to KNP

--- .pipeline/clean-knps.js
+++ .pipeline/clean-knps.js
@@ -1,5 +1,5 @@
 'use strict';
 const settings = require('./lib/config.js')
-const task = require('./lib/clean-nsps.js')
+const task = require('./lib/clean-knps.js')
 
 task(Object.assign(settings, { phase: settings.options.env}));
