0d50230702032f49a769a915c39811107fb28c76
Kuan Fan <kuan.fan@gov.bc.ca>
2021-06-28 11:23:09 -0700

    update build and deploy or 1.6.24

--- .pipeline/lib/build.js
+++ .pipeline/lib/build.js
@@ -15,7 +15,6 @@ module.exports = settings => {
   console.log( oc.git.http_url);
   console.log( oc.git.ref);
 
-  /*
   //build backend
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/backend/backend-bc.yaml`, {
     'param':{
@@ -26,7 +25,6 @@ module.exports = settings => {
       'GIT_REF': oc.git.ref
     }
   }))
-  */
 
   //build frontend
   objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/frontend/frontend-angular-app-bc.yaml`, {
@@ -48,7 +46,7 @@ module.exports = settings => {
       'GIT_REF': oc.git.ref
     }
   }))
-/*
+
  //build celery
  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/celery/celery-bc.yaml`, {
   'param':{
@@ -59,8 +57,8 @@ module.exports = settings => {
     'RELEASE_BRANCH': phases[phase].releaseBranch
   }
 }))
-*/
- //build notification server
+
+//build notification server
  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/notification/notification-server-bc.yaml`, {
   'param':{
     'NAME': phases[phase].name,
@@ -70,8 +68,8 @@ module.exports = settings => {
     'GIT_REF': oc.git.ref
   }
 }))
-/*
- //build scan coordinator server
+
+//build scan coordinator server
  objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/scan-coordinator/scan-coordinator-bc.yaml`, {
   'param':{
     'NAME': phases[phase].name,
@@ -91,7 +89,7 @@ objects = objects.concat(oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/
     'RELEASE_BRANCH': phases[phase].releaseBranch
   }
 }))
-*/
+
   oc.applyRecommendedLabels(
     objects,
     phases[phase].name,
