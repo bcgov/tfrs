478b9e3a01f0d87bae5221dffb430ea859dbef9a
kuanfandevops <kuan.fan@gov.bc.ca>
2022-11-18 11:35:39 -0800

    remove openshift v3 templates

--- openshift/templates/components/patroni/docker/contrib/root/usr/share/scripts/patroni/health_check.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu
-set -o pipefail
-
-pg_isready -q && patronictl list --format=json | jq -e ".[] | select(.Member == \"$(hostname)\" and .State == \"running\" and .\"Lag in MB\" == 0)"
