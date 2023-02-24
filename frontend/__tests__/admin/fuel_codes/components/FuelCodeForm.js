9f83d7a444717999762019f9aed8af9a60dc0ef0
AlexZorkin <alex@bigthink.io>
2023-02-24 14:05:47 -0800

    fix: jest tests fixed for new versions

--- frontend/__tests__/admin/fuel_codes/components/FuelCodeForm.js
+++ frontend/__tests__/admin/fuel_codes/components/FuelCodeForm.js
@@ -1,44 +1,52 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import { Provider } from 'react-redux'
+import renderer from 'react-test-renderer'
+import { BrowserRouter } from 'react-router-dom'
 
-import FuelCodeForm from '../../../../src/admin/fuel_codes/components/FuelCodeForm';
-import store from '../../../../src/store/store';
+import FuelCodeForm from '../../../../src/admin/fuel_codes/components/FuelCodeForm'
+import store from '../../../../src/store/store'
 
 describe('Test Fuel Code Form', () => {
   const approvedFuels = [{
     name: 'LNG'
   }, {
     name: 'Propane'
-  }];
-  const errors = {};
+  }]
+  const errors = {}
   const fields = {
     feedstockTransportMode: [],
     fuelTransportMode: []
-  };
+  }
   const transportModes = [{
     name: 'Truck'
   }, {
     name: 'Rail'
-  }];
+  }]
+  const fuelCodes = {
+    isFetching: false,
+    items: []
+  }
 
   const component = renderer.create(
-    <Provider store={store}>
-      <FuelCodeForm
-        addToFields={() => {}}
-        approvedFuels={approvedFuels}
-        errors={errors}
-        fields={fields}
-        handleInputChange={() => {}}
-        handleSubmit={() => {}}
-        title="Edit Fuel"
-        transportModes={transportModes}
-      />
-    </Provider>
-  );
+    <BrowserRouter>
+      <Provider store={store}>
+        <FuelCodeForm
+          addToFields={() => {}}
+          approvedFuels={approvedFuels}
+          errors={errors}
+          fields={fields}
+          handleInputChange={() => {}}
+          handleSubmit={() => {}}
+          title="Edit Fuel"
+          transportModes={transportModes}
+          fuelCodes={fuelCodes}
+        />
+      </Provider>
+    </BrowserRouter>
+  )
 
   test('FuelCodeForm should display', () => {
-    const tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
