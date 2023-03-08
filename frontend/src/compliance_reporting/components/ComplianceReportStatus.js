472c4f5f7abbc33210162541c0899793f372e1cd
vibhishan <vibhishan.vel@quartech.com>
2023-03-08 07:29:09 -0800

    Fixed original and current status

--- frontend/src/compliance_reporting/components/ComplianceReportStatus.js
+++ frontend/src/compliance_reporting/components/ComplianceReportStatus.js
@@ -1,41 +1,41 @@
-const ComplianceReportStatus = (props) => {
-  if (props.status.managerStatus === 'Requested Supplemental') {
+const ComplianceReportStatus = (item) => {
+  if (item.status.managerStatus === 'Requested Supplemental') {
     return 'Supplemental Requested'
   }
 
-  if (props.status.analystStatus === 'Requested Supplemental') {
+  if (item.status.analystStatus === 'Requested Supplemental') {
     return 'Supplemental Requested'
   }
 
-  if (props.status.directorStatus === 'Accepted') {
+  if (item.status.directorStatus === 'Accepted') {
     return 'Accepted'
   }
 
-  if (props.status.directorStatus === 'Rejected') {
+  if (item.status.directorStatus === 'Rejected') {
     return 'Rejected'
   }
 
-  if (props.status.managerStatus === 'Recommended') {
+  if (item.status.managerStatus === 'Recommended') {
     return 'Recommended Acceptance - Manager'
   }
 
-  if (props.status.managerStatus === 'Not Recommended') {
+  if (item.status.managerStatus === 'Not Recommended') {
     return 'Recommended Rejection - Manager'
   }
 
-  if (props.status.analystStatus === 'Recommended') {
+  if (item.status.analystStatus === 'Recommended') {
     return 'Recommended Acceptance - Analyst'
   }
 
-  if (props.status.analystStatus === 'Not Recommended') {
+  if (item.status.analystStatus === 'Not Recommended') {
     return 'Recommended Rejection - Analyst'
   }
 
-  if (props.status.fuelSupplierStatus) {
-    return props.status.fuelSupplierStatus
+  if (item.status.fuelSupplierStatus) {
+    return item.status.fuelSupplierStatus
   }
 
-  return props.status
+  return item.status
 }
 
 export default ComplianceReportStatus
