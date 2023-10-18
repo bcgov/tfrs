acaa3bdbcfcb263d23d88a38ef8432b0dded7b0f
jig-patel <122304104+jig-patel@users.noreply.github.com>
2023-10-18 19:53:18 -0400

    removing comment

--- frontend/src/organizations/components/OrganizationDetails.js
+++ frontend/src/organizations/components/OrganizationDetails.js
@@ -114,15 +114,12 @@ const OrganizationDetails = props => {
             <dd>Email <a href="mailto:lcfs@gov.bc.ca?subject=TFRS Address Update">lcfs@gov.bc.ca</a> to update address information.</dd>
           </dl>
         </div>
-        {/* {props.organization.organizationAddress &&
-        props.organization.organizationAddress.attorneyStreetAddress && */}
         <div className="address">
           <dl className="dl-horizontal">
             <dt style={{ width: '300px' }}><strong>Company Profile, EDRMS Record #:</strong></dt>
             <dd><strong>{props.organization.edrmsRecord ? props.organization.edrmsRecord : ''}</strong></dd>
           </dl>
         </div>
-        {/* // } */}
         <div className="status">
           <dl className="dl-horizontal">
             <dt style={{ width: '300px' }}><strong>Status:</strong></dt>
