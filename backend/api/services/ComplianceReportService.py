6e96df968436ca7e1867f98ae15464504e24497c
Hamed Valiollahi Bayeki <hamedv90@gmail.com>
2024-02-15 07:53:59 -0800

    fix: resolve error saving on compliance report acceptance

--- backend/api/services/ComplianceReportService.py
+++ backend/api/services/ComplianceReportService.py
@@ -308,7 +308,7 @@ class ComplianceReportService(object):
             # Code 26C is used to identify credits that must be refunded to the supplier.
             # This occurs when our debit position decreases and we have already spent credits. 
             # In such cases, any excess credits must be returned to the supplier.
-            if is_supplemental and Decimal(lines['26C']) > 0:
+            if is_supplemental and Decimal(lines.get('26C', 0)) > 0:
                 print("*** DIRECTOR 26C Increase to Credits ***")
                 required_credit_transaction = Decimal(lines['26C'])
             
